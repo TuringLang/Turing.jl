8583e37b1e276dd7a95904c75b7e65416ac518f3
Hong Ge <hg344@cam.ac.uk>
2016-05-30 17:56:31 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,4 @@
 @unix_only run(`make`)
-@windows_only Int == Int32 ? run(`mingw32-make`) : run(`mingw32-make`)
+# @windows_only Int == Int32 ? run(`mingw32-make`) : run(`mingw32-make`)
+@windows_only run(`mingw32-make`)
 
