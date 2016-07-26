55227984634d7f37789bcbc8ca409a0f166b7ef4
Kai Xu <xukai921110@gmail.com>
2016-07-26 21:44:30 +0100

    remove ess

--- src/core/util.jl
+++ src/core/util.jl
@@ -49,26 +49,3 @@ function forceVector(in_v, T :: DataType)
   end
   return out_v
 end
-
-using StatsBase
-# Effective Sample Size
-function ess(samples)
-  """
-  @input:
-    samples : a vector of samples
-  @output:
-    ess = n / (1 + 2∑ρ)
-  """
-  n = length(samples)
-  acfs = StatsBase.autocor(samples, [1:(n - 1), deman=false])
-  # Truncate the array according to http://search.r-project.org/library/LaplacesDemon/html/ESS.html
-  acfs_trancated = []
-  for i = 1:n
-    if acfs[i] < 0.05
-      break
-    end
-    push!(acfs_trancated, acfs[i])
-  end
-  # filter!(a -> a > 0.05, acfs)
-  return n / (1 + 2 * sum(acfs_trancated))
-end
