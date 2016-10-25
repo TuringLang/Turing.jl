ae2d5e8bfcde33c117cc8e146fdc47b558b65489
Hong Ge <hg344@cam.ac.uk>
2016-10-25 13:04:02 +0000

    Bugfix in building task.c

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,7 @@
-@static if is_unix() run(`make`)
-@static if is_windows() run(`mingw32-make`)
-
+if VERSION >= v"0.5"
+  if is_unix() run(`make`) end
+  if is_windows() run(`mingw32-make`) end
+else
+  @windows_only run(`mingw32-make`) 
+  @unix_only run(`make`)
+end
