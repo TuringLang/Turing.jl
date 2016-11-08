a1640d6f0dfb98842af4b00b1da8d0f63e00db98
Kai Xu <xukai921110@gmail.com>
2016-11-08 15:34:02 +0000

    Rename test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,33 +3,32 @@
 ##########################################
 
 testcases = [
-    "assume",
-    "beta-binomial",
-    "importance_sampling",
-    "noparam",
-    "observe",
-    "predict",
-    "resample"]
-
-for t in testcases include(t*".jl") end
+  "assume",
+  "beta_binomial",
+  "importance_sampling",
+  "noparam",
+  "observe",
+  "predict",
+  "resample",
+  "clonetask",
+  "particlecontainer",
+  "priorcontainer",
+  "trace",
+  "multivariate_support_for_hmc",
+  "pass_dual_to_dists"
+]
 
 testcases_v05 = [
-    "beta-binomial",
-    "test_tarray"]
+  "beta_binomial",
+  "tarray"
+]
+
+testcases_untouched = [
+  "tarray2"
+]
+
+for t in testcases include(t*".jl") end
 
 if VERSION < v"0.5"
   for t in testcases_v05 include(t*".jl") end
 end
-
-
-include("test_clonetask.jl")
-include("test_tarray.jl")
-
-# include("test_tarray2.jl")
-include("test_particlecontainer.jl")
-
-# For HMC
-include("test_priorcontainer.jl")
-include("test_replay.jl")
-include("test_multidimsupport.jl")
-include("test_distributions_with_dual.jl")
