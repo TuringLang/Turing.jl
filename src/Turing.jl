42e0ffb7b2ca1f6ed7680d7e479c5eee1c2aab4c
Kai Xu <xukai921110@gmail.com>
2016-11-08 14:21:28 +0000

    Attempt to reload pdf and logpdf

--- src/Turing.jl
+++ src/Turing.jl
@@ -1,12 +1,31 @@
-
 module Turing
 
 # Code associated with running probabilistic programs as tasks
 include("trace/trace.jl")
 
-using Distributions
-using ForwardDiff: Dual
+using ForwardDiff: Dual, npartials
 using Turing.Traces
+
+# TODO: move this to seperate AD module
+Base.convert(::Type{Float64}, d::Dual{0,Float64}) = d.value
+
+###########
+# Warning #################################
+# The following overloadings is temporary #
+# before StatsFuns.jl accepts our PR.     #
+# FIXME: this overload doesn't work now   #
+
+# using Distributions: Beta, Gamma, pdf
+# betapdf(α::Real, β::Real, x::Number) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
+# betalogpdf(α::Real, β::Real, x::Number) = (α - 1) * log(x) + (β - 1) * log(1 - x) - log(beta(α, β))
+# gammapdf(k::Real, θ::Real, x::Number) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
+# gammalogpdf(k::Real, θ::Real, x::Number) = -log(gamma(k)) - k * log(θ) + (k - 1) * log(x) - x / θ
+# pdf(d::Beta, x::Real) = betapdf(d.α, d.β, x)
+# pdf(d::Gamma, x::Real) = gammapdf(d.k, d.θ, x)
+# logpdf(d::Beta, x::Real) = betalogpdf(d.α, d.β, x)
+# logpdf(d::Gamma, x::Real) = gammalogpdf(d.k, d.θ, x)
+
+###########################################
 
 # Turing essentials - modelling macros and inference algorithms
 export @model, @assume, @observe, @predict, InferenceAlgorithm, HMC, IS, SMC, PG, sample
