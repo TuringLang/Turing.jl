62387b63233405169306430ee1abe5c442320239
Kai Xu <xukai921110@gmail.com>
2016-11-08 15:15:22 +0000

    Improve the index test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,24 @@
-include("predict.jl")
-include("assume.jl")
-include("observe.jl")
-include("noparam.jl")
-include("beta-binomial.jl")
-include("resample.jl")
-include("importance_sampling.jl")
+##########################################
+# Master test file for running all tests #
+##########################################
+
+testcases = [
+    "assume",
+    "beta-binomial",
+    "importance_sampling",
+    "noparam",
+    "observe",
+    "predict",
+    "resample"]
+
+res = pmap(testcases) do t
+    include(t*".jl")
+    nothing
+end
+
 
 include("test_clonetask.jl")
 include("test_tarray.jl")
-
 
 # include("test_tarray2.jl")
 include("test_particlecontainer.jl")
