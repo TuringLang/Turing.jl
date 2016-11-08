58205ac1b73218fefc9f10192fc219e115c15f93
Kai Xu <xukai921110@gmail.com>
2016-11-08 12:51:09 +0000

    Add HMC tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ include("importance_sampling.jl")
 
 include("test_clonetask.jl")
 include("test_tarray.jl")
-# include("test_replay.jl")
+include("test_replay.jl")
 
 # include("test_tarray2.jl")
 include("test_particlecontainer.jl")
