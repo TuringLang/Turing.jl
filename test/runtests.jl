0681a9564a4bb3332cd3fe59115507de095464df
Kai Xu <xukai921110@gmail.com>
2016-11-08 13:51:15 +0000

    Clear test of HMC

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,16 @@ include("importance_sampling.jl")
 
 include("test_clonetask.jl")
 include("test_tarray.jl")
-include("test_replay.jl")
+
 
 # include("test_tarray2.jl")
 include("test_particlecontainer.jl")
-include("test_priorcontainer.jl")
 
+
+
+
+# For HMC
+include("test_priorcontainer.jl")
+include("test_replay.jl")
 include("test_multidimsupport.jl")
+include("test_distributions_with_dual.jl")
