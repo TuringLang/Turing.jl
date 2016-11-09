18ca25922fd4d9969ab1a01eedb97391bd1fd0f8
Kai Xu <xukai921110@gmail.com>
2016-11-09 23:06:36 +0000

    Improve chain util test

--- test/chain_utility.jl
+++ test/chain_utility.jl
@@ -1,13 +1,17 @@
 using Turing: Chain, Sample
+using Base.Test
 
 c = Chain()
-println(c)
+@test string(c) == "Empty Chain, weight 0.0"
+
 d = Dict{Symbol, Any}()
 d[:m] = [1,2,3]
 s = Sample(1, d)
+
 c2 = Chain(1, Vector{Sample}([s]))
-println(c2)
+@test string(c2) == "Chain, model evidence (log)  1.0 and means Dict{Symbol,Any}(:m=>[1.0,2.0,3.0])"
 
 samples = c2[:samples]
+@test samples[1][:m] == d[:m]
 
-mean(c2, :m, x -> x)
+@test mean(c2, :m, x -> x) == [1.0, 2.0, 3.0]
