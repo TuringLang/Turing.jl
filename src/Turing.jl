c92d6d5bda7d3a7fb3471e352d159b744817e4f6
Kai Xu <xukai921110@gmail.com>
2016-11-10 19:08:03 +0000

    Remove unused file

--- src/Turing.jl
+++ src/Turing.jl
@@ -41,7 +41,6 @@ global debug_level = 0
 dprintln(v, args...) = v < Turing.debug_level ? println(args...) : nothing
 
 # Inference code
-include("distributions/distributions.jl")
 include("core/util.jl")
 include("core/compiler.jl")
 include("core/intrinsic.jl")
