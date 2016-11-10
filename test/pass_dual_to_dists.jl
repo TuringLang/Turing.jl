0493a1038edbf5c2a7c87315f79ebc1c37c40b71
Kai Xu <xukai921110@gmail.com>
2016-11-10 18:03:12 +0000

    Improve test of pass dual to dists

--- test/pass_dual_to_dists.jl
+++ test/pass_dual_to_dists.jl
@@ -3,10 +3,17 @@
 using Turing
 using Distributions
 using ForwardDiff: Dual
+using Base.Test
 
+f1 = 1.1
+f2 = 1.1
 d1 = Dual(1.1)
 d2 = Dual(2.3)
 
-logpdf(Normal(0, 1), d1)
-logpdf(Gamma(2, 3), d2)
-logpdf(Beta(2, 3), (d2 - d1) / 2)
+@test logpdf(Normal(0, 1), d1).value == logpdf(Normal(0, 1), f1)
+@test logpdf(Gamma(2, 3), d2).value == logpdf(Gamma(2, 3), f2)
+@test logpdf(Beta(2, 3), (d2 - d1) / 2).value == logpdf(Beta(2, 3), (f2 - f1) / 2)
+
+@test pdf(Normal(0, 1), d1).value == pdf(Normal(0, 1), f1)
+@test pdf(Gamma(2, 3), d2).value == pdf(Gamma(2, 3), f2)
+@test pdf(Beta(2, 3), (d2 - d1) / 2).value == pdf(Beta(2, 3), (f2 - f1) / 2)
