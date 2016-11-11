c9ca20431499b9748b5c788c6b0c72939bd4b5d3
Kai Xu <xukai921110@gmail.com>
2016-11-11 22:30:18 +0000

    Add more smc particles

--- test/chain_utility.jl
+++ test/chain_utility.jl
@@ -9,8 +9,12 @@ d[:m] = [1,2,3]
 s = Sample(1, d)
 
 c2 = Chain(1, Vector{Sample}([s]))
-@test string(c2) == "Chain, model evidence (log)  1.0 and means Dict{Symbol,Any}(:m=>[1.0,2.0,3.0])"
 
+if VERSION >= v"0.5"
+  @test string(c2) == "Chain, model evidence (log)  1.0 and means Dict(:m=>[1.0,2.0,3.0])"
+else
+  @test string(c2) == "Chain, model evidence (log)  1.0 and means Dict{Symbol,Any}(:m=>[1.0,2.0,3.0])"
+end
 samples = c2[:samples]
 @test samples[1][:m] == d[:m]
 
