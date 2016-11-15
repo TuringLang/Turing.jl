17a832f044438bcf9eb3b73411625be1a99e460c
Kai Xu <xukai921110@gmail.com>
2016-11-15 21:27:12 +0000

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -158,7 +158,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = juliadoc.default_sidebars()
+# html_sidebars = juliadoc.default_sidebars()
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
