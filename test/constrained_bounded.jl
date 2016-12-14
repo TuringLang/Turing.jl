a66c2ff8f0528c9f8794ee244d1f97b0501c9286
Kai Xu <xukai921110@gmail.com>
2016-12-14 12:30:23 +0000

    Update test

--- test/constrained_bounded.jl
+++ test/constrained_bounded.jl
@@ -12,5 +12,5 @@ obs = [0,1,0,1,1,1,1,1,1,1]
   @predict p
 end
 
-chain = sample(constrained_test, HMC(3000, 0.03, 3))  # using a large step size (1.5)
-@test_approx_eq_eps mean(chain[:p]) 10/14 0.10        # 0.716 is from SMC(10000)
+chain = sample(constrained_test, HMC(3000, 1.5, 3)) # using a large step size (1.5)
+@test_approx_eq_eps mean(chain[:p]) 10/14 0.10      # 0.716 is from SMC(10000)
