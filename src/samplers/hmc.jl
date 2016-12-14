27506f05838e932af7144daff9d9d69f9b501f8c
Kai Xu <xukai921110@gmail.com>
2016-12-14 21:11:21 +0000

    split find_logjoint and refactor

--- src/samplers/hmc.jl
+++ src/samplers/hmc.jl
@@ -26,13 +26,13 @@ immutable HMC <: InferenceAlgorithm
 end
 
 type HMCSampler{HMC} <: GradientSampler{HMC}
-  alg         :: HMC
-  model       :: Function
-  values      :: GradientInfo
-  dists       :: Dict{VarInfo, Distribution}
-  samples     :: Array{Sample}
-  predicts    :: Dict{Symbol, Any}
-  first       :: Bool
+  alg         :: HMC                          # the HMC algorithm info
+  model       :: Function                     # model function
+  values      :: GradientInfo                 # container for variables
+  dists       :: Dict{VarInfo, Distribution}  # dictionary from variables to the corresponding distributions
+  samples     :: Array{Sample}                # samples
+  predicts    :: Dict{Symbol, Any}            # outputs
+  first       :: Bool                         # the first run flag
 
   function HMCSampler(alg :: HMC, model :: Function)
     samples = Array{Sample}(alg.n_samples)
@@ -41,14 +41,14 @@ type HMCSampler{HMC} <: GradientSampler{HMC}
       samples[i] = Sample(weight, Dict{Symbol, Any}())
     end
     predicts = Dict{Symbol, Any}()
-    values = GradientInfo()   # GradientInfo Initialize logjoint as Dual(0)
+    values = GradientInfo()   # GradientInfo initialize logjoint as Dual(0)
     dists = Dict{VarInfo, Distribution}()
     new(alg, model, values, dists, samples, predicts, true)
   end
 end
 
 function Base.run(spl :: Sampler{HMC})
-  # Function to make half momentum step
+  # Half momentum step
   function half_momentum_step(p, val∇E)
     dprintln(5, "half_momentum_step...")
     for k in keys(p)
@@ -56,80 +56,81 @@ function Base.run(spl :: Sampler{HMC})
     end
     return p
   end
+
+  # Find logjoint
+  # NOTE: it returns logjoint but not -logjoint
+  function find_logjoint()
+    consume(Task(spl.model))
+    logjoint = spl.values.logjoint
+    spl.values.logjoint = Dual(0)
+    return logjoint
+  end
+
   # Run the model for the first time
   dprintln(2, "initialising...")
-  consume(Task(spl.model))
-  spl.values.logjoint = Dual(0)
+  find_logjoint()
   spl.first = false
+
   # Store the first predicts
   spl.samples[1].value = deepcopy(spl.predicts)
+
+  # Set parameters
   n = spl.alg.n_samples
   ϵ = spl.alg.lf_size
   τ = spl.alg.lf_num
-  accept_num = 1
+  accept_num = 1        # the first samples is always accepted
+
   # Sampling
   for i = 2:n
+    dprintln(3, "stepping...")
+
+    # Initialization
     has_run = false
     oldH = 0
     H = 0
+
     # Record old state
     old_values = deepcopy(spl.values)
-    # Run the step until successful
-    while has_run == false
-      dprintln(3, "stepping...")
-      # Assume the step is successful
-      has_run = true
-      # Generate random momentum
-      p = Dict{Any, Any}()
-      for k in keys(spl.values)
-        p[k] = randn(length(spl.values[k]))
-      end
-      # Record old Hamiltonian
-      dprintln(4, "old H...")
-      for k in keys(p)
-        oldH += p[k]' * p[k] / 2
-      end
-      consume(Task(spl.model))
-      oldH += realpart(-spl.values.logjoint)
-      spl.values.logjoint = Dual(0)
-      # Get gradient dict
-      dprintln(4, "first gradient...")
-      val∇E = get_gradient_dict(spl.values, spl.model)
-      dprintln(4, "leapfrog...")
-      # 'leapfrog' for each var
-      for t in 1:τ
-        p = half_momentum_step(p, val∇E)
-        # Make a full step for state
-        for k in keys(spl.values)
-          # # X -> R and move
-          # dprintln(5, "reconstruct...")
-          # real = reconstruct(spl.dists[k], spl.values[k])
-          # dprintln(5, "X -> R...")
-          # real = link(spl.dists[k], real)
-          # dprintln(5, "move...")
-          # real += ϵ * reconstruct(spl.dists[k], p[k])
-          # real = length(real) == 1 ? real[1] : real       # Array{T}[1] → T for invlink()
-          # # R -> X and store
-          # dprintln(5, "R -> X...")
-          # spl.values[k] = vectorize(spl.dists[k], invlink(spl.dists[k], real))
-          spl.values[k] = Array{Any}(spl.values[k] + ϵ * p[k])
-        end
-        val∇E = get_gradient_dict(spl.values, spl.model)
-        p = half_momentum_step(p, val∇E)
-      end
-      # Claculate the new Hamiltonian
-      dprintln(4, "new H...")
-      for k in keys(p)
-        H += p[k]' * p[k] / 2
-      end
-      consume(Task(spl.model))
-      H += realpart(-spl.values.logjoint)
-      spl.values.logjoint = Dual(0)
+
+    # Generate random momentum
+    p = Dict{Any, Any}()
+    for k in keys(spl.values)
+      p[k] = randn(length(spl.values[k]))
     end
+
+    # Record old Hamiltonian
+    dprintln(4, "old H...")
+    for k in keys(p)
+      oldH += p[k]' * p[k] / 2
+    end
+    oldH += realpart(-find_logjoint())
+
+    # Get gradient dict
+    dprintln(4, "first gradient...")
+    val∇E = get_gradient_dict(spl.values, spl.model)
+
+    # Do 'leapfrog' for each var
+    dprintln(4, "leapfrog...")
+    for t in 1:τ
+      p = half_momentum_step(p, val∇E)  # half step for momentum
+      for k in keys(spl.values)         # full step for state
+        spl.values[k] = Array{Any}(spl.values[k] + ϵ * p[k])
+      end
+      val∇E = get_gradient_dict(spl.values, spl.model)
+      p = half_momentum_step(p, val∇E)  # half step for momentum
+    end
+
+    # Claculate the new Hamiltonian
+    dprintln(4, "new H...")
+    for k in keys(p)
+      H += p[k]' * p[k] / 2
+    end
+    H += realpart(-find_logjoint())
+
     # Calculate the difference in Hamiltonian
     ΔH = H - oldH
-    # Vector{Any, 1} -> Any
-    ΔH = ΔH[1]
+    ΔH = ΔH[1]  # Vector{Any, 1} -> Any
+
     # Decide wether to accept or not
     if ΔH < 0
       acc = true
@@ -138,28 +139,24 @@ function Base.run(spl :: Sampler{HMC})
     else
       acc = false
     end
-    # Rewind of rejected
-    if ~acc
+
+    if ~acc # rejected => store the previous predcits
       spl.values = old_values
-      # Store the previous predcits
       spl.samples[i] = spl.samples[i - 1]
-    else
-      # Store the new predcits
+    else    # accepted => store the new predcits
       spl.samples[i].value = deepcopy(spl.predicts)
       accept_num += 1
     end
   end
-  # Wrap the result by Chain
-  results = Chain(0, spl.samples)
-  accept_rate = accept_num / n
+
+  accept_rate = accept_num / n    # calculate the accept rate
   println("[HMC]: Finshed with accept rate = $(accept_rate)")
-  return results
+  return Chain(0, spl.samples)    # wrap the result by Chain
 end
 
 function assume(spl :: HMCSampler{HMC}, dist :: Distribution, var :: VarInfo)
-  dprintln(2, "assuming...")
   # Step 1 - Generate or replay variable
-
+  dprintln(2, "assuming...")
   if spl.first  # first time -> generate
     # Build {var -> dist} dictionary
     spl.dists[var] = dist
@@ -209,7 +206,7 @@ function predict(spl :: HMCSampler{HMC}, name :: Symbol, value)
   dprintln(2, "predict done")
 end
 
-sample(model :: Function, alg :: HMC) = (
-                                        global sampler = HMCSampler{HMC}(alg, model);
-                                        run(sampler)
-                                        )
+function sample(model :: Function, alg :: HMC)
+  global sampler = HMCSampler{HMC}(alg, model);
+  run(sampler)
+end
