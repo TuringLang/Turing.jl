db6ec38127553523f0d543404fd8aea79388b772
Kai Xu <xukai921110@gmail.com>
2016-12-22 18:55:30 +0000

    Update test for new grammar

--- test/ad2.jl
+++ test/ad2.jl
@@ -3,13 +3,13 @@ using Turing
 using Base.Test
 
 # Define model
-@model ad_test begin
-  @assume s ~ InverseGamma(2,3)
-  @assume m ~ Normal(0,sqrt(s))
-  @observe 1.5 ~ Normal(m, sqrt(s))
-  @observe 2.0 ~ Normal(m, sqrt(s))
+@model ad_test2 begin
+  s ~ InverseGamma(2,3)
+  m ~ Normal(0,sqrt(s))
+  1.5 ~ Normal(m, sqrt(s))
+  2.0 ~ Normal(m, sqrt(s))
   @predict s m
 end
 
 # Run HMC with chunk_size=1
-chain = sample(ad_test, HMC(1, 0.1, 1); chunk_size=1)
+chain = sample(ad_test2, HMC(100, 0.1, 1); chunk_size=1)
--- test/assume.jl
+++ test/assume.jl
@@ -4,22 +4,21 @@ using Turing
 using Distributions
 using Base.Test
 
-@model testassume begin
-  @assume x ~ Bernoulli(1)
-  @assume y ~ Bernoulli(x / 2)
-  @predict y
-  @predict x
+@model test_assume begin
+  x ~ Bernoulli(1)
+  y ~ Bernoulli(x / 2)
+  @predict x y
 end
 
 s = SMC(10000)
 p = PG(10,1000)
 
-res = sample(testassume, s)
+res = sample(test_assume, s)
 
 @test reduce(&, res[:x]) == 1 # check that x is always 1
 @test_approx_eq_eps mean(res[:y]) 0.5 0.1 # check that the mean of y is between 0.4 and 0.6
 
-res = sample(testassume, p)
+res = sample(test_assume, p)
 
 @test reduce(&, res[:x]) == 1 # check that x is always 1
 @test_approx_eq_eps mean(res[:y]) 0.5 0.1 # check that the mean of y is between 0.4 and 0.6
