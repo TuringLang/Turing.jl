065ed589ff81e0344fb61591865d59922cc99a33
Kai Xu <xukai921110@gmail.com>
2016-12-22 13:39:16 +0000

    Add test for constrained abitary (a, b) bounded univar

--- test/transform.jl
+++ test/transform.jl
@@ -2,7 +2,7 @@ using Turing: link, invlink
 using Base.Test
 using Distributions
 
-dists = [Beta(2,2), Dirichlet(2, 3), Wishart(7, [1 0.5; 0.5 1])]
+dists = [Arcsine(2, 4), Beta(2,2), Dirichlet(2, 3), Wishart(7, [1 0.5; 0.5 1])]
 
 for dist in dists
   x = rand(dist)    # sample
