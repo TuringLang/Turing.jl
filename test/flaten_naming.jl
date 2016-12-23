488300c06c1ef141b6d100ad5e1e79d994b4e6da
Kai Xu <xukai921110@gmail.com>
2016-12-23 03:58:45 +0000

    (x[1])[2] => x[1][2]

--- test/flaten_naming.jl
+++ test/flaten_naming.jl
@@ -10,11 +10,11 @@ v_sym = VarInfo(:x)
 
 # Array
 v_arr = VarInfo(:(x[i]), :i, 1)
-@test v_arr.id == Symbol(:(x[1]))
+@test v_arr.id == Symbol("x[1]")
 
 # Matrix
 v_mat = VarInfo(:(x[i,j]), :i, 1, :j, 2)
-@test v_mat.id == Symbol(:(x[1,2]))
+@test v_mat.id == Symbol("x[1,2]")
 
 @model mat_name_test begin
   p = Array{Dual}((2, 2))
@@ -28,7 +28,7 @@ chain = sample(mat_name_test, HMC(2500, 0.75, 5))
 
 # Multi array
 v_arrarr = VarInfo(:(x[i][j]), :i, 1, :j, 2)
-@test v_arrarr.id == Symbol(:((x[1])[2]))
+@test v_arrarr.id == Symbol("x[1][2]")
 
 @model marr_name_test begin
   p = Array{Array{Dual}}(2)
