19d7077e61b856867d75a871776e8521e8ebaad9
Kai Xu <xukai921110@gmail.com>
2016-12-27 16:28:39 +0000

    Change PG log

--- src/samplers/pgibbs.jl
+++ src/samplers/pgibbs.jl
@@ -30,6 +30,7 @@ immutable PG <: InferenceAlgorithm
 end
 
 function Base.run(spl::Sampler{PG})
+  t_start = time()  # record the start time of PG
   chain = Chain()
   logevidence = Vector{Float64}(spl.alg.n_iterations)
 
@@ -37,7 +38,6 @@ function Base.run(spl::Sampler{PG})
   ## re-inserts reteined particle after each resampling step
   ref_particle = nothing
   for tt = 1:spl.alg.n_iterations
-    mod(tt,10) == 0 && dprintln(1, "[PG]: Iter $(tt) out of $(spl.alg.n_iterations)")
     spl.particles = ParticleContainer{TraceR}(spl.model)
     if ref_particle == nothing
       push!(spl.particles, spl.alg.n_particles)
@@ -63,5 +63,6 @@ function Base.run(spl::Sampler{PG})
     push!(chain, Sample(1/spl.alg.n_iterations, s.value))
   end
   chain.weight = exp(mean(logevidence))
+  println("[PG]: Finshed within $(time() - t_start) seconds")
   chain
 end
