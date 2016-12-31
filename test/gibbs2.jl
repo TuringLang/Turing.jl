c83033a3c45f67955a36f6f961fa8a90a5ef9521
Kai Xu <xukai921110@gmail.com>
2016-12-31 15:59:58 +0000

    Update test

--- test/gibbs2.jl
+++ test/gibbs2.jl
@@ -2,32 +2,49 @@ using Distributions
 using Turing
 using Base.Test
 
-x = [1.0 1.0 1.0 4.0 4.0 4.0]
+D = [1.0 1.0 4.0 4.0]
 
 @model MoGtest begin
-  N = length(D)
-  mu0 ~ Normal(0, 16)
-  mu1 ~ Normal(5, 16)
-  z = tzeros(N)
-  for i in 1:N
-    z[i] ~ Bernoulli(0.5)
-    if z[i] == 0
-      x[i] ~ Normal(mu0, 1)
-    else
-      x[i] ~ Normal(mu1, 1)
-    end
+  mu1 ~ Normal(1, 1)
+  mu2 ~ Normal(4, 1)
+  z1 ~ Categorical(2)
+  if z1 == 1
+    D[1] ~ Normal(mu1, 1)
+  else
+    D[1] ~ Normal(mu2, 1)
   end
-  z, mu0, mu1
+  z2 ~ Categorical(2)
+  if z2 == 1
+    D[2] ~ Normal(mu1, 1)
+  else
+    D[2] ~ Normal(mu2, 1)
+  end
+  z3 ~ Categorical(2)
+  if z3 == 1
+    D[3] ~ Normal(mu1, 1)
+  else
+    D[3] ~ Normal(mu2, 1)
+  end
+  z4 ~ Categorical(2)
+  if z4 == 1
+    D[4] ~ Normal(mu1, 1)
+  else
+    D[4] ~ Normal(mu2, 1)
+  end
+  z1, z2, z3, z4, mu1, mu2
 end
 
-gibbs = Gibbs(1500, PG(15, 1, :z), HMC(1, 0.25, 3, :mu0, :mu1))
+gibbs = Gibbs(1500, PG(15, 1, :z1, :z2, :z3, :z4), HMC(1, 0.25, 3, :mu1, :mu2))
 chain = sample(MoGtest, gibbs)
 
 Turing.TURING[:modelex]
 
-mean(chain[:z])
-mean(chain[:mu0])
+mean(chain[:z1])
+mean(chain[:z2])
+mean(chain[:z3])
+mean(chain[:z4])
 mean(chain[:mu1])
+mean(chain[:mu2])
 
 # @test_approx_eq_eps mean(chain[:s]) 49/24 0.15
 # @test_approx_eq_eps mean(chain[:m]) 7/6 0.15
