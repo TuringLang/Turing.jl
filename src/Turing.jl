445a23ea291aad613ff24774c0d44c07e4fc779f
Kai Xu <xukai921110@gmail.com>
2017-01-18 10:47:06 +0000

    Remove hackcode for StatsFun.jl

--- src/Turing.jl
+++ src/Turing.jl
@@ -8,19 +8,6 @@ import Base.~                       # to orverload @~
 using ForwardDiff: Dual, npartials  # for automatic differentiation
 using Turing.Traces
 
-###########
-# Warning #################################
-# The following overloadings is temporary #
-# before StatsFuns.jl accepts our PR.     #
-
-using StatsFuns
-StatsFuns.betapdf(α::Real, β::Real, x::Real) = x^(α - 1) * (1 - x)^(β - 1) / beta(α, β)
-StatsFuns.betalogpdf(α::Real, β::Real, x::Real) = (α - 1) * log(x) + (β - 1) * log(1 - x) - log(beta(α, β))
-StatsFuns.gammapdf(k::Real, θ::Real, x::Real) = 1 / (gamma(k) * θ^k) * x^(k - 1) * exp(-x / θ)
-StatsFuns.gammalogpdf(k::Real, θ::Real, x::Real) = -log(gamma(k)) - k * log(θ) + (k - 1) * log(x) - x / θ
-
-###########################################
-
 #################
 # Turing module #
 #################
