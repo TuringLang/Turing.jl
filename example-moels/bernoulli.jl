ba4b8c99acb9635ba43819992ccc8ae73d2052fb
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-02-23 22:54:18 +0000

    Updated examples.

--- example-moels/bernoulli.jl
+++ example-moels/bernoulli.jl
@@ -2,7 +2,7 @@ using Turing
 using Distributions
 using Base.Test
 
-@model bernoulli(y) begin
+@model bernoulli() begin
  theta ~ Beta(1,1)
  for n = 1:length(y)
    y[n] ~ Bernoulli(theta)
@@ -10,6 +10,9 @@ using Base.Test
  return theta
 end
 
+
+data = Dict(:y => [0,1,0,0,0,0,0,0,0,1])
+sample(bernoulli, data, HMC(1000, 0.1, 5))
 
 "
 data {
--- example-moels/negative_binomial.jl
+++ example-moels/negative_binomial.jl
@@ -2,14 +2,20 @@ using Turing
 using Distributions
 using Base.Test
 
-@model negative_binomial(y) begin
- alpha ~ Cauthy(0,10)
- beta ~ Cauthy(0,10)
+@model negative_binomial() begin
+ alpha ~ Cauchy(0,10)
+ beta ~ Cauchy(0,10)
  for i = 1:length(y)
    y[i] ~ NegativeBinomial(alpha, beta)
  end
  return alpha, beta
 end
+
+
+# Produce an error.
+data = Dict(:y => [0, 1, 4, 0, 2, 2, 5, 0, 1])
+sample(negative_binomial, data, HMC(1000, 0.1, 5))
+
 
 "data {
   int<lower=1> N;
