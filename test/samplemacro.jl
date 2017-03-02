dc83d0189f920e7dddd811e187cf35aa8bb20259
Kai Xu <xukai921110@gmail.com>
2017-03-02 10:54:01 +0000

    Rename model

--- test/samplemacro.jl
+++ test/samplemacro.jl
@@ -1,7 +1,7 @@
 using Distributions
 using Turing
 
-@model gdemo(x) = begin
+@model gdemo2(x) = begin
   s ~ InverseGamma(2,3)
   m ~ Normal(0,sqrt(s))
   for i = 1:length(x)
@@ -15,7 +15,7 @@ x = [2.0, 3.0]
 alg = Gibbs(1000, HMC(1, 0.2, 3, :m), PG(10, 1, :s))
 # NOTE: want to translate below to
 #       chn = sample(gdemo, Dict(:x => x), alg)
-chn = @sample(gdemo(x), alg);
+chn = @sample(gdemo2(x), alg);
 mean(chn[:s])
 
 Turing.TURING[:modelex]
