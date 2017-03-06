fed00b00d7405ae8cca78ffdfca66648b8bd10d6
Kai Xu <xukai921110@gmail.com>
2017-03-06 15:01:53 +0000

    Change profile print setting

--- example-models/negative-binomial/profile.jl
+++ example-models/negative-binomial/profile.jl
@@ -4,16 +4,9 @@ using Base.Test
 
 Profile.clear()
 
-p() = begin
+include("data.jl")
+include("model.jl")
 
-  include("data.jl")
-  include("model.jl")
+@profile @sample(negbinmodel(negbindata), HMC(1000, 0.02, 1));
 
-  # Produce an error.
-  @sample(negbinmodel(negbindata), HMC(1000, 0.02, 1));
-end
-
-@profile p()
-
-f = open("profile_res.txt","w")
-Profile.print(f)
+Profile.print(maxdepth = 13)
