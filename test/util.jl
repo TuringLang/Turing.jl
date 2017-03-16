9fc37aaca8c2a9d3c748f6c91985bdae1fe6fb49
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-03-16 19:53:18 +0000

    Fix test util.jl

--- test/util.jl
+++ test/util.jl
@@ -2,8 +2,9 @@ using Turing: getvarid, invlogit, logit, randcat, kl, align
 using Distributions: Normal
 using Base.Test
 
-@test getvarid(:s) == ":(s)"
-@test getvarid(parse("x = 1")) == ":(x), 1"
+@test getvarid(:s) == :s
+@test getvarid(:(x[1,2][1+5][45][3])) == "x[1,2][6][45][3]"
+# @test getvarid(:(x[1,2][1+5][45][3][i])) == "x[1,2][6][45][3][i]" 
 @test invlogit(1.1) == 1.0 / (exp(-1.1) + 1.0)
 @test logit(0.3) == log(0.3 / (1.0 - 0.3))
 randcat([0.1, 0.9])
