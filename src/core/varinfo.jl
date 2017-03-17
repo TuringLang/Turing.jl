c373f6ddbef7d9ee1e1d391f6c4848396d8dd7a9
Kai Xu <xukai921110@gmail.com>
2017-03-17 13:36:50 +0000

    Remove Var and update VarInfo

--- src/core/varinfo.jl
+++ src/core/varinfo.jl
@@ -1,117 +1,25 @@
-export Var, VarInfo
-
-########## Var ##########
-
-doc"""
-    Var(sym)
-
-A wrapper of symbol type representing priors.
-
-  - type is encoded in the form of [is_uni?, is_multi?, is_matrix?]
-
-Usage:
-
-```julia
-p = Var(:somesym)
-strp = string(p)
-```
-"""
-immutable Var
-  sym   ::    Symbol
-  uid   ::    Symbol
-  Var(sym::Symbol) = new(sym, sym)
-  Var(sym::Symbol, uid::Symbol) = new(sym, uid)
-end
-
-doc"""
-    Base.string(p::Var)
-
-Helper function to convert a Var to its string representation.
-"""
-function Base.string(v::Var)
-  return string(v.uid)
-end
-
 ########## VarInfo ##########
 
-doc"""
-    VarInfo()
-
-A values to store priors based on dictionary.
-
-This type is basically a dictionary supporting adding new priors by creating a VarArray and indexing using pc[] syntax.
-
-Usage:
-
-```julia
-pc = VarInfo()
-p1 = Var(:a)
-p2 = Var(:b)
-
-addVar(pc, p1, 1)
-addVar(pc, p1, 2)
-addVar(pc, p1, 3)
-addVar(pc, p2, 4)
-
-pc[p1]    # 1
-pc[p1]    # 2
-pc[p1]    # 3
-pc[p1]    # 1
-pc[p1]    # 2
-pc[p1]    # 3
-
-pc[p2]    # 4
-
-pc[p1] = 5
-pc[p1] = 6
-pc[p1] = 7
-
-pc[p1]    # 5
-pc[p1]    # 6
-pc[p1]    # 7
-
-keys(pc)  # create a key interator in the values, i.e. all the priors
-```
-"""
 type VarInfo
-  values      ::    Dict{Var, Any}
-  dists       ::    Dict{Var, Distribution}
+  vals        ::    Dict{String, Any}
+  syms        ::    Dict{String, Symbol}
+  dists       ::    Dict{String, Distribution}
   logjoint    ::    Dual
-  VarInfo() = new(Dict{Var, Any}(), Dict{Var, Distribution}(), Dual(0))
+  VarInfo() = new(
+    Dict{String, Any}(),
+    Dict{String, Symbol}(),
+    Dict{String, Distribution}(),
+    Dual(0)
+  )
 end
 
-doc"""
-    Base.getindex(pc::VarInfo, idx::Var)
+Base.getindex(vi::VarInfo, uid::String) = vi.vals[uid]
 
-Make the prior values support indexing with `[]`.
-"""
-function Base.getindex(pc::VarInfo, idx::Var)
-  @assert haskey(pc.values, idx) "VarInfo has no $idx."
-  return pc.values[idx]
-end
+# The default setindex!() for VarInfo is to set values
+Base.setindex!(vi::VarInfo, val, uid::String) = vi.vals[uid] = val
 
-doc"""
-    Base.setindex!(pc::VarInfo, val, idx::Var)
+Base.keys(vi::VarInfo) = keys(vi.vals)
 
-Make the prior values support assignment with `[]`.
-"""
-function Base.setindex!(pc::VarInfo, val, idx::Var)
-  @assert haskey(pc.values, idx) "VarInfo has no $idx."
-  pc.values[idx] = val
-end
+syms(vi::VarInfo) = Set(values(vi.syms))
 
-doc"""
-    Base.keys(vi::VarInfo)
-
-Return a key interator in the values, i.e. all the priors.
-"""
-Base.keys(vi::VarInfo) = keys(vi.values)
-
-doc"""
-    syms(vi::VarInfo)
-
-Return a set of all symbols
-"""
-syms(vi::VarInfo) = Set(map(v -> v.sym, keys(vi)))
-
-export syms
+export VarInfo, syms
