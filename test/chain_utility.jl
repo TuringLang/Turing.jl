020a26a25ef879083982d0e4a12149510a3fe297
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-03-21 19:02:49 +0000

    Cleanup tests.

--- test/chain_utility.jl
+++ test/chain_utility.jl
@@ -15,3 +15,20 @@ samples = c2[:samples]
 @test samples[1][:m] == d[:m]
 
 @test mean(c2, :m, x -> x) == [1.0, 2.0, 3.0]
+
+
+#  Tests for Mamba Chain
+
+@model mamba_chain_test() = begin
+  m = rand()
+  x = rand(2,2)
+  y = Array{Array}(2,2)
+  for i in eachindex(y)
+    y[i] = rand(2,2)
+  end
+  return(m, x, y)
+end
+
+chain = @sample(mamba_chain_test(), PG(5,300));
+sim1 = MambaChains(chain)
+describe(sim1)
