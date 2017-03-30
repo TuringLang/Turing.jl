557d540fa72b2dc9e9c270ec365cf426e4ebb6a4
Kai Xu <xukai921110@gmail.com>
2017-03-30 12:23:58 +0100

    Update test for VarInfo and VarName

--- test/varinfo.jl
+++ test/varinfo.jl
@@ -5,7 +5,7 @@ using Turing: uid, cuid
 csym = gensym()
 vn1 = VarName(csym, :x, "[1]", 1)
 @test string(vn1) == "{$csym,x[1]}:1"
-print(string(vn1))
+# println(string(vn1))
 
 vn2 = VarName(csym, :x, "[1]", 2)
 vn11 = VarName(csym, :x, "[1]", 1)
