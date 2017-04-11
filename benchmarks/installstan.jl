2bdb006ce3feb50807f67e7f47d079e4b6d79efd
Kai Xu <xukai921110@gmail.com>
2017-04-11 15:12:36 +0100

    Update Travis script

--- benchmarks/installstan.jl
+++ benchmarks/installstan.jl
@@ -1,7 +1,7 @@
 run(`wget https://github.com/stan-dev/cmdstan/releases/download/v2.14.0/cmdstan-2.14.0.zip`)
-run(`unzip -qq cmdstan-2.14.0.zip -d cmdstan`)
-CMDSTAN_HOME = pwd() * "/cmdstan"
-Pkg.add("Homebrew")
-Pkg.add("DualNumbers")
+run(`unzip -qq cmdstan-2.14.0.zip`)
+CMDSTAN_HOME = pwd() * "/cmdstan-2.14.0"
+println("CMDStan is installed at path: $CMDSTAN_HOME")
+@osx Pkg.add("Homebrew")
 Pkg.add("Stan")
 Pkg.build("Stan")
