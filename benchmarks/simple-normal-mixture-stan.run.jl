de0260a129b6fe6065c696fc8dbcb7041d2dd481
Kai Xu <xukai921110@gmail.com>
2017-04-11 16:09:30 +0100

    Rename data variable

--- benchmarks/simple-normal-mixture-stan.run.jl
+++ benchmarks/simple-normal-mixture-stan.run.jl
@@ -5,5 +5,5 @@ include("simple-normal-mixture-stan.data.jl")
 
 simplenormalmixturestan = Stanmodel(name="normalmixture", model=simplenormalmixturemodel, nchains=1);
 
-sim1 = stan(simplenormalmixturestan, simplenormalmixturedata, CmdStanDir=CMDSTAN_HOME)
+sim1 = stan(simplenormalmixturestan, simplenormalmixturestandata, CmdStanDir=CMDSTAN_HOME)
 describe(sim1)
