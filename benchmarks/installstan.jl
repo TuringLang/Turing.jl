be6f2c792bf06ab98e89fb9d9db64d74caeaeb13
Kai Xu <xukai921110@gmail.com>
2017-04-11 15:23:06 +0100

    Remove adding Homebrew as we don't run bench on OSX

--- benchmarks/installstan.jl
+++ benchmarks/installstan.jl
@@ -2,6 +2,5 @@ run(`wget https://github.com/stan-dev/cmdstan/releases/download/v2.14.0/cmdstan-
 run(`unzip -qq cmdstan-2.14.0.zip`)
 CMDSTAN_HOME = pwd() * "/cmdstan-2.14.0"
 println("CMDStan is installed at path: $CMDSTAN_HOME")
-@osx Pkg.add("Homebrew")
 Pkg.add("Stan")
 Pkg.build("Stan")
