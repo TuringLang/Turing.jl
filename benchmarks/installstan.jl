64ce2aaec9554229583c490155e7a89c3c4ba8ca
Kai Xu <xukai921110@gmail.com>
2017-04-11 14:08:15 +0100

    Update Travis script

--- benchmarks/installstan.jl
+++ benchmarks/installstan.jl
@@ -1,5 +1,7 @@
 run(`wget https://github.com/stan-dev/cmdstan/releases/download/v2.14.0/cmdstan-2.14.0.zip`)
-run(`unzip cmdstan-2.14.0.zip -d cmdstan`)
+run(`unzip -qq cmdstan-2.14.0.zip -d cmdstan`)
 CMDSTAN_HOME = pwd() * "/cmdstan"
+Pkg.add("Homebrew")
+Pkg.add("DualNumbers")
 Pkg.add("Stan")
 Pkg.build("Stan")
