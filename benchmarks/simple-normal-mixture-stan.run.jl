627239aff03ed0bf8583f2d2fcbfc4d3281ce16b
Kai Xu <xukai921110@gmail.com>
2017-04-12 20:06:27 +0100

    Disable summary

--- benchmarks/simple-normal-mixture-stan.run.jl
+++ benchmarks/simple-normal-mixture-stan.run.jl
@@ -1,9 +1,11 @@
+using Stan
+
 include("simple-normal-mixture-stan.model.jl")
 include("simple-normal-mixture-stan.data.jl")
 
 simplenormalmixturestan = Stanmodel(name="normalmixture", model=simplenormalmixturemodel, nchains=1);
 
-nm_stan_sim = stan(simplenormalmixturestan, Dict("N"=>100, "y"=>simplenormalmixturestandata[1]["y"][1:100]), CmdStanDir=CMDSTAN_HOME)
+nm_stan_sim = stan(simplenormalmixturestan, Dict("N"=>100, "y"=>simplenormalmixturestandata[1]["y"][1:100]), CmdStanDir=CMDSTAN_HOME, summary=false)
 # describe(nm_stan_sim)
 
 nm_theta = nm_stan_sim[1:1000, ["theta"], :].value[:]
