e08dd5025ab035ebea97a00ae5b6db0e246ad396
Kai Xu <xukai921110@gmail.com>
2017-04-13 15:37:27 +0100

    Bugfix

--- benchmarks/simple-normal-mixture.run.jl
+++ benchmarks/simple-normal-mixture.run.jl
@@ -2,7 +2,7 @@ include("simple-normal-mixture.data.jl")
 include("simple-normal-mixture.model.jl")
 
 # NOTE: I only run a sub-set of the data as running the whole is quite slow
-bench_res = tbenchmark("Gibbs(1000, PG(20, 1, :k), HMC(1, 0.25, 1, :theta), HMC(1, 2.0, 3, :mu))", "nmmodel", "y[1:100]")
+bench_res = tbenchmark("Gibbs(1000, PG(20, 1, :k), HMC(1, 2.0, 3, :theta, :mu))", "nmmodel", "y[1:100]")
 logd = build_logd("Simple Gaussian Mixture Model", bench_res...)
 
 logd["stan"] = Dict("theta" => mean(nm_theta), "mu[1]" => mean(nm_mu_1), "mu[2]" =>mean(nm_mu_2))
