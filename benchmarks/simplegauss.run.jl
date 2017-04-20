81259803e4fc6bf812e65c98635591e22f351429
Hong Ge <hg344@cam.ac.uk>
2017-04-20 19:14:02 +0100

    Change sampler to HMC for simplegauss.jl

--- benchmarks/simplegauss.run.jl
+++ benchmarks/simplegauss.run.jl
@@ -1,7 +1,7 @@
 include("simplegauss.data.jl")
 include("simplegauss.model.jl")
 
-bench_res = tbenchmark("Gibbs(1000, PG(100, 3, :s), HMC(2, 0.1, 3, :m))", "simplegaussmodel", "simplegaussdata")
+bench_res = tbenchmark("Gibbs(1000, HMC(2, 0.1, 3))", "simplegaussmodel", "simplegaussdata")
 logd = build_logd("Simple Gaussian Model", bench_res...)
 logd["analytic"] = Dict("s" => 49/24, "m" => 7/6)
 
