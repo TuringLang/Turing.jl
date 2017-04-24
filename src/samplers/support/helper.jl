92e67541d5d08763df10ca0397a8306791ca533a
Kai Xu <xukai921110@gmail.com>
2017-04-24 16:44:12 +0100

    Improve typing

--- src/samplers/support/helper.jl
+++ src/samplers/support/helper.jl
@@ -8,16 +8,16 @@ realpart(d::Array) = map(x -> realpart(x), d)
 dualpart(d::Dual)  = d.partials.values
 dualpart(d::Array) = map(x -> x.partials.values, d)
 
-Base.promote_rule(D1::Type{Float64}, D2::Type{Dual}) = D2
+Base.promote_rule(D1::Type{Real}, D2::Type{Dual}) = D2
 
 #####################################################
 # Helper functions for vectorize/reconstruct values #
 #####################################################
 
 # QUES: will use Any below lead to worse performance?
-vectorize(d::UnivariateDistribution,   r) = Vector{Any}([r])
-vectorize(d::MultivariateDistribution, r) = Vector{Any}(r)
-vectorize(d::MatrixDistribution,       r) = Vector{Any}(vec(r))
+vectorize(d::UnivariateDistribution,   r) = Vector{Real}([r])
+vectorize(d::MultivariateDistribution, r) = Vector{Real}(r)
+vectorize(d::MatrixDistribution,       r) = Vector{Real}(vec(r))
 
 function reconstruct(d::Distribution, val)
   if isa(d, UnivariateDistribution)
