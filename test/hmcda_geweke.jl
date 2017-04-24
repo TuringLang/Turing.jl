c460fef267ac28d4e40b78d84f98c08c7deca194
Kai Xu <xukai921110@gmail.com>
2017-04-24 16:49:11 +0100

    Add plot for HMCDA geweke

--- test/hmcda_geweke.jl
+++ test/hmcda_geweke.jl
@@ -55,8 +55,11 @@ s2 = vcat(s_bk...);
 # describe(s2)
 
 
-qqplot(s[:m], s2[:m])
-qqplot(s[:s], s2[:s])
+using UnicodePlots
+
+qqm = qqbuild(s[:m], s2[:m])
+show(scatterplot(qqm.qx, qqm.qy, title = "QQ plot for m", canvas = DotCanvas))
+show(scatterplot(qqm.qx[51:end-50], qqm.qy[51:end-50], title = "QQ plot for m (removing first and last 50 quantiles):", canvas = DotCanvas))
 
 qqm = qqbuild(s[:m], s2[:m])
 X = qqm.qx
