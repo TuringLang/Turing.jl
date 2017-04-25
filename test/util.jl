0ca6b8916ee48656d44df4790150c6a5623194b7
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-04-25 14:12:54 +0100

    Fix `util.jl` test.

--- test/util.jl
+++ test/util.jl
@@ -7,7 +7,7 @@ i = 1
 @test @VarName(s)[1:end-1] == (:s,)
 @test @VarName(x[1,2][1+5][45][3][i])[1:end-1] == (:x,([1,2],[6],[45],[3],[1]))
 @test invlogit(1.1) == 1.0 / (exp(-1.1) + 1.0)
-@test_approx_eq_eps logit(0.3) == -0.8472978603872036 1e-9
+@test_approx_eq_eps logit(0.3) -0.8472978603872036 1e-9
 @test isnan(logit(1.0)) == false
 @test isinf(logit(1.0)) == false
 @test isnan(logit(0.)) == false
