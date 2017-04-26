99e41b82a2db77ac8a170d7ae266897431edab13
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-04-26 18:22:50 +0100

    Increase num samples for naive bayes.

--- benchmarks/naive.bayes.run.jl
+++ benchmarks/naive.bayes.run.jl
@@ -4,7 +4,7 @@ include("benchmarkhelper.jl")
 include("naive.bayes-stan.data.jl")
 include("naive.bayes.model.jl")
 
-bench_res = tbenchmark("HMC(1000, 0.1, 3)", "nbmodel", "data=nbstandata[1]")
+bench_res = tbenchmark("HMC(2000, 0.01, 10)", "nbmodel", "data=nbstandata[1]")
 bench_res[4].names = ["phi[1]", "phi[2]", "phi[3]", "phi[4]"]
 logd = build_logd("Naive Bayes", bench_res...)
 
