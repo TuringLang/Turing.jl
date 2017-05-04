243e3fca5565a08bdd9541ee87e0b23840239e68
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-05-04 22:04:01 +0100

    Fix SV.

--- test/sv_nuts.jl
+++ test/sv_nuts.jl
@@ -8,7 +8,7 @@ y = readsvdata()
 @model sv_nuts(y, N, dy) = begin
   τ ~ Exponential(1/100)
   ν ~ Exponential(1/100)
-  s = Vector{Real}(N)
+  s = TArray{Real}(N)
   s[1] ~ Exponential(1/100)
   for i = 2:N
     s[i] ~ Normal(log(s[i-1]), τ)
@@ -19,7 +19,7 @@ y = readsvdata()
 end
 
 N = length(y)
-chain = sample(sv_nuts(y, N, nothing), NUTS(1000, 0.65))
-# chain = sample(sv_nuts(y, N, y), Gibbs(100, HMC(2, 0.002, 2, :τ, :ν), PG(50, 2, :s)))
+chain = sample(sv_nuts(y, N, NaN), NUTS(1000, 0.65))
+# chain = sample(sv_nuts(y, N, NaN), Gibbs(100, HMC(2, 0.002, 2, :τ, :ν), PG(50, 2, :s)))
 
 describe(chain)
