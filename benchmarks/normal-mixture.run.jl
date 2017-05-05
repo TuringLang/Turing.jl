a8c9c1e87a65bd02dc55d7446f5b407fc803ac3f
Kai Xu <xukai921110@gmail.com>
2017-05-05 19:09:15 +0100

    Fix benchmark bug

--- benchmarks/normal-mixture.run.jl
+++ benchmarks/normal-mixture.run.jl
@@ -7,7 +7,7 @@ include(Pkg.dir("Turing")*"/example-models/stan-models/normal-mixture-stan.data.
 include(Pkg.dir("Turing")*"/example-models/stan-models/normal-mixture.model.jl")
 
 # NOTE: I only run a sub-set of the data as running the whole is quite slow
-bench_res = tbenchmark("Gibbs(1000, HMC(1, 0.05, 1, :theta), PG(50, 1, :k), HMC(1, 0.2, 3, :mu))", "nmmodel", "y[1:100]")
+bench_res = tbenchmark("Gibbs(1000, HMC(1, 0.05, 1, :theta), PG(50, 1, :k), HMC(1, 0.2, 3, :mu))", "nmmodel", "simplenormalmixturestandata[1][\"y\"][1:100]")
 logd = build_logd("Simple Gaussian Mixture Model", bench_res...)
 
 include("normal-mixture-stan.run.jl")
