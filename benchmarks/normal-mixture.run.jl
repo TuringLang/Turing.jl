a95c1de73200a87fda6636e2f9b01dc08c945050
Kai Xu <xukai921110@gmail.com>
2017-05-05 17:39:51 +0100

    Fix a name changed in benchmark

--- benchmarks/normal-mixture.run.jl
+++ benchmarks/normal-mixture.run.jl
@@ -3,14 +3,14 @@ using Turing
 using Stan
 
 include(Pkg.dir("Turing")*"/benchmarks/benchmarkhelper.jl")
-include(Pkg.dir("Turing")*"/example-models/stan-models/simple-normal-mixture-stan.data.jl")
-include(Pkg.dir("Turing")*"/example-models/stan-models/simple-normal-mixture.model.jl")
+include(Pkg.dir("Turing")*"/example-models/stan-models/normal-mixture-stan.data.jl")
+include(Pkg.dir("Turing")*"/example-models/stan-models/normal-mixture.model.jl")
 
 # NOTE: I only run a sub-set of the data as running the whole is quite slow
 bench_res = tbenchmark("Gibbs(1000, HMC(1, 0.05, 1, :theta), PG(50, 1, :k), HMC(1, 0.2, 3, :mu))", "nmmodel", "y[1:100]")
 logd = build_logd("Simple Gaussian Mixture Model", bench_res...)
 
-include("simple-normal-mixture-stan.run.jl")
+include("normal-mixture-stan.run.jl")
 logd["stan"] = Dict("theta" => mean(nm_theta), "mu[1]" => mean(nm_mu_1), "mu[2]" =>mean(nm_mu_2))
 logd["time_stan"] = nm_time
 
