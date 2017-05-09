f559d4454f1680d25c2aede0d47b5e5e2c449766
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-05-09 13:48:40 +0100

    Commented out an obsolete function.

--- src/core/container.jl
+++ src/core/container.jl
@@ -37,18 +37,19 @@ function Base.push!(pc :: ParticleContainer, p :: Particle)
 end
 Base.push!(pc :: ParticleContainer) = Base.push!(pc, eltype(pc.vals)(pc.model))
 
-function Base.push!(pc :: ParticleContainer, n :: Int)
-  vals = Array{eltype(pc.vals), 1}(n)
-  logWs = Array{eltype(pc.logWs), 1}(n)
-  for i=1:n
-    vals[i]  = eltype(pc.vals)(pc.model)
-    logWs[i] = 0
-  end
-  append!(pc.vals, vals)
-  append!(pc.logWs, logWs)
-  pc.num_particles += n
-  pc
-end
+# NOTE: Function to remove.
+# function Base.push!(pc :: ParticleContainer, n :: Int)
+#   vals = Array{eltype(pc.vals), 1}(n)
+#   logWs = Array{eltype(pc.logWs), 1}(n)
+#   for i=1:n
+#     vals[i]  = eltype(pc.vals)(pc.model)
+#     logWs[i] = 0
+#   end
+#   append!(pc.vals, vals)
+#   append!(pc.logWs, logWs)
+#   pc.num_particles += n
+#   pc
+# end
 
 function Base.push!(pc :: ParticleContainer, n :: Int, spl, varInfo)
   vals = Array{eltype(pc.vals), 1}(n)
