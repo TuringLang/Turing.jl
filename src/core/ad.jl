6b78abd713de5c52ab1b30b9fa13516ad476d03b
Kai Xu <xukai921110@gmail.com>
2017-05-11 21:19:24 +0100

    Rename varibales in ad.jl

--- src/core/ad.jl
+++ src/core/ad.jl
@@ -32,46 +32,51 @@ gradient2(vi::VarInfo, model::Function, spl::Union{Void, Sampler}) = begin
 
   # Split keys(vi) into chunks,
   dprintln(4, "making chunks...")
-  prior_key_chunks = []; key_chunk = []; prior_dim = 0
+  vn_chunks = []; vn_chunk = []; chunk_dim = 0
 
-  gkeys = getvns(vi, spl)
-  for k in gkeys
+  vns_all = getvns(vi, spl)
+  for k in vns_all
     l = length(getrange(vi, k))         # dimension for the current variable
-    if prior_dim + l > CHUNKSIZE
-      push!(prior_key_chunks, # store the previous chunk
-            (key_chunk, prior_dim))
-      key_chunk = []          # initialise a new chunk
-      prior_dim = 0           # reset dimension counter
+    if chunk_dim + l > CHUNKSIZE
+      push!(vn_chunks, # store the previous chunk
+            (vn_chunk, chunk_dim))
+      vn_chunk = []          # initialise a new chunk
+      chunk_dim = 0           # reset dimension counter
     end
-    push!(key_chunk, k)       # put the current variable into the current chunk
-    prior_dim += l            # update dimension counter
+    push!(vn_chunk, k)       # put the current variable into the current chunk
+    chunk_dim += l            # update dimension counter
   end
-  push!(prior_key_chunks,     # push the last chunk
-        (key_chunk, prior_dim))
+  push!(vn_chunks,     # push the last chunk
+        (vn_chunk, chunk_dim))
 
   # Chunk-wise forward AD
-  for (key_chunk, prior_dim) in prior_key_chunks
+  for (vn_chunk, chunk_dim) in vn_chunks
     expand!(vi) # NOTE: place where calling gradient should
                 #       be responsible for clean up the vals
     # Set dual part correspondingly
     dprintln(4, "set dual...")
-    dps = zeros(prior_dim)
-    prior_count = 1
-    for k in gkeys
+    dps = zeros(chunk_dim)
+    dim_count = 1
+
+    # vns = filter(vn -> vn in vn_chunk, vns_all)
+    # ranges = union(map(vn -> getrange(vi, vn), vns)...)
+    # vi[ranges] =
+
+    for k in vns_all
       l = length(getrange(vi, k))
       reals = realpart(getval(vi, k))
       range = getrange(vi, k)
-      if k in key_chunk         # for each variable to compute gradient in this round
+      if k in vn_chunk         # for each variable to compute gradient in this round
         dprintln(5, "making dual...")
         for i = 1:l
-          dps[prior_count] = 1  # set dual part
+          dps[dim_count] = 1  # set dual part
           vi[range[i]] = ForwardDiff.Dual(reals[i], dps...)
-          dps[prior_count] = 0  # reset dual part
-          prior_count += 1      # count
+          dps[dim_count] = 0  # reset dual part
+          dim_count += 1      # count
         end
         dprintln(5, "make dual done")
       else                      # for other varilables (no gradient in this round)
-        vi[range] = map(r -> Dual{prior_dim, Float64}(r), reals)
+        vi[range] = map(r -> Dual{chunk_dim, Float64}(r), reals)
       end
     end
     vi = runmodel(model, vi, spl)
