289b004a83acc0f7b15e23b70e040207434ebbe9
Kai Xu <xukai921110@gmail.com>
2017-05-12 17:13:20 +0100

    One more LDA run

--- benchmarks/lda.run.jl
+++ benchmarks/lda.run.jl
@@ -6,12 +6,13 @@ include(Pkg.dir("Turing")*"/benchmarks/benchmarkhelper.jl")
 include(Pkg.dir("Turing")*"/example-models/stan-models/lda-stan.data.jl")
 include(Pkg.dir("Turing")*"/example-models/stan-models/lda.model.jl")
 
-bench_res = tbenchmark("HMCDA(1000, 0.65, 1.5)", "ldamodel", "data=ldastandata[1]")
-bench_res[4].names = ["phi[1]", "phi[2]"]
-logd = build_logd("LDA", bench_res...)
-
 include(Pkg.dir("Turing")*"/benchmarks/"*"lda-stan.run.jl")
-logd["stan"] = lda_stan_d
-logd["time_stan"] = lda_time
 
-print_log(logd)
+for alg in ["HMC(1000, 0.25, 6)", "HMCDA(1000, 0.65, 1.5)"]
+  bench_res = tbenchmark(alg, "ldamodel", "data=ldastandata[1]")
+  bench_res[4].names = ["phi[1]", "phi[2]"]
+  logd = build_logd("LDA", bench_res...)
+  logd["stan"] = lda_stan_d
+  logd["time_stan"] = lda_time
+  print_log(logd)
+end
