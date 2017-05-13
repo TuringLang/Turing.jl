d8c2d418fc02dccf2514042bb571f48761cc15d2
Hong Ge <h.ge@damtp.cam.ac.uk>
2017-05-13 11:49:31 +0100

    Add NUTS to lda.run.

--- benchmarks/lda.run.jl
+++ benchmarks/lda.run.jl
@@ -10,7 +10,7 @@ include(Pkg.dir("Turing")*"/benchmarks/"*"lda-stan.run.jl")
 
 setchunksize(60)
 
-for alg in ["HMC(1000, 0.25, 6)", "HMCDA(1000, 0.65, 1.5)"]
+for alg in ["HMC(1000, 0.25, 6)", "HMCDA(1000, 0.65, 1.5)", "NUTS(2000, 1000, 0.65)"]
   bench_res = tbenchmark(alg, "ldamodel", "data=ldastandata[1]")
   bench_res[4].names = ["phi[1]", "phi[2]"]
   logd = build_logd("LDA", bench_res...)
