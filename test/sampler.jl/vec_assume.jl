78abffb6e21b5292bb7c87e9a1eff180a3d77427
Kai Xu <xukai921110@gmail.com>
2017-05-13 17:48:31 +0100

    Improve vec test

--- test/sampler.jl/vec_assume.jl
+++ test/sampler.jl/vec_assume.jl
@@ -1,17 +1,39 @@
 include("../utility.jl")
-using Distributions, Turing
+using Distributions, Turing, Base.Test
 
+N = 100
+setchunksize(N)
+alg = HMC(1000, 0.2, 4)
 
-setchunksize(100)
-# Test for vectorize UnivariateDistribution
 @model vdemo() = begin
-  x = Vector{Real}(100)
-  x ~ [Normal(0, sqrt(4))]
-  # for i = 1:100
-  #   x[i] ~ Normal(0, sqrt(4))
-  # end
+  x = Vector{Real}(N)
+  for i = 1:N
+    x[i] ~ Normal(0, sqrt(4))
+  end
 end
 
-alg = HMC(1000, 0.2, 4)
-res = sample(vdemo(), alg)
-println(mean(mean(res[:x])))
+t_loop = @elapsed res = sample(vdemo(), alg)
+@test_approx_eq_eps mean(mean(res[:x])) 0 0.1
+
+
+# Test for vectorize UnivariateDistribution
+@model vdemo() = begin
+  x = Vector{Real}(N)
+  x ~ [Normal(0, 2)]
+end
+
+t_vec = @elapsed res = sample(vdemo(), alg)
+@test_approx_eq_eps mean(mean(res[:x])) 0 0.1
+
+
+@model vdemo() = begin
+  x ~ MvNormal(zeros(N), 2 * ones(N))
+end
+
+t_mv = @elapsed res = sample(vdemo(), alg)
+@test_approx_eq_eps mean(mean(res[:x])) 0 0.1
+
+println("Time for")
+println("  Loop : $t_loop")
+println("  Vec  : $t_vec")
+println("  Mv   : $t_mv")
