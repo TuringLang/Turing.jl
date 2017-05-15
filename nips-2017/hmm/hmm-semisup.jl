dbd4778ea0f1f52c7ea72d8b2b8589353e78eb66
Kai Xu <xukai921110@gmail.com>
2017-05-15 21:57:35 +0100

    LDA done
    Former-commit-id: e9da43e7731767621e898b4d2fa978f8f67cdf70

--- nips-2017/hmm/hmm-semisup.jl
+++ nips-2017/hmm/hmm-semisup.jl
@@ -42,7 +42,43 @@ model {
 }
 "
 
+using HDF5, JLD
+const hmm_semisup_data = load(Pkg.dir("Turing")*"/nips-2017/hmm/hmm_semisup_data.jld")["data"]
+
 using Distributions
 using Turing
-@modle hmm_semisup(K,V,T,T_unsup,w,z,u,alpha,beta) = begin
+using StatsFuns: logsumexp
+
+@model hmm_semisup(K,V,T,T_unsup,w,z,u,alpha,beta) = begin
+  theta = Vector{Vector{Real}}(K)
+  for k = 1:K
+    theta[k] ~ Dirichlet(alpha)
+  end
+  phi = Vector{Vector{Real}}(K)
+  for k = 1:K
+    phi[k] ~ Dirichlet(beta)
+  end
+
+  for t = 1:T
+    w[t] ~ Categorical(phi[z[t]])
+  end
+  for t = 2:T
+    z[t] ~ Categorical(theta[z[t-1]])
+  end
+
+  acc = Vector{Float64}(K)
+  gamma = Matrix{Float64}(T_unsup,K)
+  for k = 1:K
+    gamma[1,k] = log(phi[k][u[1]])
+  end
+  for t = 2:T_unsup,
+      k = 1:K
+      for j = 1:K
+        acc[j] = gamma[t-1,j] + log(theta[j][k]) + log(phi[k][u[t]])
+      end
+      gamma[t,k] = logsumexp(acc)
+  end
+  Turing.acclogp!(logsumexp(gamma[T_unsup,:]))
 end
+
+sample(hmm_semisup(data=hmm_semisup_data[1]), NUTS(200, 0.65))
--- nips-2017/hmm/sim-data.jl
+++ nips-2017/hmm/sim-data.jl
@@ -1,4 +1,40 @@
 # Script adapted from https://github.com/stan-dev/example-models/blob/master/misc/hmm/hmm-semisup.data.R
+#
+# require(MCMCpack)
+#
+# # CONSTANTS
+# K <- 3;
+# V <- 10;
+# T <- 100;
+# T_unsup <- 500;
+# alpha <- rep(1,K);
+# beta <- rep(0.1,V);
+#
+# # DATA
+# w <- rep(0,T);
+# z <- rep(0,T);
+# u <- rep(0,T_unsup);
+#
+# # PARAMETERS
+# theta <- rdirichlet(K,alpha);
+# phi <- rdirichlet(K,beta);
+#
+# # SIMULATE DATA
+#
+# # supervised
+# z[1] <- sample(1:K,1);
+# for (t in 2:T)
+#   z[t] <- sample(1:K,1,replace=TRUE,theta[z[t - 1], 1:K]);
+# for (t in 1:T)
+#   w[t] <- sample(1:V,1,replace=TRUE,phi[z[t],1:V]);
+#
+# # unsupervised
+# y <- rep(0,T_unsup);
+# y[1] <- sample(1:K,1);
+# for (t in 2:T_unsup)
+#   y[t] <- sample(1:K,1,replace=TRUE,theta[y[t-1],1:K]);
+# for (t in 1:T_unsup)
+#   u[t] <- sample(1:V,1,replace=TRUE,phi[y[t], 1:V]);
 
 using Distributions
 
@@ -54,4 +90,6 @@ const hmm_semisup_data = [
   )
 ]
 
-writedlm(Pkg.dir("Turing")*"/nips-2017/hmm/hmm_semisup_data.txt", hmm_semisup_data)
+using HDF5, JLD
+
+save(Pkg.dir("Turing")*"/nips-2017/hmm/hmm_semisup_data.jld", "data", hmm_semisup_data)
