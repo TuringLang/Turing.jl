295d34f1358c7bfb79afa0866c82e6942f86d3cf
Kai Xu <xukai921110@gmail.com>
2017-05-16 12:47:51 +0100

    Add SV script

--- example-models/nuts-paper/sv_helper.jl
+++ example-models/nuts-paper/sv_helper.jl
@@ -1,6 +1,7 @@
 # Load data
 # NOTE: put the data file in the same path of this file
+
 readsvdata() = begin
-  y = readcsv("sv_nuts.data")[:,2][2:end]
+  y = readcsv(TPATH*"/example-models/nuts-paper/sv_nuts.data")[:,2][2:end]
   y = Float64[map(i -> isa(i, Real) ? i : 0.1, y)...]
 end
---
+++ nips-2017/sv/sv-exp.jl
@@ -0,0 +1,32 @@
+using Distributions
+using Turing
+using HDF5, JLD
+
+TPATH = Pkg.dir("Turing")
+
+include(TPATH*"/example-models/nuts-paper/sv_helper.jl")
+
+y = readsvdata()
+
+# Stochastic volatility (SV)
+@model sv_nuts(y, N, dy) = begin
+  τ ~ Exponential(1/100)
+  ν ~ Exponential(1/100)
+  s = TArray{Real}(N)
+  s[1] ~ Exponential(1/100)
+  for i = 2:N
+    s[i] ~ Normal(log(s[i-1]), τ)
+    s[i] = exp(s[i])
+    dy = typeof(ν)(log(y[i] / y[i-1]) / s[i])
+    dy ~ TDist(ν)
+  end
+end
+
+N = length(y)
+# chain = sample(sv_nuts(y, N, NaN), NUTS(10, 0.65))
+chain = sample(sv_nuts(y, N, NaN), Gibbs(1000,PG(50,1,:s),NUTS(1,200,0.65,:τ,:ν)))
+# chain = sample(sv_nuts(y, N, NaN), Gibbs(100, HMC(2, 0.002, 2, :τ, :ν), PG(50, 2, :s)))
+
+describe(chain)
+
+save(TPATH*"/nips-2017/sv/sv-exps-Gibbs(1000,PG(50,1),NUTS(1,200,0.65))-chain.jld", "chain", chain)
