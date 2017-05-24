4c91c09e9a971961f8992d638e5f6d19b756635d
Kai Xu <xukai921110@gmail.com>
2017-05-24 23:24:11 +0100

    Our PR for Distributions.jl hasn't been merged

--- example-models/nips-2017/sv.run.jl
+++ example-models/nips-2017/sv.run.jl
@@ -5,6 +5,10 @@ TPATH = Pkg.dir("Turing")
 
 include("/example-models/nips-2017/sv.model.jl")
 
+PR_for_distributions_is_merged = false
+
+if PR_for_distributions_is_merged
+
 using HDF5, JLD
 sv_data = load(TPATH*"/example-models/nips-2017/sv-data.jld")["data"]
 
@@ -18,3 +22,5 @@ decribe(chain_nuts)
 setchunksize(5)
 chain_gibbs = sample(model_f, Gibbs(sample_n, PG(50,1,:h),NUTS(1000,0.65,:ϕ,:σ,:μ)))
 decribe(chain_gibbs)
+
+end
