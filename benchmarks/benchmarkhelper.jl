39e1147974cb692b56389af51392d1f13dd74f36
Kai Xu <xukai921110@gmail.com>
2017-05-24 16:30:24 +0100

    Fix Stan version to use

--- benchmarks/benchmarkhelper.jl
+++ benchmarks/benchmarkhelper.jl
@@ -8,8 +8,7 @@ end
 # Run benchmark
 tbenchmark(alg::String, model::String, data::String) = begin
   chain, time, mem, _, _  = eval(parse("@timed sample($model($data), $alg)"))
-  # alg, sum(chain[:elapsed]), mem, chain, deepcopy(chain)
-  alg, time, mem, chain, deepcopy(chain)
+  alg, sum(chain[:elapsed]), mem, chain, deepcopy(chain)
 end
 
 # Build logd from Turing chain
