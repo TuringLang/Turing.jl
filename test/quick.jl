c34767901e431f9c17867392be9159836f84fd22
Kai Xu <xukai921110@gmail.com>
2017-06-12 21:23:03 +0100

    Quick profile

--- test/quick.jl
+++ test/quick.jl
@@ -1,31 +1,20 @@
 using Turing
 
-@model ldamodel(K, V, M, N, w, doc, beta, alpha) = begin
+@model ldamodel_vec(K, V, M, N, w, doc, beta, alpha) = begin
   theta = Matrix{Real}(K, M)
-  for m = 1:M
-    theta ~ [Dirichlet(alpha)]
-  end
+  theta ~ [Dirichlet(alpha)]
 
   phi = Matrix{Real}(V, K)
-  for k = 1:K
-    phi ~ [Dirichlet(beta)]
-  end
-  
-  phi_dot_theta = phi * theta
+  phi ~ [Dirichlet(beta)]
+
+  phi_dot_theta = log(phi * theta)
   for n = 1:N
     Turing.acclogp!(vi, phi_dot_theta[w[n], doc[n]])
   end
 end
 
-include(Pkg.dir("Turing")*"/benchmarks/benchmarkhelper.jl")
 include(Pkg.dir("Turing")*"/example-models/stan-models/lda-stan.data.jl")
 
-setchunksize(60)
+setchunksize(100)
 
-alg = "HMCDA(2000, 0.65, 1.5)"
-bench_res = tbenchmark(alg, "ldamodel", "data=ldastandata[1]")
-bench_res[4].names = ["phi[1]", "phi[2]"]
-logd = build_logd("LDA", bench_res...)
-# logd["stan"] = lda_stan_d
-# logd["time_stan"] = lda_time
-print_log(logd)
+sample(ldamodel_vec(data=ldastandata[1]), HMC(2000, 0.025, 10))
