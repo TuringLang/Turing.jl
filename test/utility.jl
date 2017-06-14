6d91745d935502ab60cddb613836595caaa95f7a
Kai Xu <xukai921110@gmail.com>
2017-06-14 23:11:53 +0100

    Try to fix appveyor.

--- test/utility.jl
+++ test/utility.jl
@@ -1,16 +1,18 @@
 # Helper functions used by tarce.jl and varinfo.jl
 
+println("[Turing] in utility.jl")
 using Turing
+println("[Turing] used Turing")
 
 # Helper function for numerical tests
 check_numerical(chain::Turing.Chain, symbols::Vector{Symbol}, exact_vals::Vector;
                 eps=0.2) = begin
   for (sym, val) in zip(symbols, exact_vals)
     E = mean(chain[sym])
-    print("  $sym = $E ≈ $val (ϵ = $eps) ?")
+    print("  $sym = $E ≈ $val (eps = $eps) ?")
     cmp = abs(sum(mean(chain[sym]) - val)) <= eps
     if cmp
-      print_with_color(:green, " ✓\n")
+      print_with_color(:green, "./\n")
       print_with_color(:green, "    $sym = $E, diff = $(abs(E - val))\n")
     else
       print_with_color(:red, " X\n")
@@ -18,5 +20,7 @@ check_numerical(chain::Turing.Chain, symbols::Vector{Symbol}, exact_vals::Vector
     end
   end
 end
+println("[Turing] defined check_numerical")
 
 using Turing: Sampler
+println("[Turing] used Turing.Sampler")
