e8e67477650ff79fde0d3154c6a1186b94be4725
Kai Xu <xukai921110@gmail.com>
2017-06-24 10:40:56 -0700

    Remove obselte code

--- src/core/compiler.jl
+++ src/core/compiler.jl
@@ -5,7 +5,7 @@
 doc"""
     var_name ~ Distribution()
 
-`~` notation is to specifiy *a variable follows a distributions*. 
+`~` notation is to specifiy *a variable follows a distributions*.
 
 If `var_name` is an un-defined variable or a container (e.g. Vector or Matrix), this variable will be treated as model parameter; otherwise if `var_name` is defined, this variable will be treated as data.
 """
@@ -21,9 +21,6 @@ macro ~(left, right)
           $(left),    # Data point
           vi
         )
-        if isa(sampler, Union{Turing.Sampler{PG},Turing.Sampler{SMC}})
-          vi = Turing.current_trace().vi
-        end
       end
     )
   else
@@ -44,9 +41,7 @@ macro ~(left, right)
             $(left),    # Data point
             vi
           )
-          if isa(sampler, Union{Turing.Sampler{PG},Turing.Sampler{SMC}})
-            vi = Turing.current_trace().vi
-          end
+
         end
       )
     else
