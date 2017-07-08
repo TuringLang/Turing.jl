1f2ef467f85128487ab2106ae8514049dfafaf12
Kai Xu <xukai921110@gmail.com>
2017-07-08 22:02:15 +0100

    Clean up simple gassuain example

--- benchmarks/gauss.run.jl
+++ benchmarks/gauss.run.jl
@@ -3,10 +3,10 @@ using Turing
 using Stan
 
 include(Pkg.dir("Turing")*"/benchmarks/benchmarkhelper.jl")
-include(Pkg.dir("Turing")*"/example-models/benchmarks/gdemo.data.jl")
+include(Pkg.dir("Turing")*"/example-models/benchmarks/gdemo-stan.data.jl")
 include(Pkg.dir("Turing")*"/example-models/benchmarks/gdemo.model.jl")
 
-bench_res = tbenchmark("HMC(2000, 0.1, 3)", "simplegaussmodel", "simplegaussdata")
+bench_res = tbenchmark("HMC(2000, 0.1, 3)", "simplegaussmodel", "data=simplegaussstandata[1]")
 logd = build_logd("Simple Gaussian Model", bench_res...)
 logd["analytic"] = Dict("s" => 49/24, "m" => 7/6)
 
@@ -16,3 +16,8 @@ logd["stan"] = Dict("s" => mean(s_stan), "m" => mean(m_stan))
 logd["time_stan"] = sg_time
 
 print_log(logd)
+
+
+using Requests
+import Requests: get, post, put, delete, options, FileParam
+send_log(logd)
