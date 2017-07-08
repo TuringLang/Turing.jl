5dd6375e7856918fb85a49102390d723f6192ea2
Kai Xu <xukai921110@gmail.com>
2017-07-08 21:32:59 +0100

    Clean up Bernoulli example

--- benchmarks/bernoulli.run.jl
+++ benchmarks/bernoulli.run.jl
@@ -3,10 +3,10 @@ using Turing
 using Stan
 
 include(Pkg.dir("Turing")*"/benchmarks/benchmarkhelper.jl")
-include(Pkg.dir("Turing")*"/example-models/stan-models/bernoulli.data.jl")
+include(Pkg.dir("Turing")*"/example-models/stan-models/bernoulli-stan.data.jl")
 include(Pkg.dir("Turing")*"/example-models/stan-models/bernoulli.model.jl")
 
-bench_res = tbenchmark("HMC(1000, 0.25, 5)", "bermodel", "berdata")
+bench_res = tbenchmark("HMC(1000, 0.25, 5)", "bermodel", "data=berstandata[1]")
 logd = build_logd("Bernoulli Model", bench_res...)
 
 
@@ -15,3 +15,7 @@ logd["stan"] = Dict("theta" => mean(theta_stan))
 logd["time_stan"] = ber_time
 
 print_log(logd)
+
+using Requests
+import Requests: get, post, put, delete, options, FileParam
+send_log(logd)
