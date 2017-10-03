ffb9407868667bc33a10694d76a41dce1d9f3f23
Kai Xu <xukai921110@gmail.com>
2017-10-03 01:53:07 +0100

    seperate model wrapper call in sample.jl test

--- test/compiler.jl/sample.jl
+++ test/compiler.jl/sample.jl
@@ -15,7 +15,8 @@ x = [2.0, 3.0]
 alg = Gibbs(1000, HMC(1, 0.2, 3, :m), PG(10, 1, :s))
 # NOTE: want to translate below to
 #       chn = sample(gdemo, Dict(:x => x), alg)
-chn = sample(gdemo2(x), alg);
+modelf = gdemo2(x)
+chn = sample(modelf, alg);
 mean(chn[:s])
 
 # Turing.TURING[:modelex]
