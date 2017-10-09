bdb7f2d167a55fb3db4830fc28af9206c1418817
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2017-10-09 02:02:19 +0100

    tests for IPMCMC

---
+++ test/ipmcmc.jl/ipmcmc2.jl
@@ -0,0 +1,56 @@
+using Turing
+using Base.Test
+
+srand(125)
+
+D = [1.0 1.0 4.0 4.0]
+
+@model MoGtest(D) = begin
+  mu1 ~ Normal(1, 1)
+  mu2 ~ Normal(4, 1)
+  z1 ~ Categorical(2)
+  if z1 == 1
+    D[1] ~ Normal(mu1, 1)
+  else
+    D[1] ~ Normal(mu2, 1)
+  end
+  z2 ~ Categorical(2)
+  if z2 == 1
+    D[2] ~ Normal(mu1, 1)
+  else
+    D[2] ~ Normal(mu2, 1)
+  end
+  z3 ~ Categorical(2)
+  if z3 == 1
+    D[3] ~ Normal(mu1, 1)
+  else
+    D[3] ~ Normal(mu2, 1)
+  end
+  z4 ~ Categorical(2)
+  if z4 == 1
+    D[4] ~ Normal(mu1, 1)
+  else
+    D[4] ~ Normal(mu2, 1)
+  end
+  z1, z2, z3, z4, mu1, mu2
+end
+
+gibbs = Gibbs(500, IPMCMC(10, 1, 3, 1, :z1, :z2, :z3, :z4), HMC(1, 0.2, 4, :mu1, :mu2))
+chain = sample(MoGtest(D), gibbs)
+
+@test_approx_eq_eps mean(chain[:z1]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:z2]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:z3]) 2.0 0.1
+@test_approx_eq_eps mean(chain[:z4]) 2.0 0.1
+@test_approx_eq_eps mean(chain[:mu1]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:mu2]) 4.0 0.1
+
+gibbs = IPMCMC(15, 100, 10, 5)
+chain = sample(MoGtest(D), gibbs)
+
+@test_approx_eq_eps mean(chain[:z1]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:z2]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:z3]) 2.0 0.1
+@test_approx_eq_eps mean(chain[:z4]) 2.0 0.1
+@test_approx_eq_eps mean(chain[:mu1]) 1.0 0.1
+@test_approx_eq_eps mean(chain[:mu2]) 4.0 0.1
---
+++ test/ipmcmc.jl/ipmcmc_cons.jl
@@ -0,0 +1,22 @@
+using Turing, Distributions
+using Base.Test
+
+@model gdemo() = begin
+  s ~ InverseGamma(2,3)
+  m ~ Normal(0,sqrt(s))
+  1.5 ~ Normal(m, sqrt(s))
+  2.0 ~ Normal(m, sqrt(s))
+  return s, m
+end
+
+N = 50
+s1 = Gibbs(N, IPMCMC(10, 1, 3, 1, :s), HMC(1, 0.2, 3, :m))
+s2 = IPMCMC(10, N, 4, 2)
+
+c1 = sample(gdemo(), s1)
+c2 = sample(gdemo(), s2)
+
+# Very loose bound, only for testing constructor.
+for c in [c1, c2]
+  check_numerical(c, [:s, :m], [49/24, 7/6], eps=1.0)
+end
