2780387ae3cc63160bb4c63acb16ccd4e664ed02
Emile Mathieu <emile.mathieu@stats.ox.ac.uk>
2017-11-17 19:29:21 +0100

    fix test

--- test/mh.jl/mh.jl
+++ test/mh.jl/mh.jl
@@ -6,7 +6,7 @@ using Base.Test
 srand(125)
 x = [1.5 2.0]
 
-@model pmmhtest(x) = begin
+@model mhtest(x) = begin
   s ~ InverseGamma(2,3)
   m ~ Normal(0,sqrt(s))
   for i in 1:length(x)
@@ -15,8 +15,10 @@ x = [1.5 2.0]
   s, m
 end
 
+model = mhtest(x)
+
 # MH with prior as proposal
-res = sample(pmmhtest(x), MH(100))
+res = sample(model, MH(100))
 check_numerical(res,
 [:s, :m], [49/24, 7/6]
 )
@@ -24,12 +26,12 @@ check_numerical(res,
 # MH with Gaussian proposal
 GaussianKernel(var) = (x) -> Normal(x, sqrt(var))
 check_numerical(
-  sample(pmmhtest(x), MH(500, (:s, GaussianKernel(1.0)), (:m, GaussianKernel(1.0)))),
+  sample(model, MH(500, (:s, GaussianKernel(1.0)), (:m, GaussianKernel(1.0)))),
   [:s, :m], [49/24, 7/6]
 )
 
 # MH within Gibbs
 check_numerical(
-  sample(pmmhtest(x), Gibbs(100, MH(5, :m), MH(5, :s))),
+  sample(model, Gibbs(100, MH(5, :m), MH(5, :s))),
   [:s, :m], [49/24, 7/6]
 )
