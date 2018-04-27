ee5659e1755ee0e11ff5b232a8de7b44867df50e
Kai Xu <xukai921110@gmail.com>
2018-04-27 14:57:44 +0100

    make hmcda using _hmc_step

--- test/hmc_core.jl/simple_gauss.jl
+++ test/hmc_core.jl/simple_gauss.jl
@@ -1,6 +1,6 @@
 using Distributions, DiffBase
 using ReverseDiff: GradientTape, GradientConfig, gradient, gradient!, compile
-using Turing: _sample_momentum, _leapfrog, _find_H
+using Turing: _hmc_step
 
 # @model gdemo(x) = begin
 
@@ -47,88 +47,6 @@ function grad_func(θ)
 
 end
 
-function _hmc_step(θ, lj, lj_func, grad_func, ϵ::Float64, λ::Float64, stds;
-                   dprint=dprintln)
-
-  θ_dim = length(θ)
-
-  dprint(2, "sampling momentums...")
-  p = _sample_momentum(θ_dim, stds)
-
-  dprint(2, "recording old values...")
-  H = _find_H(θ, p, lj, stds)
-
-  τ = max(1, round(Int, λ / ϵ))
-  dprint(2, "leapfrog for $τ steps with step size $ϵ")
-  θ_new, p_new, τ_valid = _leapfrog(θ, p, τ, ϵ, grad_func)
-
-  dprint(2, "computing new H...")
-  lj_new = lj_func(θ_new)
-  H_new = (τ_valid == 0) ? Inf : _find_H(θ_new, p_new, lj_new, stds)
-
-  dprint(2, "computing accept rate α...")
-  α = min(1, exp(-(H_new - H)))
-
-  # if PROGRESS && spl.alg.gid == 0
-  # stds_str = string(spl.info[:wum][:stds])
-  # stds_str = length(stds_str) >= 32 ? stds_str[1:30]*"..." : stds_str
-  # haskey(spl.info, :progress) && ProgressMeter.update!(
-  #                                     spl.info[:progress],
-  #                                     spl.info[:progress].counter; showvalues = [(:ϵ, ϵ), (:α, α), (:pre_cond, stds_str)]
-  #                                 )
-  # end
-
-  dprint(2, "decide wether to accept...")
-  is_accept = false
-  if rand() < α             # accepted
-    θ = θ_new
-    lj = lj_new
-    is_accept = true
-  # push!(spl.info[:accept_his], true)
-  else                      # rejected
-  # push!(spl.info[:accept_his], false)
-
-  # Reset Θ
-  # NOTE: ForwardDiff and ReverseDiff need different implementation
-  #       due to immutable Dual vs mutable TrackedReal
-  # if ADBACKEND == :forward_diff
-
-  #     vi[spl] = old_θ
-
-  # elseif ADBACKEND == :reverse_diff
-
-  #     vi_spl = vi[spl]
-  #     for i = 1:length(old_θ)
-  #     if isa(vi_spl[i], ReverseDiff.TrackedReal)
-  #         vi_spl[i].value = old_θ[i]
-  #     else
-  #         vi_spl[i] = old_θ[i]
-  #     end
-  #     end
-
-  # end
-
-  # setlogp!(vi, old_logp)  # reset logp
-  end
-
-
-  # if spl.alg.delta > 0 && τ_valid > 0    # only do adaption for HMCDA
-  # adapt!(spl.info[:wum], α, realpart(vi[spl]), adapt_ϵ = true)
-  # end
-
-  dprint(3, "R -> X...")
-  # if spl.alg.gid != 0 invlink!(vi, spl); cleandual!(vi) end
-
-  return θ, lj, is_accept
-
-end
-
-_hmc_step(θ, lj, lj_func, grad_func, τ::Int, ϵ::Float64, stds; dprint=dprintln) =
-  _hmc_step(θ, lj, lj_func, grad_func, ϵ, τ * ϵ, stds; dprint=dprint)
-
-# @show _hmc_step(θ, lj, lj_func, grad_func, 5, 0.01, stds)
-
-
 stds = ones(θ_dim)
 θ = randn(θ_dim)
 lj = lj_func(θ)
@@ -143,7 +61,7 @@ end
 totla_num = 5000
 for iter = 1:totla_num
   push!(chn, θ)
-  θ, lj, is_accept = _hmc_step(θ, lj, lj_func, grad_func, 5, 0.05, stds; dprint=dummy_print)
+  θ, lj, is_accept, τ_valid, α = _hmc_step(θ, lj, lj_func, grad_func, 5, 0.05, stds; dprint=dummy_print)
   accept_num += is_accept
   if (iter % 50 == 0) println(θ) end
 end
