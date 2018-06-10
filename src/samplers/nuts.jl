78ddfc4b6c9f746cea738fc19d6b258dcdae8ef7
Kai Xu <xukai921110@gmail.com>
2018-06-10 23:45:29 +0100

    clean nuts test

--- src/samplers/nuts.jl
+++ src/samplers/nuts.jl
@@ -250,22 +250,3 @@ function _nuts_step(θ, ϵ, lj_func, grad_func, stds)
   return θ_new, da_stat
 
 end
-
-function _adapt_ϵ(logϵ, Hbar, logϵbar, da_stat, m, M_adapt, δ, μ;
-                  γ=0.05, t0=10, κ=0.75)
-
-    if m <= M_adapt
-
-        Hbar = (1.0 - 1.0 / (m + t0)) * Hbar + (1 / (m + t0)) * (δ - da_stat)
-        logϵ = μ - sqrt(m) / γ * Hbar
-        logϵbar = m^(-κ) * logϵ + (1 - m^(-κ)) * logϵbar
-
-    else
-
-        logϵ = logϵbar
-
-    end
-
-    return logϵ, Hbar, logϵbar
-
-end
