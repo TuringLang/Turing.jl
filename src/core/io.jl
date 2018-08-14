d0a27e88d9a1c3aa36e18cd21791a5c18e023497
Kai Xu <xukai921110@gmail.com>
2018-08-14 19:45:17 +0100

    make Chain mutable

--- src/core/io.jl
+++ src/core/io.jl
@@ -84,7 +84,7 @@ mean(chain[:mu])      # find the mean of :mu
 mean(chain[:sigma])   # find the mean of :sigma
 ```
 """
-struct Chain <: AbstractChains
+mutable struct Chain <: AbstractChains
   weight  ::  Float64                 # log model evidence
   value2  ::  Array{Sample}
   value   ::  Array{Float64, 3}
