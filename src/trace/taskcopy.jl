a0fefdd6f2c682a43c28af884d3823a32eadad1e
Hong Ge <hg344@cam.ac.uk>
2018-08-18 18:04:08 +0100

    Fix #466

--- src/trace/taskcopy.jl
+++ src/trace/taskcopy.jl
@@ -26,6 +26,7 @@ function Base.copy(t::Task)
   else
     newt.storage = nothing
   end
+  # copy fields not accessible in task.c
   newt.code = t.code
   newt.state = t.state
   newt.result = t.result
@@ -36,24 +37,86 @@ function Base.copy(t::Task)
   newt
 end
 
-consume(t) = begin
-    if t.storage == nothing
-        t.storage = IdDict()
-    end
-    if ~haskey(t.storage, :__chn__)
-        t.storage[:__chn__] = Channel(0)
-    end
-    istaskstarted(t) || schedule(t)
-    take!(t.storage[:__chn__])
-end
+produce(v) = begin
+    ct = current_task()
 
-produce(x) = begin
-    ct  = current_task()
     if ct.storage == nothing
         ct.storage = IdDict()
-        ct.storage[:__chn__] = Channel(0)
     end
-    println("$ct: $x")
-    chn = ct.storage[:__chn__]
-    put!(chn, x);
+
+    haskey(ct.storage, :consumers) || (ct.storage[:consumers] = nothing)
+    local empty, t, q
+    while true
+        q = ct.storage[:consumers]
+        if isa(q,Task)
+            t = q
+            ct.storage[:consumers] = nothing
+            empty = true
+            break
+        elseif isa(q,Condition) && !isempty(q.waitq)
+            t = shift!(q.waitq)
+            empty = isempty(q.waitq)
+            break
+        end
+        wait()
+    end
+
+    t.state == :runnable || throw(AssertionError("producer.consumer.state == :runnable"))
+    if empty
+        Base.schedule_and_wait(t, v)
+        ct = current_task() # When a task is copied, ct should be updated to new task ID.
+        while true
+            # wait until there are more consumers
+            q = ct.storage[:consumers]
+            if isa(q,Task)
+                return q.result
+            elseif isa(q,Condition) && !isempty(q.waitq)
+                return q.waitq[1].result
+            end
+            wait()
+        end
+    else
+        schedule(t, v)
+        # make sure `t` runs before us. otherwise, the producer might
+        # finish before `t` runs again, causing it to see the producer
+        # as done, causing done(::Task, _) to miss the value `v`.
+        # see issue #7727
+        yield()
+        return q.waitq[1].result
+    end
+end
+
+consume(p::Task, values...) = begin
+
+    if p.storage == nothing
+        p.storage = IdDict()
+    end
+    haskey(p.storage, :consumers) || (p.storage[:consumers] = nothing)
+
+    if istaskdone(p)
+        return wait(p)
+    end
+
+    ct = current_task()
+    ct.result = length(values)==1 ? values[1] : values
+
+    #### un-optimized version
+    #if P.consumers === nothing
+    #    P.consumers = Condition()
+    #end
+    #push!(P.consumers.waitq, ct)
+    # optimized version that avoids the queue for 1 consumer
+    consumers = p.storage[:consumers]
+    if consumers === nothing || (isa(consumers,Condition)&&isempty(consumers.waitq))
+        p.storage[:consumers] = ct
+    else
+        if isa(consumers, Task)
+            t = consumers
+            p.storage[:consumers] = Condition()
+            push!(p.storage[:consumers].waitq, t)
+        end
+        push!(p.storage[:consumers].waitq, ct)
+    end
+
+    p.state == :runnable ? Base.schedule_and_wait(p) : wait() # don't attempt to queue it twice
 end
