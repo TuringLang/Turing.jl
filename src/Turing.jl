9d4f2fd2fde7e9c8c5cb99522c66674714e7dded
Hong Ge <hg344@cam.ac.uk>
2018-08-18 16:44:50 +0100

    Fix warning in dirname

--- src/Turing.jl
+++ src/Turing.jl
@@ -96,6 +96,7 @@ mutable struct Sampler{T<:InferenceAlgorithm}
   info  ::  Dict{Symbol, Any}         # sampler infomation
 end
 
+include("../deps/deps.jl")
 include("helper.jl")
 include("transform.jl")
 include("core/varinfo.jl")  # core internal variable container
@@ -142,6 +143,7 @@ dwarn(v::Int, args...)    = v < Turing.VERBOSITY ?
                             print_with_color(:red, "\r[Turing.WARNING]: ", mapreduce(string,*,args), "\n $(stacktrace()[2])\n") :
                             nothing
 derror(v::Int, args...)   = error("\r[Turing.ERROR]: ", mapreduce(string,*,args))
+
 
 ##################
 # Inference code #
