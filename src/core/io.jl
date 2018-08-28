TuringLang/Turing.jl

commit be5ea0dd5127b4bee143d786a2b8664f353293dd

Fix range and findfirst.

src/core/io.jl
@@ -17,9 +17,9 @@ getjuliatype(s::Sample, v::Symbol, cached_syms=nothing) = begin
   # NOTE: cached_syms is used to cache the filter entiries in svalue. This is helpful when the dimension of model is huge.
   if cached_syms == nothing
     # Get all keys associated with the given symbol
-    syms = collect(Iterators.filter(k -> search(string(k), string(v)*"[") != 0:-1, keys(s.value)))
+    syms = collect(Iterators.filter(k -> findfirst(string(k), string(v)*"[") != nothing, keys(s.value)))
   else
-    syms = collect((Iterators.filter(k -> search(string(k), string(v)) != 0:-1, cached_syms)))
+    syms = collect((Iterators.filter(k -> findfirst(string(k), string(v)) != nothing, cached_syms)))
   end
   # Map to the corresponding indices part
   idx_str = map(sym -> replace(string(sym), string(v), ""), syms)

src/samplers/support/resample.jl
@@ -83,7 +83,7 @@ function resampleSystematic( w::Vector{Float64}, num_particles::Int )
   N = num_particles
   Q = cumsum(w)
 
-  T = collect(linspace(0,maximum(Q)-1/N,N)) + rand()/N
+  T = collect(range(0, stop = maximum(Q)-1/N, length = N)) + rand()/N
   push!(T, 1)
 
   i=1

test/hmc_core.jl/bayes_lr.jl
@@ -27,7 +27,7 @@ using Turing
 end
 
 N = 100
-xs = collect(linspace(-10, 10, N))
+xs = collect(range(-10, stop = 10, length = N))
 s = 1
 β = rand(Normal(0, 1))
 ys = xs * β + rand(Normal(0, sqrt(s)), N)

commit 8e415f53c99c27ef64086c3c3b8eb75c500c80ea

fix io bug caused by be5ea0d

src/core/io.jl
@@ -27,7 +27,7 @@ getjuliatype(s::Sample, v::Symbol, cached_syms=nothing) = begin
   idx_comp = map(idx -> collect(Iterators.filter(str -> str != "", split(string(idx), [']','[']))), idx_str)
 
   # Deal with v is really a symbol, e.g. :x
-  if length(idx_comp) == 0
+  if length(idx_str) == 1 && idx_str[1] == ""
     return Base.getindex(s.value, v)
   end
 