8dac71737b40843a4e086799eba8b645e5fb1244
Martin Trapp <trapp.martin@gmail.com>
2018-08-29 17:39:27 +0100

    several fixes to get more tests complete correctly

--- test/sampler.jl/vec_assume_mat.jl
+++ test/sampler.jl/vec_assume_mat.jl
@@ -6,14 +6,14 @@ setchunksize(4*N)
 alg = HMC(1000, 0.2, 4)
 
 @model vdemo() = begin
-  v = Vector{Matrix{Real}}(N)
+  v = Vector{Matrix{Real}}(undef, N)
   v ~ [Wishart(7, [1 0.5; 0.5 1])]
 end
 
 t_vec = @elapsed res_vec = sample(vdemo(), alg)
 
 @model vdemo() = begin
-  v = Vector{Matrix{Real}}(N)
+  v = Vector{Matrix{Real}}(undef, N)
   for i = 1:N
     v[i] ~ Wishart(7, [1 0.5; 0.5 1])
   end
