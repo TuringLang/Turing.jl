203f25ec9d7453f782b6d8d03e94d18ff8631148
Hong Ge <hg344@cam.ac.uk>
2018-08-31 16:08:26 +0100

    Fix TArray.

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,7 +39,7 @@ ALL = union(CORE_TESTS, DISTR_TESTS, SAMPLER_TESTS, TRACE_TESTS)
 
 # test groups that should be executed
 TEST_GROUPS = ALL
-TEST_GROUPS = ["nuts.jl"]
+TEST_GROUPS = ["compiler.jl"]
 
 # Run tests
 path = dirname(@__FILE__)
