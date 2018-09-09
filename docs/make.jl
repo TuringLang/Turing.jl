413f34f8b7d24dd5476627d4bdc4a7fdc3b77010
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-08 20:04:01 -0700

    Travis settings updated

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using Documenter, Turing
+makedocs(
+    # format = :html,
+    sitename = "Turing.jl",
+    pages = [
+        "Home" => ["index.md",
+                   "get-started.md",
+                   "advanced.md",
+                   "contributing/guide.md",
+                   "contributing/style_guide.md",],
+        "Tutorials" => ["ex/0_Introduction.md"],
+        "API" => "functions.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/cpfiffer/Turing.jl.git",
+    julia = "1.0"
+)
