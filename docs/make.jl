a1dc66d17a17e917660508f96d39ee7c8189542b
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-09 09:28:20 -0700

    Travis fixes

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,6 @@
 using Documenter, Turing
 makedocs(
-<<<<<<< HEAD
-    # format = :html,
-=======
     format = :html,
->>>>>>> 27347ae4916a87ff9205bf45655e0028b3641f94
     sitename = "Turing.jl",
     pages = [
         "Home" => ["index.md",
@@ -16,11 +12,8 @@ makedocs(
         "API" => "functions.md"
     ]
 )
-<<<<<<< HEAD
 
 deploydocs(
     repo = "github.com/cpfiffer/Turing.jl.git",
     julia = "1.0"
 )
-=======
->>>>>>> 27347ae4916a87ff9205bf45655e0028b3641f94
