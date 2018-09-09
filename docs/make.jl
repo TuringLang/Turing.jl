90845ca751555932173a1e9667ee391d867ee8d5
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-09 10:54:10 -0700

    Move to Documenter.jl native HTML builds

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 using Documenter, Turing
 makedocs(
+    format = :html,
     sitename = "Turing.jl",
     pages = [
         "Home" => ["index.md",
@@ -8,11 +9,14 @@ makedocs(
                    "contributing/guide.md",
                    "contributing/style_guide.md",],
         "Tutorials" => ["ex/0_Introduction.md"],
-        "API" => "functions.md"
+        "API" => "api.md"
     ]
 )
 
 deploydocs(
     repo = "github.com/cpfiffer/Turing.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing,
     julia = "1.0"
 )
