f902d87cf9914757a5b69e75469860feb7f7f367
Martin Trapp <trapp.martin@gmail.com>
2018-09-12 16:07:20 +0100

    work in progress

--- test/compiler.jl/model_macro.jl
+++ test/compiler.jl/model_macro.jl
@@ -3,8 +3,10 @@ using Turing, Distributions, Test
 @model testmodel(x1, x2) = begin
     s ~ InverseGamma(2,3)
     m ~ Normal(0,sqrt(s))
+
     x1 ~ Normal(m, sqrt(s))
     x2 ~ Normal(m, sqrt(s))
+
 	return x1, x2
 end
 
