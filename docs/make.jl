8dfd198b134eb97a7465a6b76af7bdf05205ff58
Cameron Pfiffer <cpfiffer@gmail.com>
2018-09-11 21:04:41 -0700

    Set up doc/ directory to hold Documenter.jl generated documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,23 @@
 using Documenter, Turing
+
+docpath = joinpath(@__DIR__, "doc")
+
 makedocs(
     format = :html,
     sitename = "Turing.jl",
     pages = [
-        "Home" => ["index.md",
-                   "get-started.md",
+        "Home" => ["get-started.md",
                    "advanced.md",
                    "contributing/guide.md",
                    "contributing/style_guide.md",],
         "Tutorials" => ["ex/0_Introduction.md"],
         "API" => "api.md"
-    ]
+    ],
+    build = "doc"
 )
 
+# Unused at current (2018-09-11) as our documentation serving solution doesn't
+# play well with the deploydocs function.
 # deploydocs(
 #     repo = "github.com/cpfiffer/Turing.jl.git",
 #     target = "build",
