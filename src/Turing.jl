6ac0b5ff661d7389512a76e2b90e87c26bb603f9
Kai Xu <xukai921110@gmail.com>
2018-09-21 22:07:59 +0100

    move using into require

--- src/Turing.jl
+++ src/Turing.jl
@@ -31,10 +31,10 @@ import Distributions: sample
 import ForwardDiff: gradient
 using Flux: Tracker
 import MCMCChain: AbstractChains, Chains
-using DynamicHMC, LogDensityProblems
-using LogDensityProblems: AbstractLogDensityProblem, ValueGradient
-
 @init @require DynamicHMC="bbc10e6e-7c05-544b-b16e-64fede858acb" @eval begin
+    using DynamicHMC, LogDensityProblems
+    using LogDensityProblems: AbstractLogDensityProblem, ValueGradient
+
     struct FunctionLogDensity{F} <: AbstractLogDensityProblem
       dimension::Int
       f::F
