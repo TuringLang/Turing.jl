179c8a178e373fa0f01d23b6f0a4e1308a032946
[A[A[A[B[B[Bwilltebbutt <wt0881@my.bristol.ac.uk>
2018-10-08 21:25:18 -0400

    Remove a load of code and refactor what is still here

--- test/resample.jl/resample.jl
+++ test/resample.jl/resample.jl
@@ -4,16 +4,6 @@ using Turing
 using Test
 
 num_samples = Int(1e6)
-
 resSystematic = Turing.resampleSystematic( [0.3, 0.4, 0.3], num_samples )
-resStratified = Turing.resampleStratified( [0.3, 0.4, 0.3], num_samples )
-resMultinomial= Turing.resampleMultinomial( [0.3, 0.4, 0.3], num_samples )
-resResidual   = Turing.resampleResidual( [0.3, 0.4, 0.3], num_samples )
-Turing.resample( [0.3, 0.4, 0.3])
-resSystematic2=Turing.resample( [0.3, 0.4, 0.3], num_samples )
-
 @test sum(resSystematic .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
-@test sum(resSystematic2 .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
-@test sum(resStratified .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
-@test sum(resMultinomial .== 2) ≈ (num_samples * 0.4) atol=1e-2*num_samples
-@test sum(resResidual .== 2) ≈ (num_samples * 0.4) atol=1e-2*num_samples
+
