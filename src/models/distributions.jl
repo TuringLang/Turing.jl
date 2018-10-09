216a87c09ef060560ef1b679040654385c431dcc
Will Tebbutt <wt0881@my.bristol.ac.uk>
2018-10-09 09:49:58 -0400

    Some really minor changes (#578)

--- src/models/distributions.jl
+++ src/models/distributions.jl
@@ -1,6 +1,5 @@
 # No info
-struct Flat <: ContinuousUnivariateDistribution
-end
+struct Flat <: ContinuousUnivariateDistribution end
 
 Distributions.rand(d::Flat) = rand()
 Distributions.logpdf(d::Flat, x::T) where T<:Real= zero(x)
@@ -9,22 +8,21 @@ Distributions.maximum(d::Flat) = +Inf
 
 # For vec support
 Distributions.rand(d::Flat, n::Int) = Vector([rand() for _ = 1:n])
-Distributions.logpdf(d::Flat, x::Vector{T}) where T<:Real= zero(x)
+Distributions.logpdf(d::Flat, x::AbstractVector{<:Real}) = zero(x)
 
 
 # Pos
 struct FlatPos{T<:Real} <: ContinuousUnivariateDistribution
     l::T
-    FlatPos{T}(l::T) where T = new{T}(l)
 end
 
-FlatPos(l::T) where T<:Real = FlatPos{T}(l)
-
 Distributions.rand(d::FlatPos) = rand() + d.l
-Distributions.logpdf(d::FlatPos, x::T) where T<:Real = if x <= d.l -Inf else zero(x) end
+Distributions.logpdf(d::FlatPos, x::Real) = x <= d.l ? -Inf : zero(x)
 Distributions.minimum(d::FlatPos) = d.l
-Distributions.maximum(d::FlatPos) = +Inf
+Distributions.maximum(d::FlatPos) = Inf
 
 # For vec support
 Distributions.rand(d::FlatPos, n::Int) = Vector([rand() for _ = 1:n] .+ d.l)
-Distributions.logpdf(d::FlatPos, x::Vector{T}) where T<:Real = if any(x .<= d.l) -Inf else zero(x) end
+function Distributions.logpdf(d::FlatPos, x::AbstractVector{<:Real})
+    return any(x .<= d.l) ? -Inf : zero(x)
+end
