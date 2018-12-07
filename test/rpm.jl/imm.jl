80cbaea579cf301d467cf1cba651ff067e5cc984
Martin Trapp <trapp.martin@gmail.com>
2018-12-07 18:56:00 +0100

    infinite MM test, wip

--- test/rpm.jl/imm.jl
+++ test/rpm.jl/imm.jl
@@ -1,43 +1,42 @@
 using Turing
+using Test
+using Random
 
-data = [9172.0;9350.0;9483.0;9558.0;9775.0;10227.0;10406.0;16084.0;16170.0;18419.0;18552.0;18600.0;18927.0;19052.0;19070.0;19330.0;19343.0;19349.0;19440.0;19473.0;19529.0;19541.0;19547.0;19663.0;19846.0;19856.0;19863.0;19914.0;19918.0;19973.0;19989.0;20166.0;20175.0;20179.0;20196.0;20215.0;20221.0;20415.0;20629.0;20795.0;20821.0;20846.0;20875.0;20986.0;21137.0;21492.0;21701.0;21814.0;21921.0;21960.0;22185.0;22209.0;22242.0;22249.0;22314.0;22374.0;22495.0;22746.0;22747.0;22888.0;22914.0;23206.0;23241.0;23263.0;23484.0;23538.0;23542.0;23666.0;23706.0;23711.0;24129.0;24285.0;24289.0;24366.0;24717.0;24990.0;25633.0;26960.0;26995.0;32065.0;32789.0;34279.0]
-data /= 1e4
+Random.seed!(12)
 
-mu_0 = mean(data); 
-sigma_0 = 1/sqrt(0.635); 
-sigma_1 = sigma_0/15
+data = vcat(randn(10), randn(10) .+ 10, randn(10) .- 10)
 
-@model infiniteMixture(y; μ = mu_0, σ = sigma_0) = begin
+@model infiniteMixture(y) = begin
 
     # Number of observations.
     N = length(y)
-    H = Normal(μ, σ)
+    H = Normal(mean(y), 1.0)
 
     x = tzeros(Float64, N);
-    J = tzeros(Float64, N);
     z = tzeros(Int, N)
 
     K = 0
 
-    # define total mass
-    T = 1
-    surplus = T
-
+    α = 1.0
+    rpm = DirichletProcess(α)
+    cluster_counts = tzeros(Int, 0)
     for i in 1:N
-        ps = vcat(J[1:K] / T, surplus / T)
-        z[i] ~ Categorical(ps)
+
+        z[i] ~ ChineseRestaurantProcess(rpm, cluster_counts)
         if z[i] > K
             K = K + 1
-            J[K] ~ DirichletProcess(1.0, surplus)
             x[K] ~ H
-            surplus -= J[K]
+            push!(cluster_counts, 1)
+        else
+            cluster_counts[z[i]] += 1
         end
-        y[i] ~ Normal(x[z[i]], sigma_1)
+        y[i] ~ Normal(x[z[i]], 1.0)
     end
 
     return z
 end
 
-#sampler = SMC(50)
-#permutation = randperm(length(data))
-#results = sample(infiniteMixture(data[permutation]), sampler)
+mf = infiniteMixture(data)
+chn = sample(mf, PG(50, 100))
+
+@test_broken length(unique(last(chn[:z]))) == 3
