281ea1037b8037613011021f0c0987880f95a0c9
mohamed82008 <mohamed82008@gmail.com>
2018-12-09 09:53:10 +1100

    fix broken compiler.jl/explicit_ret.jl test

--- test/compiler.jl/explicit_ret.jl
+++ test/compiler.jl/explicit_ret.jl
@@ -14,7 +14,6 @@ mf = test_ex_rt()
 for alg = [HMC(2000, 0.2, 3), PG(20, 2000), SMC(10000), IS(10000), Gibbs(2000, PG(20, 1, :x), HMC(1, 0.2, 3, :y))]
   println("[explicit_ret.jl] testing $alg")
   chn = sample(mf, alg)
-  @test mean(chn[:x]) ≈ 9.0 atol=0.2
+  @test mean(chn[:x]) ≈ 10.0 atol=0.2
   @test mean(chn[:y]) ≈ 5.0 atol=0.2
-  @test mean(chn[:z]) ≈ 6.0 atol=0.2
 end
