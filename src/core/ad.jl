2644b815efd2d78c0844d475df6baf8fdb952408
mohamed82008 <mohamed82008@gmail.com>
2018-12-16 03:49:13 +0300

    cleanup

--- src/core/ad.jl
+++ src/core/ad.jl
@@ -30,7 +30,7 @@ getADtype(::Type{<:Hamiltonian{AD}}) where {AD} = AD
 gradient(
     θ::AbstractVector{<:Real},
     vi::VarInfo,
-    model,
+    model::Function,
     sampler::Union{Nothing, Sampler}=nothing,
 )
 
@@ -40,7 +40,7 @@ Computes the gradient of the log joint of `θ` for the model specified by
 @generated function gradient(
     θ::AbstractVector{<:Real},
     vi::VarInfo,
-    model,
+    model::Function,
     sampler::TS=nothing,
 ) where {TS <: Union{Nothing, Sampler}}
     if TS == Nothing
@@ -111,7 +111,7 @@ end
 gradient_reverse(
     θ::AbstractVector{<:Real},
     vi::VarInfo,
-    model,
+    model::Function,
     sampler::Union{Nothing, Sampler}=nothing,
 )
 
