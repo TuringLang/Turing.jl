3cec755caa27b216f0d1b2117bf2694054b11839
Cameron Pfiffer <cpfiffer@gmail.com>
2019-03-02 15:09:27 -0800

    Added serialization tests to save/resume suite.

--- test/io.jl/save_resume_chain.jl
+++ test/io.jl/save_resume_chain.jl
@@ -19,33 +19,39 @@ alg3 = Gibbs(500, PG(30, 10, :s), HMCDA(1, 500, 0.65, 0.05, :m))
 end
 
 chn1 = sample(gdemo([1.5, 2.0]), alg1; save_state=true)
-
 check_numerical(chn1, [:s, :m], [49/24, 7/6])
 
-chn1_resumed = resume(chn1, 1000)
+# Test chains read/write.
+write("chn.jlds", chn1)
+chn1 = read("chn.jlds", Chains)
 
+chn1_resumed = Turing.Inference.resume(chn1, 1000)
 check_numerical(chn1_resumed, [:s, :m], [49/24, 7/6])
 
 chn1_contd = sample(gdemo([1.5, 2.0]), alg1; resume_from=chn1)
-
 check_numerical(chn1_contd, [:s, :m], [49/24, 7/6])
 
 chn1_contd2 = sample(gdemo([1.5, 2.0]), alg1; resume_from=chn1, reuse_spl_n=1000)
-
 check_numerical(chn1_contd2, [:s, :m], [49/24, 7/6])
 
 chn2 = sample(gdemo([1.5, 2.0]), alg2; save_state=true)
-
 check_numerical(chn2, [:s, :m], [49/24, 7/6])
 
-chn2_contd = sample(gdemo([1.5, 2.0]), alg2; resume_from=chn2)
+# Test chains read/write.
+write("chn.jlds", chn2)
+chn2 = read("chn.jlds", Chains)
 
+chn2_contd = sample(gdemo([1.5, 2.0]), alg2; resume_from=chn2)
 check_numerical(chn2_contd, [:s, :m], [49/24, 7/6])
 
 chn3 = sample(gdemo([1.5, 2.0]), alg3; save_state=true)
-
 check_numerical(chn3, [:s, :m], [49/24, 7/6])
 
-chn3_contd = sample(gdemo([1.5, 2.0]), alg3; resume_from=chn3)
+# Test chains read/write.
+write("chn.jlds", chn3)
+chn3 = read("chn.jlds", Chains)
 
+chn3_contd = sample(gdemo([1.5, 2.0]), alg3; resume_from=chn3)
 check_numerical(chn3_contd, [:s, :m], [49/24, 7/6])
+
+rm("chn.jlds")
