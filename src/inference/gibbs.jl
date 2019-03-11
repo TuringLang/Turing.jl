b141a6c71a8609b0e9fdf406704835fd842278c8
Hong Ge <hg344@cam.ac.uk>
2019-03-11 10:38:58 +0000

    Rename `HamiltonianRobustInit` to `SampleFromUniform`. (#714)

--- src/inference/gibbs.jl
+++ src/inference/gibbs.jl
@@ -102,7 +102,7 @@ function sample(
     # Init parameters
     varInfo = if resume_from == nothing
         vi_ = VarInfo()
-        model(vi_, HamiltonianRobustInit())
+        model(vi_, SampleFromUniform())
         vi_
     else
         resume_from.info[:vi]
