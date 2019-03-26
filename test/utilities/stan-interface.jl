3abbb83963eaa11bec51721b4d9241cf0ee5bf4a
Hong Ge <hg344@cam.ac.uk>
2019-03-26 18:07:07 +0000

    Fix stan interface #729

--- test/utilities/stan-interface.jl
+++ test/utilities/stan-interface.jl
@@ -2,6 +2,6 @@ using CmdStan
 
 @testset "stan-interface.jl" begin
     chn = sample(gdemo_default, 2000, 1000, false, 1,
-        CmdStan.Adapt(), CmdStan.Hmc())
+        CmdStan.Adapt(), CmdStan.Hmc(stepsize_jitter=0))
     check_gdemo(chn)
 end
