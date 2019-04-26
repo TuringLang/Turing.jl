89ee5bdf36314abe018fc8f14d71c694d403b1d7
Kai Xu <xukai921110@gmail.com>
2019-04-26 23:33:57 +0100

    relax Sampler to AbstractSampler

--- src/utilities/io.jl
+++ src/utilities/io.jl
@@ -132,16 +132,17 @@ function flatten(names, value :: Array{Float64}, k :: String, v)
     return
 end
 
-function save(c::Chains, spl::Sampler, model, vi, samples)
+function save(c::Chains, spl::AbstractSampler, model, vi, samples)
     nt = NamedTuple{(:spl, :model, :vi, :samples)}((spl, model, deepcopy(vi), samples))
     return setinfo(c, merge(nt, c.info))
 end
 
 function resume(c::Chains, n_iter::Int)
     @assert !isempty(c.info) "[Turing] cannot resume from a chain without state info"
-    return sample(  c.info[:model],
-                    c.info[:spl].alg;    # this is actually not used
-                    resume_from=c,
-                    reuse_spl_n=n_iter
-                  )
+    return sample(
+        c.info[:model],
+        c.info[:spl].alg;    # this is actually not used
+        resume_from=c,
+        reuse_spl_n=n_iter
+    )
 end
