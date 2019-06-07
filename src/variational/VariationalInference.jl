07724079544f6d440d19bfaabfcba92bf4fdad68
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2019-06-07 16:51:45 +0100

    optimize! is no longer specialized to ADVI

--- src/variational/VariationalInference.jl
+++ src/variational/VariationalInference.jl
@@ -96,6 +96,66 @@ function grad!(vo::VariationalObjective, alg::VariationalInference{AD}, q, model
     DiffResults.gradient!(out, Tracker.grad(θ_tracked))
 end
 
+# TODO: this default constructor for `optimizer` is probably dangerous because of the `acc` field being an `IdDict`!!!
+function optimize!(vo::VariationalObjective, alg::VariationalInference{AD}, q::VariationalPosterior, model::Model, θ; optimizer = ADAGrad()) where AD
+    alg_name = alg_str(alg)
+    samples_per_step = alg.samples_per_step
+    max_iters = alg.max_iters
+
+    # number of previous gradients to use to compute `s` in adaGrad
+    stepsize_num_prev = 10
+    
+    # setup
+    # var_info = Turing.VarInfo()
+    # model(var_info, Turing.SampleFromUniform())
+    # num_params = size(var_info.vals, 1)
+    num_params = length(q)
+
+    # # buffer
+    # θ = zeros(2 * num_params)
+
+    # HACK: re-use previous gradient `acc` if equal in value
+    # Can cause issues if two entries have idenitical values
+    if θ ∉ keys(optimizer.acc)
+        vs = [v for v ∈ keys(optimizer.acc)]
+        idx = findfirst(w -> vcat(q.μ, q.ω) == w, vs)
+        if idx != nothing
+            @info "[$alg_name] Re-using previous optimizer accumulator"
+            θ .= vs[idx]
+        end
+    else
+        @info "[$alg_name] Already present in optimizer acc"
+    end
+    
+    diff_result = DiffResults.GradientResult(θ)
+
+    # TODO: in (Blei et al, 2015) TRUNCATED ADAGrad is suggested; this is not available in Flux.Optimise
+    # Maybe consider contributed a truncated ADAGrad to Flux.Optimise
+
+    i = 0
+    prog = PROGRESS[] ? ProgressMeter.Progress(max_iters, 1, "[$alg_name] Optimizing...", 0) : 0
+
+    time_elapsed = @elapsed while (i < max_iters) # & converged # <= add criterion? A running mean maybe?
+        # TODO: separate into a `grad(...)` call; need to manually provide `diff_result` buffers
+        # ForwardDiff.gradient!(diff_result, f, x)
+        grad!(vo, alg, q, model, θ, diff_result, samples_per_step)
+
+        # apply update rule
+        Δ = DiffResults.gradient(diff_result)
+        Δ = Optimise.apply!(optimizer, θ, Δ)
+        @. θ = θ - Δ
+        
+        Turing.DEBUG && @debug "Step $i" Δ DiffResults.value(diff_result) norm(DiffResults.gradient(diff_result))
+        PROGRESS[] && (ProgressMeter.next!(prog))
+
+        i += 1
+    end
+
+    @info time_elapsed
+
+    return θ
+end
+
 # distributions
 include("distributions.jl")
 
