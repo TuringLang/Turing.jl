336239b575938c9eb4065bddf0c975866f70f29b
KDr2 <zhuo@hexoasis.com>
2019-06-20 15:07:17 +0800

    append date string to job name

--- scripts/benchmark-on-ci.sh
+++ scripts/benchmark-on-ci.sh
@@ -6,6 +6,7 @@ BASE_BRANCH=master
 if [[ $TRAVIS == true ]]; then
     if [[ $TRAVIS_PULL_REQUEST == true ]]; then
         CURRENT_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+        exit 0 # avoid double run on commit and PR
     else
         CURRENT_BRANCH=$TRAVIS_BRANCH
     fi
@@ -17,7 +18,8 @@ COMMIT_MSG=$(git show -s --format="%s")
 
 SANTI_BR_NAME=$(echo $CURRENT_BRANCH | sed 's/\W/_/g')
 COMMIT_SHA=$(git rev-parse  HEAD)
-BM_JOB_NAME="BMCI-${SANTI_BR_NAME}-${COMMIT_SHA:0:7}"
+TIME=$(date +%Y%m%d%H%M%S)
+BM_JOB_NAME="BMCI-${SANTI_BR_NAME}-${COMMIT_SHA:0:7}-${TIME}"
 
 if [[ $COMMIT_MSG != *"[bm]"* ]]; then
     echo "skipping the benchmark jobs."
@@ -28,6 +30,7 @@ git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch --all --unshallow
 
 git clone https://github.com/TuringLang/TuringBenchmarks.git ../TuringBenchmarks
+# TEMP: use travis-ci branch of TuringBenchmarks
 git -C ../TuringBenchmarks checkout -b travis-ci origin/travis-ci
 
 unset JULIA_PROJECT
