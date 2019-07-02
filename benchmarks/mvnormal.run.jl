15a1c69f0b889597821398267cdf96ae1be5ca52
Hong Ge <hg344@cam.ac.uk>
2019-07-02 17:33:23 +0100

    Fix MvNormal

--- benchmarks/mvnormal.run.jl
+++ benchmarks/mvnormal.run.jl
@@ -1,34 +1,20 @@
 using Turing, TuringBenchmarks.TuringTools
 
-using Distributions: MvNormal, logpdf
-using ForwardDiff: gradient
-using AdvancedHMC
-
 # Define the target distribution and its gradient
 const D = 10
-const target = MvNormal(zeros(D), ones(D))
-logπ(θ::AbstractVector{<:Real}) = logpdf(target, θ)
-∂logπ∂θ(θ::AbstractVector{<:Real}) = gradient(logπ, θ)
+
+@model target(dim) = begin
+   Θ = Vector{Real}(undef, dim)
+   θ ~ MvNormal(zeros(D), ones(dim))
+end
 
 # Sampling parameter settings
 n_samples = 100_000
 n_adapts = 2_000
 
-# Initial points
-θ_init = randn(D)
-
-# Define metric space, Hamiltonian and sampling method
-metric = DenseEuclideanMetric(D)
-h = Hamiltonian(metric, logπ, ∂logπ∂θ)
-prop = Turing.NUTS(Leapfrog(find_good_eps(h, θ_init)))
-adaptor = StanHMCAdaptor(
-    n_adapts, Preconditioner(metric), NesterovDualAveraging(0.8, prop.integrator.ϵ))
-
 # Sampling
-# samples = sample(h, prop, θ_init, n_samples, adaptor, n_adapts)
 bench_res = @tbenchmark_expr("NUTS(Leapfrog(...))",
-                             sample(h, prop, θ_init, n_samples, adaptor, n_adapts))
+                             sample(target(D), HMC(n_samples, 0.1, 5)));
 
-# bench_res[4].names = ["phi[1]", "phi[2]", "phi[3]", "phi[4]"]
 LOG_DATA = build_log_data("MvNormal-Benchmark", bench_res...)
 print_log(LOG_DATA)
