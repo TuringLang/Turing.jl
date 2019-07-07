01fe845965f8b05b9c088d15be47f51ff7ee8b31
Cameron Pfiffer <cpfiffer@gmail.com>
2019-07-06 20:48:01 -0700

    DynamicHMC support.

--- src/contrib/inference/dynamichmc.jl
+++ src/contrib/inference/dynamichmc.jl
@@ -1,61 +1,68 @@
 ###
 ### DynamicHMC backend - https://github.com/tpapp/DynamicHMC.jl
 ###
-
 struct DynamicNUTS{AD, T} <: Hamiltonian{AD}
-    n_iters   ::  Integer   # number of samples
     space     ::  Set{T}    # sampling space, emtpy means all
 end
 
 """
-    DynamicNUTS(n_iters::Integer)
+    DynamicNUTS()
 
 Dynamic No U-Turn Sampling algorithm provided by the DynamicHMC package.
 To use it, make sure you have the DynamicHMC package installed.
 
 """
 DynamicNUTS(args...) = DynamicNUTS{ADBackend()}(args...)
-function DynamicNUTS{AD}(n_iters::Integer, space...) where AD
+function DynamicNUTS{AD}(space...) where AD
     _space = isa(space, Symbol) ? Set([space]) : Set(space)
-    DynamicNUTS{AD, eltype(_space)}(n_iters, _space)
+    DynamicNUTS{AD, eltype(_space)}(_space)
 end
 
-function Sampler(alg::DynamicNUTS{T}, s::Selector=Selector()) where T <: Hamiltonian
-  return Sampler(alg, Dict{Symbol,Any}(), s)
-end
+function sample(
+    rng::AbstractRNG,
+    model::Model,
+    spl::Sampler{<:DynamicNUTS},
+    N::Integer;
+    kwargs...
+)
+    samples = Array{Transition}(undef, N)
 
-function sample(model::Model,
-                alg::DynamicNUTS{AD},
-                ) where AD
-
-    spl = Sampler(alg)
-
-    n = alg.n_iters
-    samples = Array{Sample}(undef, n)
-    weight = 1 / n
-    for i = 1:n
-        samples[i] = Sample(weight, Dict{Symbol, Any}())
-    end
-
-    vi = VarInfo(model)
-    runmodel!(model, vi, SampleFromUniform())
+    runmodel!(model, spl.state.vi, SampleFromUniform())
 
     if spl.selector.tag == :default
-        link!(vi, spl)
-        runmodel!(model, vi, spl)
+        link!(spl.state.vi, spl)
+        runmodel!(model, spl.state.vi, spl)
     end
 
     function _lp(x)
-        value, deriv = gradient_logp(x, vi, model, spl)
+        value, deriv = gradient_logp(x, spl.state.vi, model, spl)
         return ValueGradient(value, deriv)
     end
 
-    chn_dynamic, _ = NUTS_init_tune_mcmc(FunctionLogDensity(length(vi[spl]), _lp), alg.n_iters)
+    chn_dynamic, _ = NUTS_init_tune_mcmc(
+        FunctionLogDensity(
+            length(spl.state.vi[spl]),
+            _lp
+        ),
+        N
+    )
 
-    for i = 1:alg.n_iters
-        vi[spl] = chn_dynamic[i].q
-        samples[i].value = Sample(vi, spl).value
+    spl.selector.tag == :default && invlink!(spl.state.vi, spl)
+
+    for i = 1:N
+        spl.selector.tag == :default && link!(spl.state.vi, spl)
+        spl.state.vi[spl] = chn_dynamic[i].q
+        spl.selector.tag == :default && invlink!(spl.state.vi, spl)
+        samples[i] = transition(spl)
     end
 
-    return Chain(0.0, samples)
+    return Chains(rng, model, spl, N, samples; kwargs...)
+end
+
+function Sampler(
+    alg::DynamicNUTS{AD},
+    model::Turing.Model,
+    s::Selector=Selector()
+) where AD
+    return Sampler(alg, Dict{Symbol,Any}(), s, BlankState(VarInfo(model)))
 end
--- test/inference/dynamichmc.jl
+++ test/inference/dynamichmc.jl
@@ -5,6 +5,6 @@ include(dir*"/test/test_utils/AllUtils.jl")
 
 @stage_testset "dynamichmc" "dynamichmc.jl" begin
     import DynamicHMC
-    chn = sample(gdemo_default, DynamicNUTS(2000));
+    chn = sample(gdemo_default, DynamicNUTS(), 5000);
     check_numerical(chn, [:s, :m], [49/24, 7/6], eps=0.2)
 end
