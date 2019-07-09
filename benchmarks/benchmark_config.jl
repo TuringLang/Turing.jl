85c713b77866b65a23a4fbeec952f58a12d41e8f
Hong Ge <hg344@cam.ac.uk>
2019-07-09 10:01:53 +0100

    Fix commit link and typos.

--- benchmarks/benchmark_config.jl
+++ benchmarks/benchmark_config.jl
@@ -20,9 +20,9 @@ BENCHMARK_FILES = map(BENCHMARK_FILES) do x joinpath(@__DIR__, x) end
 COMMENT_TEMPLATES = Dict{Symbol, String}()
 COMMENT_TEMPLATES[:CI_BM_COMPLETE_FOR_COMMIT] = """
 
-The benchmark job for this commit is complete. 
-Here is the benchmark [report]( {{{ :report_url }}}) and [commit]({{{ :report_repo }}}@{{{ :commit_id }}}). 
-The benchmark log (printted via `log_report`) can be found below.
+The benchmark job for this commit has completed. 
+Here is the benchmark [report]( {{{ :report_url }}}) and commit ({{{ :report_repo }}}@{{{ :commit_id }}}). 
+The benchmark log (printed via `log_report`) can be found below.
 
 ```
 {{#:report_logs}}
