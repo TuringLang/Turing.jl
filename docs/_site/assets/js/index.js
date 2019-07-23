d3341164b141572b522818b15273863611be570f
gold-developer <gold.denis@outlook.com>
2019-07-23 00:43:53 -0700

    skin update & responsive fix

---
+++ docs/_site/assets/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.md-header').attr('data-md-state', 'none');
+    
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 20) {
+            $('.md-header').attr('data-md-state', 'shadow');
+        } else {
+            $('.md-header').attr('data-md-state', 'none');
+        }
+    });
+});
