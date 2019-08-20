24770c9a5fcebd6a58f743a7119c37022794b381
KDr2 <zhuo@hexoasis.com>
2019-08-20 15:34:02 +0800

    version switch

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -4,12 +4,45 @@ excluded_in_search: true
 ---
 $(document).ready(function(){
     $('.md-header').attr('data-md-state', 'none');
-    
+
     $(window).scroll(function() {
         if ($(window).scrollTop() > 20) {
             $('.md-header').attr('data-md-state', 'shadow');
         } else {
             $('.md-header').attr('data-md-state', 'none');
         }
+    });
+
+    if (typeof(window.DOC_VERSIONS) == "undefined") {
+        window.DOC_VERSIONS = ["dev"];
+    }
+    var url_parts = /(.*:\/\/[^/]+\/)(.+?)(\/.*)/.exec(window.location.href);
+
+    $(".dropdown > a").text(url_parts[2]);
+    $.each(DOC_VERSIONS, function(index, value) {
+        if(value == url_parts[2]) {
+            // mobile
+            $("select#version-selector").append(
+                $('<option value="' + value + '" selected="selected">' + value + '</option>'));
+            return;
+        }
+        // desktop
+        $(".dropdown > div.dropdown-menu").append(
+            $('<a class="dropdown-item" href="#">' + value + '</a>'));
+        // mobile
+        $("select#version-selector").append(
+            $('<option value="' + value + '">' + value + '</option>'));
+    });
+
+    $(".dropdown > div.dropdown-menu > a").on("click", function() {
+        var target_version = $(this).text().trim();
+        if (target_version == url_parts[2]) return;
+        window.location.href = url_parts[1] + target_version + url_parts[3];
+    });
+
+    $("select#version-selector").change(function() {
+        var target_version = $("select#version-selector option:selected").text();
+        if (target_version == url_parts[2]) return;
+        window.location.href = url_parts[1] + target_version + url_parts[3];
     });
 });
