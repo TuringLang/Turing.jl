577c89b2729f3642c9fee91417f0b9791b56c708
David Widmann <dev+github@devmotion.de>
2019-11-17 21:57:12 +0100

    Remove unneeded broadcast operations

--- test/utilities/util.jl
+++ test/utilities/util.jl
@@ -11,12 +11,12 @@ include(dir*"/test/test_utils/AllUtils.jl")
     @test @VarName(s)[1:end-1] == (:s,())
     @test @VarName(x[1,2][1+5][45][3][i])[1:end-1] ==
         (:x,("[1, 2]","[6]","[45]","[3]","[1]"))
-    @test StatsFuns.logistic(1.1) == 1.0 / (exp.(-1.1) + 1.0)
+    @test StatsFuns.logistic(1.1) == 1.0 / (exp(-1.1) + 1.0)
     @test StatsFuns.logit(0.3) ≈ -0.8472978603872036 atol=1e-9
-    @test isnan.(StatsFuns.logit(1.0)) == false
-    @test isinf.(StatsFuns.logit(1.0)) == true
-    @test isnan.(StatsFuns.logit(0.)) == false
-    @test isinf.(StatsFuns.logit(0.)) == true
+    @test !isnan(StatsFuns.logit(1.0))
+    @test isinf(StatsFuns.logit(1.0))
+    @test !isnan(StatsFuns.logit(0.0))
+    @test isinf(StatsFuns.logit(0.0))
 
     @test Turing.Core.data(Dict(:y => 1, :x => 2, :z => 1), [:x, :y]) ==
         (x = 2, y = 1)
