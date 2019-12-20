90f5106654acdca2ed7e3beaf60f9a1977c9dbed
mohamed82008 <mohamed82008@gmail.com>
2019-12-20 18:42:20 +0200

    some more tests

--- test/core/prob_macro.jl
+++ test/core/prob_macro.jl
@@ -21,18 +21,25 @@ Random.seed!(129)
         logjoint = logprior + loglike
 
         @test logprob"m = mval | model = demo" == logprior
+        @test logprob"m = mval | x = xval, model = demo" == logprior
         @test logprob"x = xval | m = mval, model = demo" == loglike
         @test logprob"x = xval, m = mval | model = demo" == logjoint
 
         varinfo = Turing.VarInfo(demo(xval))
         @test logprob"m = mval | model = demo, varinfo = varinfo" == logprior
+        @test logprob"m = mval | x = xval, model = demo, varinfo = varinfo" == logprior
         @test logprob"x = xval | m = mval, model = demo, varinfo = varinfo" == loglike
         varinfo = Turing.VarInfo(demo(missing))
         @test logprob"x = xval, m = mval | model = demo, varinfo = varinfo" == logjoint
 
         chain = sample(demo(xval), IS(), iters)
+        chain2 = Chains(chain.value, chain.logevidence, chain.name_map, NamedTuple())
         lps = logpdf.(Normal.(vec(chain["m"].value), 1), xval)
-        @test logprob"x = xval | chain = chain, model = demo" == lps
+        @test logprob"x = xval | chain = chain" == lps
+        @test logprob"x = xval | chain = chain2, model = demo" == lps
+        varinfo = Turing.VarInfo(demo(xval))
+        @test logprob"x = xval | chain = chain, varinfo = varinfo" == lps
+        @test logprob"x = xval | chain = chain2, model = demo, varinfo = varinfo" == lps
     end
 
     @testset "vector" begin
@@ -61,7 +68,11 @@ Random.seed!(129)
         # Currently, we cannot easily pre-allocate `VarInfo` for vector data
 
         chain = sample(demo(xval), HMC(0.5, 1), iters)
+        chain2 = Chains(chain.value, chain.logevidence, chain.name_map, NamedTuple())
         lps = like.([[chain["m[$i]"].value[j] for i in 1:n] for j in 1:iters], Ref(xval))
-        @test logprob"x = xval | chain = chain, model = demo" == lps
+        @test logprob"x = xval | chain = chain" == lps
+        @test logprob"x = xval | chain = chain2, model = demo" == lps
+        @test logprob"x = xval | chain = chain, varinfo = varinfo" == lps
+        @test logprob"x = xval | chain = chain2, model = demo, varinfo = varinfo" == lps
     end
 end
