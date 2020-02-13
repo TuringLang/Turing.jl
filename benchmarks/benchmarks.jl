6eb54fab911f8cca6ef723a2d0b20e8f7dac51ea
Hong Ge <hg344@cam.ac.uk>
2020-02-13 22:11:38 +0000

    Update benchmarks.jl

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -3,6 +3,31 @@ using Turing, BenchmarkTools, BenchmarkHelper
 # TODO: only definite `suite` once for all benchmarks
 suite = BenchmarkGroup()
 
+## Dummny benchmarks
+
+suite["dummy"] = BenchmarkGroup(["dummy"])
+
+data = [0, 1, 0, 1, 1, 1, 1, 1, 1, 1]
+
+# log_report("Dummy Benchmark started!")
+
+@model constrained_test(obs) = begin
+    p ~ Beta(2,2)
+    for i = 1:length(obs)
+        obs[i] ~ Bernoulli(p)
+    end
+    p
+end
+
+# log_report("Dummy model constructed!")
+
+#BENCHMARK_RESULT = @benchmark_expr "HMC" sample(constrained_test(data),
+#                                                HMC(1.5, 3),
+#                                                1000)
+
+suite["dummy"]["dummy"] = @benchmarkable sample(constrained_test($data), HMC(0.01, 2), 2000)
+
+# log_report("Dummy benchmark finished!")
 
 ## gdemo
 
