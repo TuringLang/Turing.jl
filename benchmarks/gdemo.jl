6833c9833cc421f4658efee8a9037b6e1a456b6e
Hong Ge <hg344@cam.ac.uk>
2020-02-13 21:58:42 +0000

    Update gdemo.jl

--- benchmarks/gdemo.jl
+++ benchmarks/gdemo.jl
@@ -1,4 +1,9 @@
-using Turing, BenchmarkHelper
+using Turing, BenchmarkTools, BenchmarkHelper
+
+# TODO: only definite `suite` once for all benchmarks
+suite = BenchmarkGroup()
+
+suite["gdemo"] = BenchmarkGroup(["gdemo"])
 
 @model gdemo(x, y) = begin
     s ~ InverseGamma(2, 3)
@@ -8,8 +13,11 @@ using Turing, BenchmarkHelper
     return s, m
 end
 
-data = (1.5, 2.0)
+# BENCHMARK_RESULT = @benchmark_expr "NUTS" sample(gdemo(1.5, 2.0),
+#                                                 Turing.NUTS(1000, 0.65),
+#                                                 6000)
 
-BENCHMARK_RESULT = @benchmark_expr "NUTS" sample(gdemo(1.5, 2.0),
-                                                 Turing.NUTS(1000, 0.65),
-                                                 6000)
+suite["gdemo"] = @benchmarkable sample(gdemo(1.5, 2.0), HMC(0.01, 2), 2000)
+
+# Execute `run` from a benchmarking manager, e.g. Nanosoldier. 
+# run(suite)
