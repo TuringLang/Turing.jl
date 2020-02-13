9bcadc5d4e9f8f6e63737b8b8dce6ca648201758
Hong Ge <hg344@cam.ac.uk>
2020-02-13 22:08:10 +0000

    Update and rename gdemo.jl to benchmarks.jl

---
+++ benchmarks/benchmarks.jl
@@ -0,0 +1,76 @@
+using Turing, BenchmarkTools, BenchmarkHelper
+
+# TODO: only definite `suite` once for all benchmarks
+suite = BenchmarkGroup()
+
+
+## gdemo
+
+suite["gdemo"] = BenchmarkGroup(["gdemo"])
+
+@model gdemo(x, y) = begin
+    s ~ InverseGamma(2, 3)
+    m ~ Normal(0, sqrt(s))
+    x ~ Normal(m, sqrt(s))
+    y ~ Normal(m, sqrt(s))
+    return s, m
+end
+
+# BENCHMARK_RESULT = @benchmark_expr "NUTS" sample(gdemo(1.5, 2.0),
+#                                                 Turing.NUTS(1000, 0.65),
+#                                                 6000)
+
+suite["gdemo"]["hmc"] = @benchmarkable sample(gdemo(1.5, 2.0), HMC(0.01, 2), 2000)
+
+# Execute `run` from a benchmarking manager, e.g. Nanosoldier. 
+# run(suite)
+
+##
+## MvNormal
+##
+
+suite["mnormal"] = BenchmarkGroup(["mnormal"])
+
+# Define the target distribution and its gradient
+const D = 10
+
+@model target(dim) = begin
+   Θ = Vector{Real}(undef, dim)
+   θ ~ MvNormal(zeros(D), ones(dim))
+end
+
+# Sampling parameter settings
+n_samples = 100_000
+n_adapts = 2_000
+
+# Sampling
+#BENCHMARK_RESULT = @benchmark_expr("NUTS(Leapfrog(...))",
+#                                   sample(target(D), HMC(0.1, 5), n_samples));
+
+suite["mnormal"]["hmc"] = @benchmarkable sample(target($D), HMC(0.1, 5), $n_samples)
+
+## MvNormal: ForwardDiff vs BackwardDiff (Tracker)
+
+using LinearAlgebra
+
+@model mdemo(d, N) = begin
+    Θ = Vector(undef, N)
+   for n=1:N
+      Θ[n] ~ d
+   end
+end
+
+dim2 = 250
+A    = rand(Wishart(dim2, Matrix{Float64}(I, dim2, dim2)));
+d    = MvNormal(zeros(dim2), A)
+
+# ForwardDiff
+Turing.setadbackend(:forward_diff)
+# @benchmark chain = sample(mdemo(d, 1), HMC(0.1, 5), 5000)
+suite["mnormal"]["forward_diff"] = @benchmarkable sample(mdemo($d, 1), HMC(0.1, 5), 5000)
+
+
+#BackwardDiff
+Turing.setadbackend(:reverse_diff)
+# @benchmark chain = sample(mdemo(d, 1), HMC(0.1, 5), 5000)
+suite["mnormal"]["reverse_diff"] = @benchmarkable sample(mdemo($d, 1), HMC(0.1, 5), 5000)
