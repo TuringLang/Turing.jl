6472fc6539f0780f583d014a1ac25c04d637fddf
David Widmann <devmotion@users.noreply.github.com>
2020-03-27 10:13:03 +0100

    Run Github actions only once per day (#1189)
    * Run CompatHelper only once a day
    
    * Run TagBot only once per day
    
    * Set seed in DynamicHMC tests

--- test/contrib/inference/dynamichmc.jl
+++ test/contrib/inference/dynamichmc.jl
@@ -1,10 +1,12 @@
 using Turing, Test
+import Random
 
 dir = splitdir(splitdir(pathof(Turing))[1])[1]
 include(dir*"/test/test_utils/AllUtils.jl")
 
 @stage_testset "dynamichmc" "dynamichmc.jl" begin
     import DynamicHMC
+    Random.seed!(100)
     chn = sample(gdemo_default, DynamicNUTS(), 5000);
     check_numerical(chn, [:s, :m], [49/24, 7/6], atol=0.2)
 end
