0a769b772e5e379270f55678af25f57f5242a559
Cameron Pfiffer <cpfiffer@gmail.com>
2020-05-18 20:29:31 -0700

    Update tests & address comments

--- src/modes/ModeEstimation.jl
+++ src/modes/ModeEstimation.jl
@@ -2,8 +2,11 @@ using ..Turing
 using ..Bijectors
 using LinearAlgebra
 
+import ..AbstractMCMC: AbstractSampler
 import ..DynamicPPL
-import ..DynamicPPL: Model, AbstractContext, VarInfo
+import ..DynamicPPL: Model, AbstractContext, VarInfo, AbstractContext, VarName,
+    _getindex, getsym, getfield, settrans!,  setorder!,
+    get_and_set_val!, istrans, tilde, dot_tilde
 import Optim
 import Optim: optimize
 import NamedArrays
@@ -17,6 +20,101 @@ struct MLE end
 struct MAP end
 
 """
+    OptimizationContext{C<:AbstractContext} <: AbstractContext
+
+The `OptimizationContext` transforms variables to their constrained space, but
+does not use the density with respect to the transformation. This context is
+intended to allow an optimizer to sample in R^n freely.
+"""
+struct OptimizationContext{C<:AbstractContext} <: AbstractContext
+	context::C
+end
+
+# assume
+function DynamicPPL.tilde(ctx::OptimizationContext{<:LikelihoodContext}, spl, dist, vn::VarName, inds, vi)
+    if haskey(vi, vn)
+        # Always overwrite the parameters with new ones for `SampleFromUniform`.
+        if spl isa SampleFromUniform || is_flagged(vi, vn, "del")
+            unset_flag!(vi, vn, "del")
+            r = init(dist, spl)
+            vi[vn] = vectorize(dist, r)
+            settrans!(vi, false, vn)
+            setorder!(vi, vn, get_num_produce(vi))
+        else
+            r = vi[vn]
+        end
+    else
+        r = init(dist, spl)
+        push!(vi, vn, r, dist, spl)
+        settrans!(vi, false, vn)
+    end
+	return r, 0
+end
+
+function DynamicPPL.tilde(ctx::OptimizationContext, spl, dist, vn::VarName, inds, vi)
+    if haskey(vi, vn)
+        # Always overwrite the parameters with new ones for `SampleFromUniform`.
+        if spl isa SampleFromUniform || is_flagged(vi, vn, "del")
+            unset_flag!(vi, vn, "del")
+            r = init(dist, spl)
+            vi[vn] = vectorize(dist, r)
+            settrans!(vi, false, vn)
+            setorder!(vi, vn, get_num_produce(vi))
+        else
+            r = vi[vn]
+        end
+    else
+        r = init(dist, spl)
+        push!(vi, vn, r, dist, spl)
+        settrans!(vi, false, vn)
+    end
+    return r, Distributions.logpdf(dist, r)
+end
+
+# observe
+function DynamicPPL.tilde(ctx::OptimizationContext{<:PriorContext}, sampler, right, left, vi)
+    return 0
+end
+
+function DynamicPPL.tilde(ctx::OptimizationContext, sampler, dist, value, vi)
+    return Distributions.logpdf(dist, value)
+end
+
+# dot assume
+function DynamicPPL.dot_tilde(ctx::OptimizationContext{<:LikelihoodContext}, sampler, right, left, vn::VarName, _, vi)
+    vns, dist = get_vns_and_dist(right, left, vn)
+    r = get_and_set_val!(vi, vns, dist, sampler)
+    return r, 0
+end
+
+function DynamicPPL.dot_tilde(ctx::OptimizationContext, sampler, right, left, vn::VarName, _, vi)
+    vns, dist = get_vns_and_dist(right, left, vn)
+    r = get_and_set_val!(vi, vns, dist, sampler)
+    lp = sum(logpdf(dist, r))
+    return r, lp
+end
+
+# dot observe
+function DynamicPPL.dot_tilde(ctx::OptimizationContext{<:PriorContext}, sampler, right, left, vn, _, vi)
+    return 0
+end
+
+function DynamicPPL.dot_tilde(ctx::OptimizationContext{<:PriorContext}, sampler, right, left, vi)
+    return 0
+end
+
+function DynamicPPL.dot_tilde(ctx::OptimizationContext, sampler, right, left, vn, _, vi)
+    vns, dist = get_vns_and_dist(right, left, vn)
+    r = get_and_set_val!(vi, vns, dist, sampler)
+    lp = sum(logpdf(dist, r))
+    return lp
+end
+
+function DynamicPPL.dot_tilde(ctx::OptimizationContext, sampler, dists, value, vi)
+    return sum(Distributions.logpdf.(dists, value))
+end
+
+"""
     OptimLogDensity{M<:Model,C<:Context,V<:VarInfo}
 
 A struct that stores the log density function of a `DynamicPPL` model.
@@ -27,7 +125,9 @@ struct OptimLogDensity{M<:Model,C<:AbstractContext,V<:VarInfo}
     "A `DynamicPPL.AbstractContext` used to evaluate the model. `LikelihoodContext` or `DefaultContext` are typical for MAP/MLE."
     context::C
     "A `DynamicPPL.VarInfo` struct that will be used to update model parameters."
-	init::V
+    vi::V
+    "Whether the to evaluate the model with transformed variables."
+    linked::Bool
 end
 
 """
@@ -35,10 +135,10 @@ end
 
 Create a callable `OptimLogDensity` struct that evaluates a model using the given `context`.
 """
-function OptimLogDensity(model::Model, context::AbstractContext)
-	init = VarInfo(model)
-	DynamicPPL.link!(init, SampleFromPrior())
-	return OptimLogDensity(model, context, init)
+function OptimLogDensity(model::Model, context::AbstractContext, linked::Bool)
+	init = VarInfo(model, context)
+	linked && DynamicPPL.link!(init, DynamicPPL.SampleFromPrior())
+	return OptimLogDensity(model, context, init, linked)
 end
 
 """
@@ -47,21 +147,33 @@ end
 Evaluate the log joint (with `DefaultContext`) or log likelihood (with `LikelihoodContext`)
 at the array `z`. If `unlinked=true`, no change of variables will occur.
 """
-function (f::OptimLogDensity)(z; unlinked::Bool = false)
+function (f::OptimLogDensity)(z; linked::Bool = true)
     spl = DynamicPPL.SampleFromPrior()
 
-    varinfo = DynamicPPL.VarInfo(f.init, spl, z)
+    varinfo = DynamicPPL.VarInfo(f.vi, spl, z)
+    f.model(varinfo, spl, f.context)
+    return -DynamicPPL.getlogp(varinfo)
+end
 
-    if unlinked
-        DynamicPPL.invlink!(f.init, spl)
-        f.model(varinfo, spl, f.context)
-        lp = -DynamicPPL.getlogp(varinfo)
-        DynamicPPL.link!(f.init, spl)
-        return lp
-    else
-        f.model(varinfo, spl, f.context)
-        return -DynamicPPL.getlogp(varinfo)
-    end
+"""
+    unlink(f::OptimLogDensity)
+
+Generate an unlinked (with no variable transformions) version of an existing `OptimLogDensity`.
+"""
+function unlink(f::OptimLogDensity)
+    init = VarInfo(f.model, f.context)
+    return OptimLogDensity(f.model, f.context, init, false)
+end
+
+"""
+    link(f::OptimLogDensity)
+
+Generate an linked (with variable transformions) version of an existing `OptimLogDensity`.
+"""
+function link(f::OptimLogDensity)
+    init = VarInfo(f.model, f.context)
+    DynamicPPL.link!(init, DynamicPPL.SampleFromPrior())
+    return OptimLogDensity(f.model, f.context, init, false)
 end
 
 """
@@ -88,7 +200,6 @@ struct ModeResult{
     "The evaluation function used to calculate the output."
     f :: M
 end
-
 #############################
 # Various StatsBase methods #
 #############################
@@ -111,8 +222,8 @@ end
 function StatsBase.informationmatrix(m::ModeResult; hessian_function=ForwardDiff.hessian, kwargs...)
     # Calculate Hessian and information matrix.
     varnames = StatsBase.coefnames(m)
-    info = hessian_function(x -> m.f(x, unlinked=true), m.values.array[:, 1])
-    info = inv(info)
+    f = unlink(m.f)
+    info = inv(hessian_function(x -> f(x), m.values.array[:, 1]))
     return NamedArrays.NamedArray(info, (varnames, varnames))
 end
 
@@ -147,7 +258,8 @@ mle = optimize(model, MLE(), NelderMeade())
 ```
 """
 function Optim.optimize(model::Model, ::MLE, args...; kwargs...)
-    return optimize(model, OptimLogDensity(model, DynamicPPL.LikelihoodContext()), args...; kwargs...)
+    ctx = OptimizationContext(DynamicPPL.LikelihoodContext())
+    return optimize(model, OptimLogDensity(model, ctx, true), args...; kwargs...)
 end
 
 """
@@ -171,7 +283,8 @@ map_est = optimize(model, MAP(), NelderMeade())
 ```
 """
 function Optim.optimize(model::Model, ::MAP, args...; kwargs...)
-    return optimize(model, OptimLogDensity(model, DynamicPPL.DefaultContext()), args...; kwargs...)
+    ctx = OptimizationContext(DynamicPPL.DefaultContext())
+    return optimize(model, OptimLogDensity(model, ctx, true), args...; kwargs...)
 end
 
 """
@@ -181,26 +294,21 @@ Estimate a mode, i.e., compute a MLE or MAP estimate.
 """
 function Optim.optimize(model::Model, f::OptimLogDensity, optimizer=Optim.LBFGS(), args...; kwargs...)
     # Do some initialization.
-    b = bijector(model)
-    binv = inv(b)
-
     spl = DynamicPPL.SampleFromPrior()
-    vi = DynamicPPL.VarInfo(model)
-    init_params = model(vi, spl)
-    init_vals = vi[spl]
+    init_params = model(f.vi, spl)
+    init_vals = f.vi[spl]
 
     # Optimize!
-    M = Optim.optimize(f, init_vals, args...; kwargs...)
-
-    # Retrieve the estimated values.
-    vals = binv(M.minimizer)
+    M = Optim.optimize(f, init_vals, optimizer, args...; kwargs...)
 
     # Get the VarInfo at the MLE/MAP point, and run the model to ensure 
     # correct dimensionality.
-    vi[spl] = vals
+    f.vi[spl] = M.minimizer
+    invlink!(f.vi, spl)
+    vals = f.vi[spl]
 
     # Make one transition to get the parameter names.
-    ts = [Turing.Inference.Transition(DynamicPPL.tonamedtuple(vi), DynamicPPL.getlogp(vi))]
+    ts = [Turing.Inference.Transition(DynamicPPL.tonamedtuple(f.vi), DynamicPPL.getlogp(f.vi))]
     varnames, _ = Turing.Inference._params_to_array(ts)
 
     # Store the parameters and their names in an array.
--- test/modes/ModeEstimation.jl
+++ test/modes/ModeEstimation.jl
@@ -5,6 +5,7 @@ using StatsBase
 using NamedArrays
 using ReverseDiff
 using Random
+using LinearAlgebra
 
 dir = splitdir(splitdir(pathof(Turing))[1])[1]
 include(dir*"/test/test_utils/AllUtils.jl")
@@ -17,10 +18,12 @@ include(dir*"/test/test_utils/AllUtils.jl")
         m2 = optimize(gdemo_default, MLE(), NelderMead())
         m3 = optimize(gdemo_default, MLE(), Newton())
 
-        @test all(isapprox.(m1.values.array - [0.0625031, 1.75], 0.0, atol=0.01))
-        @test all(isapprox.(m2.values.array - [0.0625031, 1.75], 0.0, atol=0.01))
-        @test all(isapprox.(m3.values.array - [0.0625031, 1.75], 0.0, atol=0.01))
+        true_value = [0.0625031, 1.75]
+        @test all(isapprox.(m1.values.array - true_value, 0.0, atol=0.01))
+        @test all(isapprox.(m2.values.array - true_value, 0.0, atol=0.01))
+        @test all(isapprox.(m3.values.array - true_value, 0.0, atol=0.01))
     end
+
     @testset "MAP" begin
         Random.seed!(222)
         
@@ -28,9 +31,10 @@ include(dir*"/test/test_utils/AllUtils.jl")
         m2 = optimize(gdemo_default, MAP(), NelderMead())
         m3 = optimize(gdemo_default, MAP(), Newton())
         
-        @test all(isapprox.(m1.values.array - [49/24, 7/6], 0.0, atol=0.01))
-        @test all(isapprox.(m2.values.array - [49/24, 7/6], 0.0, atol=0.01))
-        @test all(isapprox.(m3.values.array - [49/24, 7/6], 0.0, atol=0.01))
+        true_value = [0.0625031, 1.75]
+        @test all(isapprox.(m1.values.array - true_value, 0.0, atol=0.01))
+        @test all(isapprox.(m2.values.array - true_value, 0.0, atol=0.01))
+        @test all(isapprox.(m3.values.array - true_value, 0.0, atol=0.01))
     end
 
     @testset "StatsBase integration" begin
@@ -55,5 +59,47 @@ include(dir*"/test/test_utils/AllUtils.jl")
         @test vcov(mle_est) == informationmatrix(mle_est)
 
         @test isapprox(loglikelihood(mle_est), -0.0652883561466624, atol=0.01)
+    end
+
+    @testset "Linear regression test" begin
+        @model function regtest(x, y)
+            beta ~ MvNormal(2,1)
+            mu = x*beta
+            y ~ MvNormal(mu, 1.0)
+        end
+        
+        Random.seed!(987)
+        true_beta = [1.0, -2.2]
+        x = rand(40, 2)
+        y = x*true_beta
+        
+        model = regtest(x, y)
+        mle = optimize(model, MLE())
+        
+        vcmat = inv(x'x)
+        vcmat_mle = informationmatrix(mle).array
+        
+        @test isapprox(mle.values.array, true_beta)
+        @test isapprox(vcmat, vcmat_mle)
+    end
+
+    @testset "Dot tilde test" begin
+        @model function dot_gdemo(x)
+            s ~ InverseGamma(2,3)
+            m ~ Normal(0, sqrt(s))
+        
+            x .~ Normal(m, sqrt(s))
+        end
+        
+        model_dot = dot_gdemo([1.5, 2.0])
+
+        mle1 = optimize(gdemo_default, MLE())
+        mle2 = optimize(model_dot, MLE())
+
+        map1 = optimize(gdemo_default, MAP())
+        map2 = optimize(model_dot, MAP())
+
+        @test isapprox(mle1.values.array, mle2.values.array)
+        @test isapprox(map1.values.array, map2.values.array)
     end
 end
