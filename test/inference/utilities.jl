a23b6f6f36c7d8c67065cf684c66c41b0961eea1
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-07-17 23:04:27 +0200

    added test for predict with model containing multivariate variable

--- test/inference/utilities.jl
+++ test/inference/utilities.jl
@@ -11,6 +11,11 @@ using Random
         end
     end
 
+    @model function linear_reg_vec(x, y, σ = 0.1)
+        β ~ Normal(0, 1)
+        y ~ MvNormal(β .* x, σ)
+    end
+
     f(x) = 2 * x + 0.1 * randn()
 
     Δ = 0.1
@@ -28,4 +33,11 @@ using Random
     ys_pred = vec(mean(Array(group(predictions, :y)); dims = 1))
 
     @test sum(abs2, ys_test - ys_pred) ≤ 0.1
+
+    # Predict on two last indices for vectorized
+    m_lin_reg_test = linear_reg_vec(xs_test, missing);
+    predictions_vec = Turing.Inference.predict(m_lin_reg_test, chain_lin_reg)
+    ys_pred_vec = vec(mean(Array(group(predictions_vec, :y)); dims = 1))
+
+    @test sum(abs2, ys_test - ys_pred_vec) ≤ 0.1
 end
