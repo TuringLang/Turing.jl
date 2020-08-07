896cf2d3e5d58bd15c6e23bff999ae6edda56f13
Hong Ge <hg344@cam.ac.uk>
2020-08-08 00:00:32 +0100

    Update advi.jl

--- src/variational/advi.jl
+++ src/variational/advi.jl
@@ -104,7 +104,7 @@ end
 
 
 function AdvancedVI.vi(model::Model, alg::ADVI, q::TransformedDistribution{<:TuringDiagMvNormal}; optimizer = TruncatedADAGrad())
-    Turing.DEBUG && @debug "Optimizing ADVI..."
+    @debug "Optimizing ADVI..."
     # Initial parameters for mean-field approx
     μ, σs = params(q)
     θ = vcat(μ, invsoftplus.(σs))
