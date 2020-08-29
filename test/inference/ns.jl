0f133765c5d79d37d9a3518c57f8acb73bc52296
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-08-29 17:46:47 +0530

    Update ns.jl

--- test/inference/ns.jl
+++ test/inference/ns.jl
@@ -1,32 +1,24 @@
 using Turing, Random, Test
 import Turing.Inference
+import NestedSamplers
 
 dir = splitdir(splitdir(pathof(Turing))[1])[1]
 include(dir*"/test/test_utils/AllUtils.jl")
 
 @testset "ns.jl" begin
-    @turing_testset "NS constructor" begin
+    @testset "gdemo" begin
         Random.seed!(1729)
-        N = 1000
-        s = NS()
-        @test DynamicPPL.alg_str(Sampler(s, gdemo_default)) == "NS"
-        c = sample(gdemo_default, s, N)
         
-        ## within Gibbs as well ??
-        ## s1 = Gibbs(NS(:m), NS(:s))
-        ## c1 = sample(gdemo_default, s1, N)
-    end
-    
-    @numerical_testset "NS inference" begin
-        Random.seed!(729)
-        alg = NS()
-        chain = sample(gdemo_default, alg, 1500)
-        check_gdemo(chain, atol = 0.1)
-
-        ## within Gibbs as well ??
-        ## NS within Gibbs
-        ## alg = Gibbs(NS(:m), NS(:s))
-        ## chain = sample(gdemo_default, alg, 1500)
-        ## check_gdemo(chain, atol = 0.1)
+        N = 1000
+        ndims = 3
+        nactive = 100
+        bound_type = NestedSamplers.Bounds.Ellipsoid
+        proposal_type = NestedSamplers.Proposals.Uniform()
+        
+        spl = NS(ndims, nactive, bound_type, proposal_type)
+        @test DynamicPPL.alg_str(Sampler(spl, gdemo_default)) == "NS"
+        
+        chain = sample(gdemo_default, spl, N)
+        check_gdemo(chain)
     end
 end
