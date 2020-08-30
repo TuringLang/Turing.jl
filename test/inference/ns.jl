43d89ee3352d75d52d1f5d25845a395df288277c
Saranjeet Kaur <kaur.saranjeet3@gmail.com>
2020-08-30 23:02:36 +0530

    spl -> s in test/inference/ns.jl

--- test/inference/ns.jl
+++ test/inference/ns.jl
@@ -15,10 +15,10 @@ include(dir*"/test/test_utils/AllUtils.jl")
         bound_type = NestedSamplers.Bounds.Ellipsoid
         proposal_type = NestedSamplers.Proposals.Uniform()
         
-        spl = NS(ndims, nactive, bound_type, proposal_type)
-        @test DynamicPPL.alg_str(Sampler(spl, gdemo_default)) == "NS"
+        s = NS(ndims, nactive, bound_type, proposal_type)
+        @test DynamicPPL.alg_str(Sampler(s, gdemo_default)) == "NS"
         
-        chain = sample(gdemo_default, spl, N)
+        chain = sample(gdemo_default, s, N)
         check_gdemo(chain)
     end
 end
