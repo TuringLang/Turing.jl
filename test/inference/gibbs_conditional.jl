bf62f0940eebfcef80518bf61ae31e892e9483b1
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-11-10 21:34:30 +0100

    Integrate suggestions from David

--- test/inference/gibbs_conditional.jl
+++ test/inference/gibbs_conditional.jl
@@ -6,45 +6,42 @@ include(dir*"/test/test_utils/AllUtils.jl")
 
 
 @turing_testset "gibbs conditionals" begin
-    let N = 1000,
-        (α_0, θ_0) = (2.0, inv(3.0)),
-        λ_true = rand(Gamma(α_0, θ_0)),
-        σ_true = sqrt(1 / λ_true),
-        m_true = rand(Normal(0, σ_true)),
+    Random.seed!(100)
+    
+    @turing_testset "gdemo" begin
+        N = 1000
+        (α_0, θ_0) = (2.0, inv(3.0))
+        λ_true = rand(Gamma(α_0, θ_0))
+        σ_true = sqrt(1 / λ_true)
+        m_true = rand(Normal(0, σ_true))
         x = rand(Normal(m_true, σ_true), N)
+
+        # The conditionals and posterior can be formulated in terms of the following statistics:
+        x_bar = mean(x) # sample mean
+        s2 = var(x; mean=x_bar, corrected=false) # sample variance
+        m_n = N * x_bar / (N + 1)
         
         @model function inverse_gdemo(x)
             λ ~ Gamma(α_0, θ_0)
-            m ~ Normal(0, sqrt(1 / λ))
-            @. x ~ $(Normal(m, sqrt(1 / λ)))
-        end
-
-        function gdemo_statistics(x)
-            # The conditionals and posterior can be formulated in terms of the following statistics:
-            n = length(x) # number of samples
-            x_bar = mean(x) # sample mean
-            s2 = var(x; mean=x_bar, corrected=false) # sample variance
-            return n, x_bar, s2
+            σ = sqrt(1 / λ)
+            m ~ Normal(0, σ)
+            @. x ~ $(Normal(m, σ))
         end
 
         function cond_m(c)
-            n, x_bar, s2 = gdemo_statistics(x)
-            m_n = N * x_bar / (n + 1)
-            λ_n = c.λ * (n + 1)
+            λ_n = c.λ * (N + 1)
             σ_n = sqrt(1 / λ_n)
             return Normal(m_n, σ_n)
         end
 
         function cond_λ(c)
-            n, x_bar, s2 = gdemo_statistics(x)
-            α_n = α_0 + (n - 1) / 2 + 1
-            β_n = s2 * n / 2 + c.m^2 / 2 + inv(θ_0)
+            α_n = α_0 + (N - 1) / 2 + 1
+            β_n = s2 * N / 2 + c.m^2 / 2 + inv(θ_0)
             return Gamma(α_n, inv(β_n))
         end
 
         # Three tests: one for each variable fixed to the true value, and one for both
         # using the conditional
-        Random.seed!(100)
         for alg in (Gibbs(GibbsConditional(:m, cond_m),
                           GibbsConditional(:λ, c -> Normal(λ_true, 0))),
                     Gibbs(GibbsConditional(:m, c -> Normal(m_true, 0)),
@@ -56,21 +53,24 @@ include(dir*"/test/test_utils/AllUtils.jl")
         end
     end
 
-    Random.seed!(100)
-    let π = [0.5, 0.5],
-        K = length(π),
-        m = 0.5,
-        λ = 2.0,
-        σ = 0.1,
-        N = 20,
-        μ_true = [rand(Normal(m, λ)) for _ in π],
-        z_true = [rand(Categorical(π)) for _ in 1:N],
-        x = [rand(Normal(μ_true[z_n], σ)) for z_n in z_true]
+    
+
+    @turing_testset "GMM" begin
+        π = [0.5, 0.5] # cluster weights
+        K = length(π)  # number of clusters
+        m = 0.5 # prior mean
+        s = 2.0 # prior variance
+        σ = 0.1 # observation variance
+        N = 20  # number of observations
+        
+        μ_true = rand(Normal(m, s), K)
+        z_true = rand(Categorical(π), N)
+        x = rand(MvNormal(μ_true[z_true], σ))
         
         @model function mixture(x)
-            μ ~ arraydist(Normal.(fill(m, K), fill(λ, K)))
-            z ~ arraydist(Categorical.(fill(π, N)))
-            x ~ arraydist(Normal.(μ[z], σ))
+            μ ~ MvNormal(fill(m, K), s)
+            z ~ filldist(Categorical(π), N)
+            x ~ MvNormal(μ[z], σ)
             return x
         end
 
@@ -89,10 +89,10 @@ include(dir*"/test/test_utils/AllUtils.jl")
 
             # If there were no observations assigned to center `k`, `n[k] == 0`, and
             # we use the prior instead.
-            λ_hat = [(n[k] != 0) ? inv(n[k] / σ^2 + 1/λ^2) : λ for k = 1:K]
-            μ_hat = [(n[k] != 0) ? (sum(x[z .== k]) / σ^2) * λ_hat[k] : m for k = 1:K]
+            s_hat = [(n[k] != 0) ? inv(n[k] / σ^2 + 1/s^2) : s for k = 1:K]
+            μ_hat = [(n[k] != 0) ? (sum(x[z .== k]) / σ^2) * s_hat[k] : m for k = 1:K]
 
-            return arraydist(Normal.(μ_hat, λ_hat))
+            return MvNormal(μ_hat, s_hat)
         end
 
         estimate(chain, var) = dropdims(mean(Array(group(chain, var)), dims=1), dims=1)
@@ -100,9 +100,9 @@ include(dir*"/test/test_utils/AllUtils.jl")
             z = Int.(Array(group(chain, var)))
             return map(i -> findmax(counts(z[:,i], range))[2], 1:size(z,2))
         end        
+
+        lμ_true, uμ_true = extrema(μ_true)
         
-        # Both variables sampled using the Gibbs conditional
-        Random.seed!(100)
         for alg in (Gibbs(GibbsConditional(:z, cond_z), GibbsConditional(:μ, cond_μ)),
                     Gibbs(GibbsConditional(:z, cond_z), MH(:μ)),
                     Gibbs(GibbsConditional(:z, cond_z), HMC(0.01, 7, :μ)), )
@@ -111,7 +111,6 @@ include(dir*"/test/test_utils/AllUtils.jl")
             
             μ_hat = estimate(chain, :μ)
             lμ_hat, uμ_hat = extrema(μ_hat)
-            lμ_true, uμ_true = minimum(μ_true), maximum(μ_true)
             @test isapprox([lμ_true, uμ_true], [lμ_hat, uμ_hat], atol=0.1)
             
             z_hat = estimatez(chain, :z, 1:2)
