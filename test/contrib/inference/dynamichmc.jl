cec4d98f0340f474ebc3a9394bf65fc0ad91bd2c
David Widmann <dev+git@devmotion.de>
2020-12-22 22:59:05 +0100

    Increase number of samples

--- test/contrib/inference/dynamichmc.jl
+++ test/contrib/inference/dynamichmc.jl
@@ -10,12 +10,12 @@ include(dir*"/test/test_utils/AllUtils.jl")
 
     @test DynamicPPL.alg_str(Sampler(DynamicNUTS(), gdemo_default)) == "DynamicNUTS"
 
-    chn = sample(gdemo_default, DynamicNUTS(), 5_000)
+    chn = sample(gdemo_default, DynamicNUTS(), 10_000)
     check_gdemo(chn)
 
-    chn2 = sample(gdemo_default, Gibbs(PG(3, :s), DynamicNUTS(:m)), 5_000)
+    chn2 = sample(gdemo_default, Gibbs(PG(15, :s), DynamicNUTS(:m)), 10_000)
     check_gdemo(chn2)
 
-    chn3 = sample(gdemo_default, Gibbs(DynamicNUTS(:s), ESS(:m)), 5_000)
+    chn3 = sample(gdemo_default, Gibbs(DynamicNUTS(:s), ESS(:m)), 10_000)
     check_gdemo(chn3)
 end
