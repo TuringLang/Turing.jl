277a5f10f447ddc3cf9867e35e00e1e54e100234
Hong Ge <3279477+yebai@users.noreply.github.com>
2022-12-06 18:00:28 +0000

    Apply suggestions from code review

--- test/inference/Inference.jl
+++ test/inference/Inference.jl
@@ -58,7 +58,7 @@
         chn1 = sample(gdemo_default, alg1, 5000; save_state=true)
         check_gdemo(chn1)
 
-        chn1_resumed = Turing.Inference.resume(chn1, 2000)
+        chn1_resumed = Turing.Inference.resume(chn1, 5000)
         check_gdemo(chn1_resumed)
 
         chn1_contd = sample(gdemo_default, alg1, 5000; resume_from=chn1)
@@ -67,10 +67,10 @@
         chn1_contd2 = sample(gdemo_default, alg1, 5000; resume_from=chn1, reuse_spl_n=1000)
         check_gdemo(chn1_contd2)
 
-        chn2 = sample(gdemo_default, alg2, 2000; save_state=true)
+        chn2 = sample(gdemo_default, alg2, 5000; save_state=true)
         check_gdemo(chn2)
 
-        chn2_contd = sample(gdemo_default, alg2, 2000; resume_from=chn2)
+        chn2_contd = sample(gdemo_default, alg2, 5000; resume_from=chn2)
         check_gdemo(chn2_contd)
 
         chn3 = sample(gdemo_default, alg3, 10_000; save_state=true)
