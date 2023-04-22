1fd08f18ee8aeed91b2d30e6d4acbb7fb7f201d4
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-04-22 01:14:42 +0100

    reduced number of iterations for emcee to see if that fixes tests

--- test/inference/emcee.jl
+++ test/inference/emcee.jl
@@ -12,7 +12,8 @@
         check_gdemo(chain)
 
         # https://github.com/TuringLang/Turing.jl/pull/1976
-        n_samples = 100_000
+        @info "Testing emcee with large number of iterations"
+        n_samples = 10_000
         chain = sample(gdemo_default, spl, n_samples)
         check_gdemo(chain)
     end
