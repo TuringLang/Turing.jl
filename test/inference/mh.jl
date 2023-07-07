af5018815806dd73c3febb9ed0e8932a920f27cb
jaimerz <jaimerz011235813@gmail.com>
2023-07-07 09:43:55 +0100

    externalsampler() in tests

--- test/inference/mh.jl
+++ test/inference/mh.jl
@@ -18,10 +18,10 @@
         s4 = Gibbs(MH(:m), MH(:s))
         c4 = sample(gdemo_default, s4, N)
 
-        s5 = RWMH(gdemo_default)
+        s5 = externalsampler(RWMH(gdemo_default))
         c5 = sample(gdemo_default, s5, N)
 
-        s6 = StaticMH(gdemo_default)
+        s6 = externalsampler(StaticMH(gdemo_default))
         c6 = sample(gdemo_default, s6, N)
     end
     @numerical_testset "mh inference" begin
