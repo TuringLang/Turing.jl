5522c6c63a882b5a173cbf243fa94fa517cb33fb
jaimerz <jaimerz011235813@gmail.com>
2023-07-27 16:06:07 +0100

    fixing tests

--- test/contrib/inference/dynamichmc.jl
+++ test/contrib/inference/dynamichmc.jl
@@ -2,9 +2,9 @@
     import DynamicHMC
     Random.seed!(100)
 
-    @test DynamicPPL.alg_str(Sampler(DynamicNUTS(), gdemo_default)) == "DynamicNUTS"
+    @test DynamicPPL.alg_str(Sampler(externalsampler(DynamicHMC.NUTS()))) == "DynamicNUTS"
 
-    spl = externalsampler(DynamicNUTS())
+    spl = externalsampler(DynamicHMC.NUTS())
     chn = sample(gdemo_default, spl, 10_000)
     check_gdemo(chn)
 end
