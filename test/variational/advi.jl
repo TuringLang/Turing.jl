97353691a8d5d68b36a313acb6051bcd4658316e
Ray Kim <msca8h@naver.com>
2023-08-26 22:43:14 +0100

    fix test for advi alternative interface

--- test/variational/advi.jl
+++ test/variational/advi.jl
@@ -20,29 +20,19 @@
         check_gdemo(chn, atol = 0.5)
     end
 
-    @turing_testset "advi different interfaces" begin
+    @turing_testset "advi user supplied q interface" begin
         Random.seed!(1234)
 
-        target = MvNormal(zeros(2), I)
-        logπ(z) = logpdf(target, z)
-        advi = ADVI(10, 1000)
+        opt = Adam()
+        Random.seed!(1)
+        N = 500
 
-        # Using a function z ↦ q(⋅∣z)
-        getq(θ) = TuringDiagMvNormal(θ[1:2], exp.(θ[3:4]))
-        q = vi(logπ, advi, getq, randn(4))
+        q = meanfield(model)
+        alg = ADVI(10)
+        q = vi(gdemo_default, alg, q, 5000; optimizer = opt)
+        samples = transpose(rand(q, N))
+        chn = Chains(reshape(samples, size(samples)..., 1), ["s", "m"])
 
-        xs = rand(target, 10)
-        @test mean(abs2, logpdf(q, xs) - logpdf(target, xs)) ≤ 0.07
-
-        # OR: implement `update` and pass a `Distribution`
-        function AdvancedVI.update(d::TuringDiagMvNormal, θ::AbstractArray{<:Real})
-            return TuringDiagMvNormal(θ[1:length(q)], exp.(θ[length(q) + 1:end]))
-        end
-
-        q0 = TuringDiagMvNormal(zeros(2), ones(2))
-        q = vi(logπ, advi, q0, randn(4))
-
-        xs = rand(target, 10)
-        @test mean(abs2, logpdf(q, xs) - logpdf(target, xs)) ≤ 0.05
+        check_gdemo(chn, atol = 0.5)
     end
 end
