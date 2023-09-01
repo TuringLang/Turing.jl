24ce611df1deae078063f827456d2313c8c6b1db
Hong Ge <3279477+yebai@users.noreply.github.com>
2023-09-01 19:06:24 +0000

    remove obsolete utility module

--- src/utilities/Utilities.jl
+++
@@ -1,9 +0,0 @@
-module Utilities
-
-using DynamicPPL: AbstractSampler, Sampler
-using DynamicPPL: init, inittrans, reconstruct, reconstruct!, vectorize
-using Distributions, Bijectors
-using StatsFuns, SpecialFunctions
-import Distributions: sample
-
-end # module
