0856783a68d2af51cefcd4269b0475b185a6f720
Xianda Sun <sunxdt@gmail.com>
2024-01-30 16:50:36 +0000

    Put `getADbackend` back in and renamed

--- src/mcmc/Inference.jl
+++ src/mcmc/Inference.jl
@@ -96,17 +96,18 @@ Wrap a sampler so it can be used as an inference algorithm.
 """
 externalsampler(sampler::AbstractSampler) = ExternalSampler(sampler)
 
+getADType(spl::Sampler) = getADType(spl.alg)
+getADType(::SampleFromPrior) = AutoForwardDiff(; chunksize=0)
+
+getADType(ctx::DynamicPPL.SamplingContext) = getADType(ctx.sampler)
+getADType(ctx::DynamicPPL.AbstractContext) = getADType(DynamicPPL.NodeTrait(ctx), ctx)
+getADType(::DynamicPPL.IsLeaf, ctx::DynamicPPL.AbstractContext) = AutoForwardDiff(; chunksize=0)
+getADType(::DynamicPPL.IsParent, ctx::DynamicPPL.AbstractContext) = getADType(DynamicPPL.childcontext(ctx))
+
+getADType(alg::Hamiltonian) = alg.adtype
+
 function LogDensityProblemsAD.ADgradient(ℓ::LogDensityFunction)
-    adtype = if DynamicPPL.hassampler(ℓ)
-        alg = getsampler(ℓ).alg
-        @assert alg isa InferenceAlgorithm
-        if alg isa Hamiltonian
-            return alg.adtype
-        else
-            return AutoForwardDiff(; chunksize=0)
-        end
-    end
-    return LogDensityProblemsAD.ADgradient(adtype, ℓ)
+    return LogDensityProblemsAD.ADgradient(getADType(ℓ.context), ℓ)
 end
 
 function LogDensityProblems.logdensity(
