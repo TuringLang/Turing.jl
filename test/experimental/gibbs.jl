b1dcadf664f4c417e3716c33defa71bb08a15a93
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-01-30 16:59:41 +0000

    relax atol for a numerical test with Gibbs a bit

--- test/experimental/gibbs.jl
+++ test/experimental/gibbs.jl
@@ -133,7 +133,7 @@ end
 
         # `sample`
         chain = sample(model, alg, 10_000; progress=false)
-        check_numerical(chain, [:s, :m], [49 / 24, 7 / 6], atol = 0.1)
+        check_numerical(chain, [:s, :m], [49 / 24, 7 / 6], atol = 0.2)
 
         # Without `m` as random.
         model = gdemo(1.5, 2.0) | (m = 7 / 6,)
