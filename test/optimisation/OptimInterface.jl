a462970d58d531a5a913541196461187ee76380d
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-04-19 20:27:44 +0100

    dirichlet onles has a unique mode for alpha > 1...

--- test/optimisation/OptimInterface.jl
+++ test/optimisation/OptimInterface.jl
@@ -228,9 +228,9 @@ end
 
     # Issue: https://discourse.julialang.org/t/turing-mixture-models-with-dirichlet-weightings/112910
     @testset "with different linked dimensionality" begin
-        @model demo_dirichlet() = x ~ Dirichlet(ones(3))
+        @model demo_dirichlet() = x ~ Dirichlet(2 * ones(3))
         model = demo_dirichlet()
         result = optimize(model, MAP())
-        @test result.values ≈ fill(1/3, 3) atol=0.2
+        @test result.values ≈ mode(Dirichlet(2 * ones(3))) atol=0.2
     end
 end
