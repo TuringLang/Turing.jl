17ab13d3007727efa333b3e4ec23a3f4822eea18
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-04-28 11:36:43 +0100

    Improvements to `externalsampler` (#2204)
    * added option of specifying if an `ExternalSampler` requires linking
    and choice of adtype
    
    * use `default_adtype` everywhere to avoid accidental incorrect choices
    
    * fixed incorrect argument order
    
    * fixed incorrect reference
    
    * Update src/mcmc/Inference.jl
    
    * remove usage of `chunksize=0` in favour of `chunksize=nothing`, as is
    default in ADTypes.jl
    
    * fixed incorrect refrences in externalsampler stepp
    
    * added tests for new kwargrs to externalsampler
    
    * removed accidental import
    
    * made `default_adtype()` a const `DEFAULT_ADTYPE` instead
    
    * chagned constructor for ExternalSampler to allow type-stable construction
    
    * fixed docstring
    
    * use `Val` type to make constructor of `ExternalSampler` type-stable
    
    * error if not val with true or false is provided
    
    * handle `initial_params` properly in `step` for `externalsampler`
    
    * forgot to link initial params
    
    * test setting of initial params in external samplers
    
    * fixed NUTS externalsampler test
    
    * test initial parameters for external samplers properly
    
    * losen tests a bit
    
    * Apply suggestions from code review
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/mcmc/Inference.jl
+++ src/mcmc/Inference.jl
@@ -79,29 +79,73 @@ abstract type StaticHamiltonian <: Hamiltonian end
 abstract type AdaptiveHamiltonian <: Hamiltonian end
 
 """
-    ExternalSampler{S<:AbstractSampler}
+    ExternalSampler{S<:AbstractSampler,AD<:ADTypes.AbstractADType,Unconstrained}
+
+Represents a sampler that is not an implementation of `InferenceAlgorithm`.
+
+The `Unconstrained` type-parameter is to indicate whether the sampler requires unconstrained space.
 
 # Fields
 $(TYPEDFIELDS)
 """
-struct ExternalSampler{S<:AbstractSampler} <: InferenceAlgorithm
+struct ExternalSampler{S<:AbstractSampler,AD<:ADTypes.AbstractADType,Unconstrained} <: InferenceAlgorithm
     "the sampler to wrap"
     sampler::S
+    "the automatic differentiation (AD) backend to use"
+    adtype::AD
+
+    """
+        ExternalSampler(sampler::AbstractSampler, adtype::ADTypes.AbstractADType, ::Val{unconstrained})
+
+    Wrap a sampler so it can be used as an inference algorithm.
+
+    # Arguments
+    - `sampler::AbstractSampler`: The sampler to wrap.
+    - `adtype::ADTypes.AbstractADType`: The automatic differentiation (AD) backend to use.
+    - `unconstrained::Val=Val{true}()`: Value type containing a boolean indicating whether the sampler requires unconstrained space.
+    """
+    function ExternalSampler(
+        sampler::AbstractSampler,
+        adtype::ADTypes.AbstractADType,
+       ::Val{unconstrained}=Val(true)
+    ) where {unconstrained}
+        if !(unconstrained isa Bool)
+            throw(ArgumentError("Expected Val{true} or Val{false}, got Val{$unconstrained}"))
+        end
+        return new{typeof(sampler),typeof(adtype),unconstrained}(sampler, adtype)
+    end
 end
 
 """
-    externalsampler(sampler::AbstractSampler)
+    requires_unconstrained_space(sampler::ExternalSampler)
+
+Return `true` if the sampler requires unconstrained space, and `false` otherwise.
+"""
+requires_unconstrained_space(::ExternalSampler{<:Any,<:Any,Unconstrained}) where {Unconstrained} = Unconstrained
+
+"""
+    externalsampler(sampler::AbstractSampler; adtype=AutoForwardDiff(), unconstrained=true)
 
 Wrap a sampler so it can be used as an inference algorithm.
+
+# Arguments
+- `sampler::AbstractSampler`: The sampler to wrap.
+
+# Keyword Arguments
+- `adtype::ADTypes.AbstractADType=ADTypes.AutoForwardDiff()`: The automatic differentiation (AD) backend to use.
+- `unconstrained::Bool=true`: Whether the sampler requires unconstrained space.
 """
-externalsampler(sampler::AbstractSampler) = ExternalSampler(sampler)
+function externalsampler(sampler::AbstractSampler; adtype=Turing.DEFAULT_ADTYPE, unconstrained::Bool=true)
+    return ExternalSampler(sampler, adtype, Val(unconstrained))
+end
+
 
 getADType(spl::Sampler) = getADType(spl.alg)
-getADType(::SampleFromPrior) = AutoForwardDiff(; chunksize=0)
+getADType(::SampleFromPrior) = Turing.DEFAULT_ADTYPE
 
 getADType(ctx::DynamicPPL.SamplingContext) = getADType(ctx.sampler)
 getADType(ctx::DynamicPPL.AbstractContext) = getADType(DynamicPPL.NodeTrait(ctx), ctx)
-getADType(::DynamicPPL.IsLeaf, ctx::DynamicPPL.AbstractContext) = AutoForwardDiff(; chunksize=0)
+getADType(::DynamicPPL.IsLeaf, ctx::DynamicPPL.AbstractContext) = Turing.DEFAULT_ADTYPE
 getADType(::DynamicPPL.IsParent, ctx::DynamicPPL.AbstractContext) = getADType(DynamicPPL.childcontext(ctx))
 
 getADType(alg::Hamiltonian) = alg.adtype
