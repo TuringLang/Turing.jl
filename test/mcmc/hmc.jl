56f64ec5909cec4a5ded4e28555c2b289020bbe1
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-05-07 18:40:53 +0100

    Use `values_as_in_model` to extract the parameters from a `Transition` (#2202)
    * use `values_as_in_model` to extract the parameters from a `Transition`
    rather than `invlink` + `values_as`
    
    * bump BangBang compat entry
    
    * added test from #2195 + added HypothesisTests.jl so we can compare
    chains properly
    
    * deepcopy varinfo before calling `values_as_in_model` to avoid mutating
    the original logprob computations, etc.
    
    * bump patch version
    
    * fixed tests
    
    ---------
    
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- test/mcmc/hmc.jl
+++ test/mcmc/hmc.jl
@@ -261,4 +261,46 @@
             @test mean(Array(chain)) ≈ 0.2
         end
     end
+
+    @turing_testset "issue: #2195" begin
+        @model function buggy_model()
+            lb ~ Uniform(0, 1)
+            ub ~ Uniform(1.5, 2)
+
+            # HACK: Necessary to avoid NUTS failing during adaptation.
+            try
+                x ~ transformed(Normal(0, 1), inverse(Bijectors.Logit(lb, ub)))
+            catch e
+                if e isa DomainError
+                    Turing.@addlogprob! -Inf
+                    return nothing
+                else
+                    rethrow()
+                end
+            end
+        end
+
+        model = buggy_model();
+        num_samples = 1_000;
+
+        chain = sample(
+            model,
+            NUTS(),
+            num_samples;
+            initial_params=[0.5, 1.75, 1.0]
+        )
+        chain_prior = sample(model, Prior(), num_samples)
+        
+        # Extract the `x` like this because running `generated_quantities` was how
+        # the issue was discovered, hence we also want to make sure that it works.
+        results = generated_quantities(model, chain)
+        results_prior = generated_quantities(model, chain_prior)
+
+        # Make sure none of the samples in the chains resulted in errors.
+        @test all(!isnothing, results)
+
+        # The discrepancies in the chains are in the tails, so we can't just compare the mean, etc.
+        # KS will compare the empirical CDFs, which seems like a reasonable thing to do here.
+        @test pvalue(ApproximateTwoSampleKSTest(vec(results), vec(results_prior))) > 0.01
+    end
 end
