7c4368e3c71586036bec64a05f70d7d343248c8f
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-07-15 08:32:35 +0100

    Always reconstruct `ADGradientWrapper` using the `adype` available in `ExternalSampler`

--- src/mcmc/abstractmcmc.jl
+++ src/mcmc/abstractmcmc.jl
@@ -25,11 +25,8 @@ Return the `DynamicPPL.Model` wrapped in the given log-density function `f`.
 getmodel(f::LogDensityProblemsAD.ADGradientWrapper) = getmodel(parent(f))
 getmodel(f::DynamicPPL.LogDensityFunction) = f.model
 
-# FIXME: We'll have to overload this for every AD backend since some of the AD backends
-# will cache certain parts of a given model, e.g. the tape, which results in a discrepancy
-# between the primal (forward) and dual (backward).
 """
-    setmodel(f, model)
+    setmodel(f, model[, adtype])
 
 Set the `DynamicPPL.Model` in the given log-density function `f` to `model`.
 
@@ -38,8 +35,20 @@ Set the `DynamicPPL.Model` in the given log-density function `f` to `model`.
     `DynamicPPL.LogDensityFunction`, performing an update of the `model` in `f`
     might require recompilation of the gradient tape, depending on the AD backend.
 """
-function setmodel(f::LogDensityProblemsAD.ADGradientWrapper, model::DynamicPPL.Model)
-    return Accessors.@set f.ℓ = setmodel(f.ℓ, model)
+function setmodel(
+    f::LogDensityProblemsAD.ADGradientWrapper,
+    model::DynamicPPL.Model,
+    adtype::ADTypes.AbstractADType
+)
+    # TODO: Should we handle `SciMLBase.NoAD`?
+    # For an `ADGradientWrapper` we do the following:
+    # 1. Update the `Model` in the underlying `LogDensityFunction`.
+    # 2. Re-construct the `ADGradientWrapper` using `ADgradient` using the provided `adtype`
+    #    to ensure that the recompilation of gradient tapes, etc. also occur. For example,
+    #    ReverseDiff.jl in compiled mode will cache the compiled tape, which means that just
+    #    replacing the corresponding field with the new model won't be sufficient to obtain
+    #    the correct gradients.
+    return LogDensityProblemsAD.ADgradient(adtype, setmodel(parent(f), model))
 end
 function setmodel(f::DynamicPPL.LogDensityFunction, model::DynamicPPL.Model)
     return Accessors.@set f.model = model
@@ -88,7 +97,7 @@ function recompute_logprob!!(
 )
     # Re-using the log-density function from the `state` and updating only the `model` field,
     # since the `model` might now contain different conditioning values.
-    f = setmodel(state.logdensity, model)
+    f = setmodel(state.logdensity, model, sampler.alg.adtype)
     # Recompute the log-probability with the new `model`.
     state_inner = recompute_logprob!!(
         rng, AbstractMCMC.LogDensityModel(f), sampler.alg.sampler, state.state
