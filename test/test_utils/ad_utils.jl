e50d8c553f6fd6a9fb96e27e3eabe76d029952bd
Markus Hauru <mhauru@turing.ac.uk>
2024-07-17 16:48:25 +0100

    Turn Tapir's safe mode off

--- test/test_utils/ad_utils.jl
+++ test/test_utils/ad_utils.jl
@@ -15,7 +15,7 @@ install_tapir = isdefined(Turing, :AutoTapir)
 if install_tapir
     # TODO(mhauru) Is there a better way to install optional dependencies like this?
     Pkg.add("Tapir")
-    push!(adbackends, Turing.AutoTapir())
+    push!(adbackends, Turing.AutoTapir(false))
 end
 
 end
