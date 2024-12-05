6ea2f189cf230f629f6812560292f35fd458ddaa
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-12-05 08:18:41 +0000

    added initial impl of ext for MarginalLogDensities.jl

---
+++ ext/TuringMarginalLogDensitiesExt.jl
@@ -0,0 +1,38 @@
+module TuringMarginalLogDensitiesExt
+
+using Turing: Turing, DynamicPPL
+using MarginalLogDensities: MarginalLogDensities
+
+
+# Use a struct for this to avoid closure overhead.
+struct Drop2ndArgAndFlipSign{F}
+    f::F
+end
+
+(f::Drop2ndArg)(x, _) = -f.f(x)
+
+
+function Turing.marginalize(
+    model::DynamicPPL.Model,
+    varnames::Vector,
+    method::MarginalLogDensities.AbstractMarginalizer=MarginalLogDensities.LaplaceApprox()
+)
+    # Determine the indices for the variables to marginalise out.
+    varinfo = DynamicPPL.typed_varinfo(model)
+    varindices = DynamicPPL.getranges(varinfo, varnames)
+    # Construct the marginal log-density model.
+    # Use linked `varinfo` to that we're working in unconstrained space and `OptimizationContext` to ensure
+    # that the log-abs-det jacobian terms are not included.
+    context = Turing.Optimisation.OptimizationContext(DynamicPPL.leafcontext(model.context))
+    varinfo_linked = DynamicPPL.link(varinfo, model)
+    f = Base.Fix1(
+        LogDensityProblems.logdensity,
+        DynamicPPL.LogDensityFunction(varinfo_linked, model, context)
+    )
+    # HACK: need the sign-flip here because `OptimizationContext` is a hacky impl which
+    # represent the _negative_ log-density.
+    mdl = MarginalLogDensity(
+        Drop2ndArgAndFlipSign(f), varinfo_linked[:], varindices, (), method
+    )
+    return mdl
+end
--- src/Turing.jl
+++ src/Turing.jl
@@ -58,6 +58,8 @@ using .Optimisation
 include("experimental/Experimental.jl")
 include("deprecated.jl") # to be removed in the next minor version release
 
+include("extensions.jl")
+
 ###########
 # Exports #
 ###########
