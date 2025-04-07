4c4a73dfbd809e4a306eb9235d0cf9d759fcb4aa
Documenter.jl <documenter@juliadocs.github.io>
2025-04-07 23:23:01 +0000

    build based on fc32e10

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -722,6 +722,9 @@ function runSearchMainCode() {
   };
 
   $(document).on("click", ".search-filter", function () {
+    let search_input = $(".documenter-search-input");
+    let cursor_position = search_input[0].selectionStart;
+
     if ($(this).hasClass("search-filter-selected")) {
       selected_filter = "";
     } else {
@@ -730,6 +733,9 @@ function runSearchMainCode() {
 
     // This updates search results and toggles classes for UI:
     update_search();
+
+    search_input.focus();
+    search_input.setSelectionRange(cursor_position, cursor_position);
   });
 
   /**
