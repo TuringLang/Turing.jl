62e9e2b035738583fa1d21886d5ebdbc6b9b82c5
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-05-20 22:11:52 +0100

    Delete benchmarks
    Fix https://github.com/TuringLang/Turing.jl/issues/2554

--- benchmarks/models/lr_helper.jl
+++
@@ -1,9 +0,0 @@
-using DelimitedFiles
-
-function readlrdata()
-    fname = joinpath(dirname(@__FILE__), "lr_nuts.data")
-    z = readdlm(fname)
-    x = z[:, 1:(end - 1)]
-    y = z[:, end] .- 1
-    return x, y
-end
