d8f6dfa10bb6842202f4ba9c4527d07d63231051
Penelope Yong <penelopeysm@gmail.com>
2025-05-23 12:36:36 +0100

    Remove AD backend loops

--- test/mcmc/sghmc.jl
+++ test/mcmc/sghmc.jl
@@ -12,14 +12,14 @@ import Mooncake
 using Test: @test, @testset
 using Turing
 
-@testset "Testing sghmc.jl with $adbackend" for adbackend in ADUtils.adbackends
+@testset "Testing sghmc.jl" begin
     @testset "sghmc constructor" begin
-        alg = SGHMC(; learning_rate=0.01, momentum_decay=0.1, adtype=adbackend)
+        alg = SGHMC(; learning_rate=0.01, momentum_decay=0.1, adtype=Turing.DEFAULT_ADTYPE)
         @test alg isa SGHMC
         sampler = Turing.Sampler(alg)
         @test sampler isa Turing.Sampler{<:SGHMC}
 
-        alg = SGHMC(; learning_rate=0.01, momentum_decay=0.1, adtype=adbackend)
+        alg = SGHMC(; learning_rate=0.01, momentum_decay=0.1, adtype=Turing.DEFAULT_ADTYPE)
         @test alg isa SGHMC
         sampler = Turing.Sampler(alg)
         @test sampler isa Turing.Sampler{<:SGHMC}
@@ -27,20 +27,20 @@ using Turing
     @testset "sghmc inference" begin
         rng = StableRNG(123)
 
-        alg = SGHMC(; learning_rate=0.02, momentum_decay=0.5, adtype=adbackend)
+        alg = SGHMC(; learning_rate=0.02, momentum_decay=0.5, adtype=Turing.DEFAULT_ADTYPE)
         chain = sample(rng, gdemo_default, alg, 10_000)
         check_gdemo(chain; atol=0.1)
     end
 end
 
-@testset "Testing sgld.jl with $adbackend" for adbackend in ADUtils.adbackends
+@testset "Testing sgld.jl" begin
     @testset "sgld constructor" begin
-        alg = SGLD(; stepsize=PolynomialStepsize(0.25), adtype=adbackend)
+        alg = SGLD(; stepsize=PolynomialStepsize(0.25), adtype=Turing.DEFAULT_ADTYPE)
         @test alg isa SGLD
         sampler = Turing.Sampler(alg)
         @test sampler isa Turing.Sampler{<:SGLD}
 
-        alg = SGLD(; stepsize=PolynomialStepsize(0.25), adtype=adbackend)
+        alg = SGLD(; stepsize=PolynomialStepsize(0.25), adtype=Turing.DEFAULT_ADTYPE)
         @test alg isa SGLD
         sampler = Turing.Sampler(alg)
         @test sampler isa Turing.Sampler{<:SGLD}
