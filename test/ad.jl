0d2955fbb9cbb495e9f8eb1eb659f8f92b580f28
Penelope Yong <penelopeysm@gmail.com>
2025-05-28 01:05:28 +0100

    Finish AD tests

--- test/ad.jl
+++ test/ad.jl
@@ -7,14 +7,200 @@ using DynamicPPL.TestUtils.AD: run_ad
 using StableRNGs: StableRNG
 using Test
 using ..Models: gdemo_default
-using ..ADUtils: ADTypeCheckContext, adbackends
+
+"""Element types that are always valid for a VarInfo regardless of ADType."""
+const always_valid_eltypes = (AbstractFloat, AbstractIrrational, Integer, Rational)
+
+"""A dictionary mapping ADTypes to the element types they use."""
+const eltypes_by_adtype = Dict(
+    Turing.AutoForwardDiff => (ForwardDiff.Dual,),
+    Turing.AutoReverseDiff => (
+        ReverseDiff.TrackedArray,
+        ReverseDiff.TrackedMatrix,
+        ReverseDiff.TrackedReal,
+        ReverseDiff.TrackedStyle,
+        ReverseDiff.TrackedType,
+        ReverseDiff.TrackedVecOrMat,
+        ReverseDiff.TrackedVector,
+    ),
+    Turing.AutoMooncake => (Mooncake.CoDual,),
+)
+
+"""
+    AbstractWrongADBackendError
+
+An abstract error thrown when we seem to be using a different AD backend than expected.
+"""
+abstract type AbstractWrongADBackendError <: Exception end
+
+"""
+    WrongADBackendError
+
+An error thrown when we seem to be using a different AD backend than expected.
+"""
+struct WrongADBackendError <: AbstractWrongADBackendError
+    actual_adtype::Type
+    expected_adtype::Type
+end
+
+function Base.showerror(io::IO, e::WrongADBackendError)
+    return print(
+        io, "Expected to use $(e.expected_adtype), but using $(e.actual_adtype) instead."
+    )
+end
+
+"""
+    IncompatibleADTypeError
+
+An error thrown when an element type is encountered that is unexpected for the given ADType.
+"""
+struct IncompatibleADTypeError <: AbstractWrongADBackendError
+    valtype::Type
+    adtype::Type
+end
+
+function Base.showerror(io::IO, e::IncompatibleADTypeError)
+    return print(
+        io,
+        "Incompatible ADType: Did not expect element of type $(e.valtype) with $(e.adtype)",
+    )
+end
+
+"""
+    ADTypeCheckContext{ADType,ChildContext}
+
+A context for checking that the expected ADType is being used.
+
+Evaluating a model with this context will check that the types of values in a `VarInfo` are
+compatible with the ADType of the context. If the check fails, an `IncompatibleADTypeError`
+is thrown.
+
+For instance, evaluating a model with
+`ADTypeCheckContext(AutoForwardDiff(), child_context)`
+would throw an error if within the model a type associated with e.g. ReverseDiff was
+encountered.
+
+"""
+struct ADTypeCheckContext{ADType,ChildContext<:DynamicPPL.AbstractContext} <:
+       DynamicPPL.AbstractContext
+    child::ChildContext
+
+    function ADTypeCheckContext(adbackend, child)
+        adtype = adbackend isa Type ? adbackend : typeof(adbackend)
+        if !any(adtype <: k for k in keys(eltypes_by_adtype))
+            throw(ArgumentError("Unsupported ADType: $adtype"))
+        end
+        return new{adtype,typeof(child)}(child)
+    end
+end
+
+adtype(_::ADTypeCheckContext{ADType}) where {ADType} = ADType
+
+DynamicPPL.NodeTrait(::ADTypeCheckContext) = DynamicPPL.IsParent()
+DynamicPPL.childcontext(c::ADTypeCheckContext) = c.child
+function DynamicPPL.setchildcontext(c::ADTypeCheckContext, child)
+    return ADTypeCheckContext(adtype(c), child)
+end
+
+"""
+    valid_eltypes(context::ADTypeCheckContext)
+
+Return the element types that are valid for the ADType of `context` as a tuple.
+"""
+function valid_eltypes(context::ADTypeCheckContext)
+    context_at = adtype(context)
+    for at in keys(eltypes_by_adtype)
+        if context_at <: at
+            return (eltypes_by_adtype[at]..., always_valid_eltypes...)
+        end
+    end
+    # This should never be reached due to the check in the inner constructor.
+    throw(ArgumentError("Unsupported ADType: $(adtype(context))"))
+end
+
+"""
+    check_adtype(context::ADTypeCheckContext, vi::DynamicPPL.VarInfo)
+
+Check that the element types in `vi` are compatible with the ADType of `context`.
+
+Throw an `IncompatibleADTypeError` if an incompatible element type is encountered.
+"""
+function check_adtype(context::ADTypeCheckContext, vi::DynamicPPL.AbstractVarInfo)
+    valids = valid_eltypes(context)
+    for val in vi[:]
+        valtype = typeof(val)
+        if !any(valtype .<: valids)
+            throw(IncompatibleADTypeError(valtype, adtype(context)))
+        end
+    end
+    return nothing
+end
+
+# A bunch of tilde_assume/tilde_observe methods that just call the same method on the child
+# context, and then call check_adtype on the result before returning the results from the
+# child context.
+
+function DynamicPPL.tilde_assume(context::ADTypeCheckContext, right, vn, vi)
+    value, logp, vi = DynamicPPL.tilde_assume(
+        DynamicPPL.childcontext(context), right, vn, vi
+    )
+    check_adtype(context, vi)
+    return value, logp, vi
+end
+
+function DynamicPPL.tilde_assume(
+    rng::Random.AbstractRNG, context::ADTypeCheckContext, sampler, right, vn, vi
+)
+    value, logp, vi = DynamicPPL.tilde_assume(
+        rng, DynamicPPL.childcontext(context), sampler, right, vn, vi
+    )
+    check_adtype(context, vi)
+    return value, logp, vi
+end
+
+function DynamicPPL.tilde_observe(context::ADTypeCheckContext, right, left, vi)
+    logp, vi = DynamicPPL.tilde_observe(DynamicPPL.childcontext(context), right, left, vi)
+    check_adtype(context, vi)
+    return logp, vi
+end
+
+function DynamicPPL.tilde_observe(context::ADTypeCheckContext, sampler, right, left, vi)
+    logp, vi = DynamicPPL.tilde_observe(
+        DynamicPPL.childcontext(context), sampler, right, left, vi
+    )
+    check_adtype(context, vi)
+    return logp, vi
+end
+
+"""
+All the ADTypes on which we want to run the tests.
+"""
+ADTYPES = [
+    Turing.AutoForwardDiff(),
+    Turing.AutoReverseDiff(; compile=false),
+    Turing.AutoMooncake(; config=nothing),
+]
+
+@testset verbose = true "AD / ADTypeCheckContext" begin
+    # This testset ensures that samplers don't accidentally override the AD
+    # backend set in it.
+    @testset "Check ADType" begin
+        seed = 123
+        alg = HMC(0.1, 10; adtype=adtype)
+        m = DynamicPPL.contextualize(
+            gdemo_default, ADTypeCheckContext(adtype, gdemo_default.context)
+        )
+        # These will error if the adbackend being used is not the one set.
+        sample(StableRNG(seed), m, alg, 10)
+    end
+end
 
 @testset verbose = true "AD / SamplingContext" begin
     # AD tests for gradient-based samplers need to be run with SamplingContext
     # because samplers can potentially use this to define custom behaviour in
     # the tilde-pipeline and thus change the code executed during model
     # evaluation.
-    @testset "adtype=$adtype" for adtype in adbackends
+    @testset "adtype=$adtype" for adtype in ADTYPES
         @testset "alg=$alg" for alg in [
             HMC(0.1, 10; adtype=adtype),
             HMCDA(0.8, 0.75; adtype=adtype),
@@ -30,16 +216,6 @@ using ..ADUtils: ADTypeCheckContext, adbackends
                 @test run_ad(model, adtype; context=ctx, test=true, benchmark=false) isa Any
             end
         end
-
-        @testset "Check ADType" begin
-            seed = 123
-            alg = HMC(0.1, 10; adtype=adtype)
-            m = DynamicPPL.contextualize(
-                gdemo_default, ADTypeCheckContext(adtype, gdemo_default.context)
-            )
-            # These will error if the adbackend being used is not the one set.
-            sample(StableRNG(seed), m, alg, 10)
-        end
     end
 end
 
@@ -49,7 +225,7 @@ end
     # `gibbs_initialstep_recursive` and `gibbs_step_recursive` in
     # src/mcmc/gibbs.jl -- the code here mimics what happens in those
     # functions)
-    @testset "adtype=$adtype" for adtype in adbackends
+    @testset "adtype=$adtype" for adtype in ADTYPES
         @testset "model=$(model.f)" for model in DEMO_MODELS
             # All the demo models have variables `s` and `m`, so we'll pretend
             # that we're using a Gibbs sampler where both of them are sampled
