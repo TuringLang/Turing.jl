205704b8f6e89ee8a17d67f3d469cbfcf2794925
Penelope Yong <penelopeysm@gmail.com>
2025-10-21 09:59:56 +0100

    Fix qualifier in src/mcmc/external_sampler.jl
    Co-authored-by: Xianda Sun <5433119+sunxd3@users.noreply.github.com>

--- src/mcmc/external_sampler.jl
+++ src/mcmc/external_sampler.jl
@@ -142,7 +142,7 @@ function AbstractMCMC.step(
     model::DynamicPPL.Model,
     sampler_wrapper::ExternalSampler;
     initial_state=nothing,
-    initial_params=DynamicPPL.init_strategy(sampler_wrapper.sampler),
+    initial_params=Turing.Inference.init_strategy(sampler_wrapper.sampler),
     kwargs...,
 )
     sampler = sampler_wrapper.sampler
