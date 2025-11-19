b6d820261866c0fb6ccac9a3da66989c78ad8968
Kyurae Kim <msca8h@naver.com>
2025-11-19 17:11:24 -0500

    run formatter
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/variational/vi.jl
+++ test/variational/vi.jl
@@ -53,7 +53,7 @@ using Turing.Variational
     @testset "custom algorithm $name" for (name, algorithm) in [
         ("KLMinRepGradProxDescent", KLMinRepGradProxDescent(adtype; n_samples=10)),
         ("KLMinRepGradDescent", KLMinRepGradDescent(adtype; operator, n_samples=10)),
-        ("KLMinNaturalGradDescent", KLMinNaturalGradDescent(stepsize=1e-3, n_samples=10)),
+        ("KLMinNaturalGradDescent", KLMinNaturalGradDescent(; stepsize=1e-3, n_samples=10)),
         (
             "KLMinSqrtNaturalGradDescent",
             KLMinSqrtNaturalGradDescent(stepsize=1e-3, n_samples=10),
