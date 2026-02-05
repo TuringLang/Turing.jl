3a61cf0b52db5d8ae3a9b510234a7842c1969596
Penelope Yong <penelopeysm@gmail.com>
2026-02-05 13:03:08 +0000

    fix missing import

--- src/init_strategy.jl
+++ src/init_strategy.jl
@@ -37,6 +37,6 @@ function _check_model(model::DynamicPPL.Model)
         new_model, DynamicPPL.OnlyAccsVarInfo(); error_on_failure=true
     )
 end
-function _check_model(model::DynamicPPL.Model, ::AbstractSampler)
+function _check_model(model::DynamicPPL.Model, ::AbstractMCMC.AbstractSampler)
     return _check_model(model)
 end
