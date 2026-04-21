904897d7ccfdc0aeb857cca08468102402d7173a
Penelope Yong <penelopeysm@gmail.com>
2026-04-21 12:04:11 +0100

    Document Gibbs limitations

--- src/mcmc/gibbs.jl
+++ src/mcmc/gibbs.jl
@@ -344,7 +344,7 @@ Gibbs((@varname(x), :y) => NUTS(), :z => MH())
 Note that all variables in the model should be handled by one or more samplers. The
 behaviour of Gibbs when there are unhandled variables is undefined: depending on the version
 of Turing, it may either crash, or it may sample once from the prior and not update values
-after that.
+after that. See https://github.com/TuringLang/Turing.jl/issues/2810 for more information.
 
 There is currently no way to specify a different initialisation strategy for for each
 component sampler individually. When sampling with Gibbs, `initial_params` applies to the
