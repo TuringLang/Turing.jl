4817f9b9fcbe8e122ba254b6fbbe5956852bc5bd
Charles Knipp <charles.a.knipp@frb.gov>
2026-07-16 16:56:08 -0400

    uncomment unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,53 +34,53 @@ end
         @timeit_include("Aqua.jl")
     end
 
-    # @testset "AD" verbose = true begin
-    #     @timeit_include("ad.jl")
-    # end
-
-    # @testset "essential" verbose = true begin
-    #     @timeit_include("essential/container.jl")
-    # end
-
-    @testset "samplers (without AD)" verbose = true begin
-        # @timeit_include("mcmc/abstractmcmc.jl")
-        # @timeit_include("mcmc/callbacks.jl")
-        # @timeit_include("mcmc/chains.jl")
-        @timeit_include("mcmc/particle_mcmc.jl")
-        # @timeit_include("mcmc/emcee.jl")
-        # @timeit_include("mcmc/ess.jl")
+    @testset "AD" verbose = true begin
+        @timeit_include("ad.jl")
     end
 
-    # @timeit TIMEROUTPUT "inference" begin
-    #     @testset "inference with samplers" verbose = true begin
-    #         @timeit_include("mcmc/gibbs.jl")
-    #         @timeit_include("mcmc/gibbs_conditional.jl")
-    #         @timeit_include("mcmc/hmc.jl")
-    #         @timeit_include("mcmc/Inference.jl")
-    #         @timeit_include("mcmc/sghmc.jl")
-    #         @timeit_include("mcmc/external_sampler.jl")
-    #         @timeit_include("mcmc/mh.jl")
-    #         @timeit_include("ext/dynamichmc.jl")
-    #         @timeit_include("mcmc/repeat_sampler.jl")
-    #     end
+    @testset "essential" verbose = true begin
+        @timeit_include("essential/container.jl")
+    end
 
-    #     @testset "variational algorithms" begin
-    #         @timeit_include("variational/vi.jl")
-    #     end
+    @testset "samplers (without AD)" verbose = true begin
+        @timeit_include("mcmc/abstractmcmc.jl")
+        @timeit_include("mcmc/callbacks.jl")
+        @timeit_include("mcmc/chains.jl")
+        @timeit_include("mcmc/particle_mcmc.jl")
+        @timeit_include("mcmc/emcee.jl")
+        @timeit_include("mcmc/ess.jl")
+    end
 
-    #     @testset "mode estimation" verbose = true begin
-    #         @timeit_include("optimisation/Optimisation.jl")
-    #     end
-    # end
+    @timeit TIMEROUTPUT "inference" begin
+        @testset "inference with samplers" verbose = true begin
+            @timeit_include("mcmc/gibbs.jl")
+            @timeit_include("mcmc/gibbs_conditional.jl")
+            @timeit_include("mcmc/hmc.jl")
+            @timeit_include("mcmc/Inference.jl")
+            @timeit_include("mcmc/sghmc.jl")
+            @timeit_include("mcmc/external_sampler.jl")
+            @timeit_include("mcmc/mh.jl")
+            @timeit_include("ext/dynamichmc.jl")
+            @timeit_include("mcmc/repeat_sampler.jl")
+        end
 
-    # @testset "stdlib" verbose = true begin
-    #     @timeit_include("stdlib/distributions.jl")
-    #     @timeit_include("stdlib/RandomMeasures.jl")
-    # end
+        @testset "variational algorithms" begin
+            @timeit_include("variational/vi.jl")
+        end
 
-    # @testset "utilities" begin
-    #     @timeit_include("mcmc/utilities.jl")
-    # end
+        @testset "mode estimation" verbose = true begin
+            @timeit_include("optimisation/Optimisation.jl")
+        end
+    end
+
+    @testset "stdlib" verbose = true begin
+        @timeit_include("stdlib/distributions.jl")
+        @timeit_include("stdlib/RandomMeasures.jl")
+    end
+
+    @testset "utilities" begin
+        @timeit_include("mcmc/utilities.jl")
+    end
 end
 
 show(TIMEROUTPUT; compact=true, sortby=:firstexec)
